d67a0826b06cb97862638fed7feead4c53dce815
Bassel Mannaa <bm@concordium.com>
2019-02-07 11:43:01 +0100

    custom random bytes

--- Concordium/Crypto/Ed25519/ed25519-randombytes-custom.h
+++ Concordium/Crypto/Ed25519/ed25519-randombytes-custom.h
@@ -1,8 +1,12 @@
-/*
-	a custom randombytes must implement:
+#include "../Hacl/hacl_test_utils.h"
+void
+ED25519_FN(ed25519_randombytes_unsafe) (void *p, size_t len) {
 
-	void ED25519_FN(ed25519_randombytes_unsafe) (void *p, size_t len);
+  read_random_bytes((uint64_t) len,  p);
 
-	ed25519_randombytes_unsafe is used by the batch verification function
-	to create random scalars
-*/
+}
+
+
+
+
+
