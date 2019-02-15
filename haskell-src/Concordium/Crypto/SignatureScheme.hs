046d912f8dbc752e1ad9e0783463891fbfdca456
Bassel Mannaa <bm@concordium.com>
2019-02-15 15:46:14 +0100

    SignatureScheme class

--- haskell-src/Concordium/Crypto/SignatureScheme.hs
+++ haskell-src/Concordium/Crypto/SignatureScheme.hs
@@ -1,31 +1,47 @@
-{-# INCLUDE <termios.h> #-}
-{-# INCLUDE "termops.h" #-}
-{-# LANGUAGE ForeignFunctionInterface #-}
-
+{-# LANGUAGE TypeFamilies #-}
 module SignatureScheme where
+import           Data.ByteString (ByteString) 
+import qualified Data.ByteString as B
 
-import System.Random
-import Data.ByteString
-import Foreign.C
-import Forieng.Ptr (Ptr,nullPtr)
 
-hashUpdates :: forall a ba . (HashAlgorithm a, ByteArrayAccess ba)
-            => Context a
-            -> [ba]
-            -> Context a
-hashUpdates c l
-    | null ls   = c
-    | otherwise = Context $ B.copyAndFreeze c $ \(ctx :: Ptr (Context a)) ->
-        mapM_ (\b -> B.withByteArray b $ \d -> hashInternalUpdate ctx d (fromIntegral $ B.length b)) ls
-  where
-    ls = filter (not . B.null) l
+data SchemeID = Schnorr | CamenischeLysyanskaya | ECDSA_Ed25519
+class SignatureScheme scheme where
+    data VerifyKey scheme :: *
+    data SignKey scheme :: *
+    data Signature scheme :: *
+    generatePrivateKey ::  SignKey scheme
+    publicKey :: SignKey scheme -> VerifyKey scheme
+    sign :: SignKey scheme -> VerifyKey scheme -> ByteString -> Signature scheme
+    verify :: VerifyKey scheme -> ByteString -> Signature scheme -> Bool
 
---hash:: ByteString -> ByteString allocAndFreeze sizeOfHash (\(hash::ptr) -> withByteArray (\(tohash::ptr) -> hacl_sha2_256 hash tohash len)
-hash (PS ptr offset len) = allocAndFreeze 265 (\hash -> Hacl_SHA2_265_hash hash ptr len) 
---Hacl_SHA2_256_hash(uint8_t *hash1, uint8_t *input, uint32_t len)
---
-foreign import ccall "Hacl_SHA2_256_hash" :: Ptr Word8 -> Ptr Word8 -> Word32 -> IO () 
-data SignKey = SignKey ByteString
 
-data VerifKey = VerifKey ByteString
+-- A setup takes a scheme id and returns an element of the corresponding signature scheme
+-- e.g. setup CamenischeLysyanskaya would return a pair (q, G, G', e) where q is the oder of G (G')
+--  and e : G x G -> G' a bilnear map (note G and G' are not types)
+--  SecurityParam is yet to be defined. e.g. in CL we need a bit length as a parameter
+--setup :: (SignatureScheme a) => [SecurityParam] -> SchemeId -> a
 
+-- Example the signature scheme CamenischeLysyanskaya 
+-- FiniteGroup is just and identifier G (g, p) where g is a generator and p is the order
+-- What shoudl BinlinearMap be???
+--data CL = (Int, FiniteGroup, FiniteGroup, BilinearMap)
+data Ed25519 = Ed25519
+
+instance SignatureScheme Ed25519 where
+    data VerifyKey Ed25519    = Ed25519_PK ByteString
+    data SignKey Ed25519      = Ed25519_SK ByteString
+    data Signature Ed25519    = Ed25519_Sig ByteString
+    generatePrivateKey        = Ed25519_SK B.empty
+    publicKey (Ed25519_SK x) = Ed25519_PK x
+    sign (Ed25519_SK x) (Ed25519_PK y) b = Ed25519_Sig b
+    verify (Ed25519_PK x) b s = True
+-- Example instance CL signature scheme
+-- Scheme B in Camenische-Lysyanskaya 04 paper
+    {-
+instance SignatureScheme CL where
+	schemeID = CamenischeLysyanskaya
+        data VerifKey CL = VK { order :: Int, group :: FiniteGroupd, group' :: FiniteGroup, e :: BilinearMap, _X :: Int, _Y :: Int, _Z :: Int}
+        data SignKey CL = SK { x :: Int, y :: Int, z :: Int}
+            .....
+
+--}
