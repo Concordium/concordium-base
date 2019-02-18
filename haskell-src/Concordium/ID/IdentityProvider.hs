cc94d4aa639a08d419e1763204ce187b29ce2b98
Bassel Mannaa <bm@concordium.com>
2019-02-18 16:10:25 +0100

    initial: API for IP

--- haskell-src/Concordium/ID/IdentityProvider.hs
+++ haskell-src/Concordium/ID/IdentityProvider.hs
@@ -1,26 +1,38 @@
-module Concrodium.Crypto.ID.IdProvider
+module Concordium.ID.IdentityProvider where
 
-class SignatureScheme s where
-    sign    :: SignKey-> VerifyKey -> ByteString -> s
-    verify  :: VerifyKey -> ByteString -> s -> Bool
-      
+import           Concordium.Crypto.SignatureScheme
+import           Concordium.Crypto.Elgamal
+import qualified Data.ByteString as B
 
-data AHI = AHI IdAH IdCredPub IdCredSec PrfKey AL --stored by AH
-data AHC = AHC IdIP AHI Signature
+--newtype PublicKey = PK B.ByteString
+newtype ZKP = ZKP B.ByteString
+newtype Commitment = Cmt B.ByteString
+
+type Attribute = String
+
+type AL = [Attribute]
+
+type IdAH =  String
+--type IdCredPub = AHPK ByteString
+--type IdCred = AHPK ByteString
+--data AHI = AHI IdAH IdCredPub IdCredSec PrfKey AL --stored by AH
+--data AHC = AHC IdIP AHI Signature
 -- Signature on (IdCredSec, k, AL)
-data ACI = -- on the chain
+--data ACI = -- on the chain
 data IPIAH = IPIAH {ipiah_AhId                      ::  IdAH, 
                     ipiah_AhAttrList                ::  AL  , 
                     ipiah_AhIdCredPub               ::  PublicKey,
                     ipiah_AhPrfKeyCommitment        ::  Commitment, 
                     ipiah_ZkpOfPrfKeyAndIdCredSec   ::  ZKP,
-                    ipiah_ERegID                    ::  [(AR, [Cipher])],
+                    ipiah_ERegId                    ::  [(AR, [Cipher])],
                     ipiah_zkpOfMatchingPrfKey       ::  ZKP 
+                   }
 
-newtype AR = AR IdAR PublicKey
+type IdAR = String
+data AR = AR IdAR PublicKey
 
 type REASON = String
-data CertificationOutcome = SUCCESS Signature | FALIURE REASON 
+data CertificationOutcome = SUCCESS (Signature CL) | FAILURE REASON 
 
 
 -- | The commitment is a commitment to PRF key
@@ -32,19 +44,19 @@ verifyKeys = undefined
 verifyMatchingPrfKeyCommitment:: Commitment -> [(AR,[Cipher])] -> ZKP -> Bool 
 verifyMatchingPrfKeyCommitment  = undefined
 
-certifyAH :: SignKey-> VerifyKey -> IPIAH -> CertificationOutcome 
-cerifyAH signK verifyK ipiah = if (not b) 
+certifyAH :: SignKey CL-> VerifyKey CL -> IPIAH -> CertificationOutcome 
+certifyAH signK verifyK ipiah = if (not b) 
                                   then FAILURE "Verification of keys failed" 
                                else if (not b') 
                                   then FAILURE "Verification of AR data failed"
-                               else SUCCESS (sign signK verifyK..)
+                               else SUCCESS (sign signK verifyK B.empty)
                     where
                         prfKeyCommitment = ipiah_AhPrfKeyCommitment ipiah
                         ahPubKey         = ipiah_AhIdCredPub ipiah
                         prfKeys          = ipiah_ZkpOfPrfKeyAndIdCredSec ipiah
                         eRegId           = ipiah_ERegId ipiah
                         prfMatching      =  ipiah_zkpOfMatchingPrfKey ipiah
-                        b = verifyKey prfKeyCommitment ahPubKey prfKeys
+                        b = verifyKeys prfKeyCommitment ahPubKey prfKeys
                         b' = verifyMatchingPrfKeyCommitment prfKeyCommitment eRegId prfMatching
                 
                
