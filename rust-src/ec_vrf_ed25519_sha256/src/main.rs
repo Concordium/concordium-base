99fcbd9d6e6a3dd1066965740b5f69e2ccf19b6d
Bassel Mannaa <bm@concordium.com>
2019-03-18 16:38:18 +0100

    testing ffi

--- rust-src/ec_vrf_ed25519_sha256/src/main.rs
+++ rust-src/ec_vrf_ed25519_sha256/src/main.rs
@@ -15,11 +15,13 @@ mod errors;
 mod public;
 mod secret;
 mod proof;
+mod ec_vrf_ed25519_sha256;
 // Export everything public in ec_vrf_ed25519_sha256.rs
 pub use crate::secret::*;
 pub use crate::public::*;
 use curve25519_dalek::edwards::*;
 use curve25519_dalek::traits::{Identity, IsIdentity};
+use ec_vrf_ed25519_sha256::*;
 
 fn main(){
    //print!("{:?}", EdwardsPoint::identity());
@@ -34,22 +36,26 @@ fn main(){
     let mut data = [0u8; 64];
     let mut sk_bytes = [0u8;32];
     let sk = SecretKey::generate(&mut csprng);
+    let sk_bytes = sk.to_bytes();
     print!("SK: "); print_bytes(&sk.to_bytes());
     let pk = PublicKey::from(&sk);
+    let pk_bytes = pk.to_bytes();
     print!("PK: "); print_bytes(&pk.to_bytes());
 
     for i in 0..100 {
         rand::thread_rng().fill_bytes(&mut data);
         print!("MSG: "); print_bytes(&data);
-        
-        let p = sk.prove(&pk, &data, &mut csprng).expect("Proof failed");
-        let h = p.to_hash();
-        print!("proof hash: "); print_bytes(&h);
+        let mut proof =  [0u8;80];
+        ec_vrf_prove(&mut proof, &pk_bytes, &sk_bytes, data.as_ptr(), data.len()); 
+        println!("{}", ec_vrf_verify(&pk_bytes, &proof, data.as_ptr(), data.len()));
+        //let p = sk.prove(&pk, &data, &mut csprng).expect("Proof failed");
+        //let h = p.to_hash();
+        //print!("proof hash: "); print_bytes(&h);
 
-        let b = pk.verify(p, &data);
-        let z = PublicKey::verify_key(&pk.0.to_bytes());
-        println!("proof {}", if b {"Succeeded"} else {"failed"});
-        println!("verif key {}", z);
+        //let b = pk.verify(p, &data);
+        //let z = PublicKey::verify_key(&pk.0.to_bytes());
+        //println!("proof {}", if b {"Succeeded"} else {"failed"});
+        //println!("verif key {}", z);
     }
 }
 
