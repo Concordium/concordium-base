96c852830c84b75fb4f7627c472449e9c47a9f22
Bassel Mannaa <bm@concordium.com>
2019-03-21 11:51:43 +0100

    work with fixed byte string

--- haskell-src/Concordium/Crypto/Signature.hs
+++ haskell-src/Concordium/Crypto/Signature.hs
@@ -29,52 +29,69 @@ import           Data.Serialize
 import qualified Data.ByteString  as B
 import           Data.ByteString (ByteString) 
 import           Data.ByteString.Builder
+import qualified Data.FixedByteString as FBS
 import           Data.Word
 import           System.Random
 import           Foreign.Marshal.Array
 import           Foreign.Marshal.Alloc
 import           Foreign.C.Types
 
+
 foreign import ccall "eddsa_priv_key" c_priv_key :: Ptr Word8 -> IO CInt
-foreign import ccall "eddsa_pub_key" c_public_key :: Ptr Word8 -> Ptr Word8 -> IO () 
-foreign import ccall "eddsa_sign" c_sign :: Ptr Word8 -> Word32 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO () 
-foreign import ccall "eddsa_verify" c_verify :: Ptr Word8 -> Word32 -> Ptr Word8 -> Ptr Word8 -> IO CInt 
+foreign import ccall "eddsa_pub_key" c_public_key :: Ptr Word8 -> Ptr Word8 -> IO ()
+foreign import ccall "eddsa_sign" c_sign :: Ptr Word8 -> Word32 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> IO ()
+foreign import ccall "eddsa_verify" c_verify :: Ptr Word8 -> Word32 -> Ptr Word8 -> Ptr Word8 -> IO CInt
 
 
-privKeyToHex :: SignKey -> String
-privKeyToHex (SignKey sk) = byteStringToHex sk
+signKeySize :: Int
+signKeySize = 32
+verifyKeySize :: Int
+verifyKeySize = 32
+signatureSize :: Int
+signatureSize = 64
 
-pubKeyToHex :: VerifyKey -> String
-pubKeyToHex (VerifyKey pk) = byteStringToHex pk
+data SignKeySize
+instance FBS.FixedLength SignKeySize where
+    fixedLength _ = signKeySize
+
+data VerifyKeySize
+instance FBS.FixedLength VerifyKeySize where
+    fixedLength _ = verifyKeySize
+
+data SignatureSize
+instance FBS.FixedLength SignatureSize where
+    fixedLength _ = signatureSize
+
+
 
 -- |Signature private key.  32 bytes
-data SignKey = SignKey ByteString
+data SignKey = SignKey (FBS.FixedByteString SignKeySize)
     deriving (Eq)
 instance Serialize SignKey where
-    put (SignKey key) = putByteString key
-    get = SignKey <$> getByteString 32
+    put (SignKey key) = putByteString $ FBS.toByteString key
+    get = SignKey . FBS.fromByteString <$> getByteString signKeySize
 instance Show SignKey where
-    show (SignKey sk) = byteStringToHex sk
+    show (SignKey sk) = byteStringToHex $ FBS.toByteString sk
 
 
 -- |Signature public (verification) key. 32 bytes
-data VerifyKey = VerifyKey ByteString
+data VerifyKey = VerifyKey (FBS.FixedByteString VerifyKeySize)
     deriving (Eq, Ord)
 instance Serialize VerifyKey where
-    put (VerifyKey key) = putByteString key
-    get = VerifyKey <$> getByteString 32
+    put (VerifyKey key) = putByteString $ FBS.toByteString key
+    get = VerifyKey . FBS.fromByteString <$> getByteString verifyKeySize
 instance Show VerifyKey where
-    show (VerifyKey vk) = byteStringToHex vk
+    show (VerifyKey vk) = byteStringToHex $ FBS.toByteString vk
 
 -- |Signature. 64 bytes
-newtype Signature = Signature ByteString
+newtype Signature = Signature (FBS.FixedByteString SignatureSize)
     deriving (Eq)
 
 instance Serialize Signature where
-    put (Signature sig) = putByteString sig
-    get = Signature <$> getByteString 64
+    put (Signature sig) = putByteString $ FBS.toByteString sig
+    get = Signature . FBS.fromByteString <$> getByteString signatureSize
 instance Show Signature where
-    show (Signature sig) = byteStringToHex sig
+    show (Signature sig) = byteStringToHex $ FBS.toByteString sig
 
 data KeyPair = KeyPair {
     signKey :: SignKey,
@@ -90,7 +107,7 @@ instance Serialize KeyPair where
 newPrivKey :: IO SignKey
 newPrivKey =
      do suc <- newIORef (0::Int)
-        sk <- create 32 $ \priv ->
+        sk <- FBS.create $ \priv ->
             do rc <-  c_priv_key priv
                case rc of
                     1 ->  do writeIORef suc 1
@@ -101,15 +118,15 @@ newPrivKey =
             _ -> error "Private key generation failed"
 
 pubKey :: SignKey -> IO VerifyKey
-pubKey (SignKey sk) = do pk <- create 32 $ \pub -> 
-                                 withByteStringPtr sk $ \y -> c_public_key y pub
+pubKey (SignKey sk) = do pk <- FBS.create $ \pub -> 
+                                 FBS.withPtr sk $ \y -> c_public_key y pub
                          return (VerifyKey pk)
 
 randomKeyPair :: RandomGen g => g -> (KeyPair, g)
 randomKeyPair gen = (key, gen')
         where
             (gen0, gen') = split gen
-            privKey = SignKey $ B.pack $ take 32 $ randoms gen0
+            privKey = SignKey $ FBS.pack $ randoms gen0
             key = KeyPair privKey (unsafePerformIO $ pubKey privKey)
 
 
@@ -119,11 +136,10 @@ newKeyPair = do sk <- newPrivKey
                 return (KeyPair sk pk)
 
 sign :: KeyPair -> ByteString -> Signature
-sign (KeyPair (SignKey sk) (VerifyKey pk)) m = Signature $ unsafeDupablePerformIO $ 
-    create 64 $ \sig ->
+sign (KeyPair (SignKey sk) (VerifyKey pk)) m = Signature $ FBS.unsafeCreate $ \sig ->
        withByteStringPtr m $ \m' -> 
-          withByteStringPtr pk $ \pk' ->
-             withByteStringPtr sk $ \sk' ->
+          FBS.withPtr pk $ \pk' ->
+             FBS.withPtr sk $ \sk' ->
                 c_sign m' mlen sk' pk' sig 
    where
        mlen = fromIntegral $ B.length m
@@ -135,20 +151,20 @@ verify (VerifyKey pk) m (Signature sig) =  suc > -1
        mlen = fromIntegral $ B.length m
        suc  = unsafeDupablePerformIO $ 
            withByteStringPtr m $ \m'->
-                 withByteStringPtr pk $ \pk'->
-                    withByteStringPtr sig $ \sig' ->
+                 FBS.withPtr pk $ \pk'->
+                    FBS.withPtr sig $ \sig' ->
                        c_verify m' mlen pk' sig'
 
 
 
 test :: IO ()
 test = do kp@(KeyPair sk pk) <- newKeyPair
-          putStrLn ("SK: " ++ privKeyToHex sk)
-          putStrLn ("PK: " ++ pubKeyToHex pk)
+          putStrLn ("SK: " ++ show sk)
+          putStrLn ("PK: " ++ show pk)
           putStrLn("MESSAGE:")
           alpha <- B.getLine
-          let sig@(Signature b) = sign kp alpha
+          let sig = sign kp alpha
               suc = verify pk alpha sig
            in
-              putStrLn ("signature: " ++ byteStringToHex b) >>
+              putStrLn ("signature: " ++ show sig) >>
               putStrLn ("Good?: " ++ if suc then "YES" else "NO")
