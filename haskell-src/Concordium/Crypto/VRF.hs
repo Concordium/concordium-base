567be9e5d1450c9fe8b834fd4024272d86ed7175
Bassel Mannaa <bm@concordium.com>
2019-03-21 11:51:50 +0100

    work with fixed byte string

--- haskell-src/Concordium/Crypto/VRF.hs
+++ haskell-src/Concordium/Crypto/VRF.hs
@@ -29,6 +29,7 @@ import           Data.ByteString            (ByteString)
 import qualified Data.ByteString            as B
 import qualified Data.ByteString.Lazy       as L
 import           Data.ByteString.Internal   (create, toForeignPtr)
+import qualified Data.FixedByteString       as FBS
 import           Foreign.Ptr
 import           Foreign.ForeignPtr
 import           Data.Word
@@ -46,48 +47,67 @@ import           Text.Printf
 import           Concordium.Crypto.SHA256
 import           System.Random
 
+
 foreign import ccall "ec_vrf_priv_key" c_priv_key :: Ptr Word8 -> IO CInt
 foreign import ccall "ec_vrf_pub_key" c_public_key :: Ptr Word8 -> Ptr Word8 -> IO CInt
-foreign import ccall "ec_vrf_prove" c_prove :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Word32-> IO () 
+foreign import ccall "ec_vrf_prove" c_prove :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Word32-> IO ()
 foreign import ccall "ec_vrf_proof_to_hash" c_proof_to_hash :: Ptr Word8 -> Ptr Word8 -> IO CInt
 foreign import ccall "ec_vrf_verify_key" c_verify_key :: Ptr Word8 -> IO CInt
 foreign import ccall "ec_vrf_verify" c_verify :: Ptr Word8 -> Ptr Word8 -> Ptr Word8 -> Word32-> IO CInt
 
+-- |The size of a VRF public key in bytes (32).
+publicKeySize :: Int
+publicKeySize = 32
 
-privKeyToHex :: PrivateKey -> String
-privKeyToHex (PrivateKey sk) = byteStringToHex sk
-
-pubKeyToHex :: PublicKey -> String
-pubKeyToHex (PublicKey pk) = byteStringToHex pk
+data PublicKeySize
+instance FBS.FixedLength PublicKeySize where
+    fixedLength _ = publicKeySize
 
 -- |A VRF public key. 32 bytes.
-data PublicKey = PublicKey ByteString
+data PublicKey = PublicKey (FBS.FixedByteString PublicKeySize)
     deriving (Eq, Ord)
 instance Serialize PublicKey where
-    put (PublicKey key) = putByteString key
-    get = PublicKey <$> getByteString 32
+    put (PublicKey key) = putByteString $ FBS.toByteString key
+    get = PublicKey . FBS.fromByteString <$> getByteString (publicKeySize)
 instance Show PublicKey where
-    show (PublicKey key) = byteStringToHex key
+    show (PublicKey key) = byteStringToHex $ FBS.toByteString key
+
+-- |The size of a VRF private key in bytes (32).
+privateKeySize :: Int
+privateKeySize = 32
+
+data PrivateKeySize
+instance FBS.FixedLength PrivateKeySize where
+    fixedLength _ = privateKeySize
 
 -- |A VRF private key. 32 bytes.
-data PrivateKey = PrivateKey ByteString
+data PrivateKey = PrivateKey (FBS.FixedByteString PrivateKeySize)
     deriving (Eq)
 instance Serialize PrivateKey where
-    put (PrivateKey key) = putByteString key
-    get = PrivateKey <$> getByteString 32
+    put (PrivateKey key) = putByteString $ FBS.toByteString key
+    get = PrivateKey . FBS.fromByteString <$> getByteString privateKeySize
 instance Show PrivateKey where
-    show (PrivateKey key) = byteStringToHex key
+    show (PrivateKey key) = byteStringToHex $ FBS.toByteString key
+
+
+-- |The size of a VRF proof in bytes (80).
+proofSize :: Int
+proofSize = 80
+
+data ProofSize
+instance FBS.FixedLength ProofSize where
+    fixedLength _ = proofSize
 
 -- |A VRF proof. 80 bytes.
-newtype Proof = Proof ByteString
+newtype Proof = Proof (FBS.FixedByteString ProofSize)
     deriving (Eq)
 
 instance Serialize Proof where
-    put (Proof p) = putByteString p
-    get = Proof <$> getByteString 80
+    put (Proof p) = putByteString $ FBS.toByteString p
+    get = Proof . FBS.fromByteString <$> getByteString proofSize
 
 instance Show Proof where
-    show (Proof p) = byteStringToHex p
+    show (Proof p) = byteStringToHex $ FBS.toByteString p
 
 -- |A VRF key pair.
 data KeyPair = KeyPair {
@@ -108,7 +128,7 @@ randomKeyPair :: RandomGen g => g -> (KeyPair, g)
 randomKeyPair gen = (key, gen')
         where
             (gen0, gen') = split gen
-            privKey = PrivateKey $ B.pack $ take 32 $ randoms gen0
+            privKey = PrivateKey $ FBS.pack $ randoms gen0
             key = KeyPair privKey (unsafePerformIO $ pubKey privKey)
 
 -- |Generate a new key pair using the system random number generator.
@@ -120,7 +140,7 @@ newKeyPair = do sk <- newPrivKey
 newPrivKey :: IO PrivateKey
 newPrivKey = 
     do suc <- newIORef (0::Int)
-       sk <- create 32 $ \priv -> 
+       sk <- FBS.create $ \priv -> 
            do rc <-  c_priv_key priv 
               case rc of
                    1 ->  do writeIORef suc 1 
@@ -132,8 +152,8 @@ newPrivKey =
 
 pubKey :: PrivateKey -> IO PublicKey
 pubKey (PrivateKey sk) = do suc <- newIORef (0::Int)
-                            pk  <- create 32 $ \pub -> 
-                                 do pc <- withByteStringPtr sk $ \y -> c_public_key pub y
+                            pk  <- FBS.create $ \pub -> 
+                                 do pc <- FBS.withPtr sk $ \y -> c_public_key pub y
                                     if (pc == 1) 
                                        then writeIORef suc 1
                                        else writeIORef suc 0
@@ -145,33 +165,33 @@ pubKey (PrivateKey sk) = do suc <- newIORef (0::Int)
 
 test :: IO () 
 test = do kp@(KeyPair sk pk) <- newKeyPair
-          _ <- putStrLn("SK: " ++ privKeyToHex sk)
-          _ <- putStrLn("PK: " ++ pubKeyToHex pk)
+          _ <- putStrLn("SK: " ++ show sk)
+          _ <- putStrLn("PK: " ++ show pk)
           _ <- putStrLn("MESSAGE:") 
           alpha <- B.getLine 
-          let prf@(Proof b) = prove kp alpha  
+          let prf = prove kp alpha  
               valid = verify pk alpha prf 
-              Hash h' = proofToHash prf 
+              h' = proofToHash prf 
            in
-              putStrLn ("Proof: " ++ byteStringToHex b) >>
+              putStrLn ("Proof: " ++ show prf) >>
               putStrLn ("PK IS " ++ if verifyKey pk then "OK" else "BAD") >>
               putStrLn ("Verification: " ++ if valid then "VALID" else "INVALID") >>
-              putStrLn ("Proof hash: " ++ byteStringToHex h')
+              putStrLn ("Proof hash: " ++ show h')
 
 -- |Generate a VRF proof.
 prove :: KeyPair -> ByteString -> Proof
-prove (KeyPair (PrivateKey sk) (PublicKey pk)) b = Proof $ unsafeDupablePerformIO $
-                                        create 80 $ \prf -> 
-                                           withByteStringPtr pk $ \pk' -> 
-                                               withByteStringPtr sk $ \sk' -> 
+prove (KeyPair (PrivateKey sk) (PublicKey pk)) b = Proof $
+                                        FBS.unsafeCreate $ \prf -> 
+                                           FBS.withPtr pk $ \pk' -> 
+                                               FBS.withPtr sk $ \sk' -> 
                                                    withByteStringPtr b $ \b' -> 
                                                        c_prove prf pk' sk' b' (fromIntegral $ B.length b)
 
 -- |Verify a VRF proof.
 verify :: PublicKey -> ByteString -> Proof -> Bool
 verify (PublicKey pk) alpha (Proof prf) = cIntToBool $ unsafeDupablePerformIO $ 
-                                                withByteStringPtr pk $ \pk' ->
-                                                   withByteStringPtr prf $ \pi' ->
+                                                FBS.withPtr pk $ \pk' ->
+                                                   FBS.withPtr prf $ \pi' ->
                                                      withByteStringPtr alpha $ \alpha'->
                                                        c_verify pk' pi' alpha' (fromIntegral $ B.length alpha)
               where
@@ -179,31 +199,11 @@ verify (PublicKey pk) alpha (Proof prf) = cIntToBool $ unsafeDupablePerformIO $
                                                            
 -- |Generate a 256-bit hash from a VRF proof.
 proofToHash :: Proof -> Hash
-proofToHash (Proof p) =  Hash $ unsafeDupablePerformIO $ 
-    create 32 $ \x -> 
-        withByteStringPtr p $ \p' -> c_proof_to_hash x p' >> return()
+proofToHash (Proof p) =  Hash $ FBS.unsafeCreate $ \x -> 
+        FBS.withPtr p $ \p' -> c_proof_to_hash x p' >> return()
 
 -- |Verify a VRF public key.
 verifyKey :: PublicKey -> Bool
 verifyKey (PublicKey pk) =  x > 0 
             where
-               x = unsafeDupablePerformIO $  withByteStringPtr pk $ \pk' -> c_verify_key pk'
-
--- |Convert a 'Hash' into a 'Double' value in the range [0,1].
--- This implementation takes the first 64-bit word (big-endian) and uses it
--- as the significand, with an exponent of -64.  Since the precision of a
--- 'Double' is only 53 bits, there is inevitably some loss.  This also means
--- that the outcome 1 is not possible.
-hashToDouble :: Hash -> Double
-hashToDouble (Hash  h) = case runGet getWord64be h of
-    Left e -> error e
-    Right w -> encodeFloat (toInteger w) (-64)
-
--- |Convert a 'Hash' to an 'Int'.
-hashToInt :: Hash -> Int
-hashToInt (Hash h) = case runGet getInt64be h of
-    Left e -> error e
-    Right i -> fromIntegral i
-
-
-
+               x = unsafeDupablePerformIO $ FBS.withPtr pk $ \pk' -> c_verify_key pk'
