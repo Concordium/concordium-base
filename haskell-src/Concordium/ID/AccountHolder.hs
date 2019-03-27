4d33e78e9d1654edf5ec8b098e9db260e563f343
Bassel Mannaa <bm@concordium.com>
2019-03-27 16:03:09 +0100

    dummy create account

--- haskell-src/Concordium/ID/AccountHolder.hs
+++ haskell-src/Concordium/ID/AccountHolder.hs
@@ -1,1 +1,9 @@
 module Concordium.ID.AccountHolder where
+
+import  Concordium.ID.Types
+
+-- This is a dummy module for testing
+--
+
+createAccount :: AccountHolderCertificate -> AccountCreationInformation  
+createAccount ahc = undefined
