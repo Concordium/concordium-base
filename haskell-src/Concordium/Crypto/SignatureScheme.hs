7342687b8b3d182a4b36dc4ece2fcc389ede9aac
Bassel Mannaa <bm@concordium.com>
2019-03-27 13:22:08 +0100

    cleanup

--- haskell-src/Concordium/Crypto/SignatureScheme.hs
+++ haskell-src/Concordium/Crypto/SignatureScheme.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, ExistentialQuantification #-}
 module Concordium.Crypto.SignatureScheme where
 import           Data.ByteString (ByteString) 
 import qualified Data.ByteString as B
@@ -6,7 +6,7 @@ import qualified Data.ByteString as B
 
 data SchemeID = Schnorr | CamenischeLysyanskaya | EDDSA_Ed25519
 
-class SignatureScheme scheme where
+class SignatureScheme_ scheme where
     data VerifyKey scheme :: *
     data SignKey scheme :: *
     data Signature scheme :: *
@@ -16,34 +16,13 @@ class SignatureScheme scheme where
     verify :: VerifyKey scheme -> ByteString -> Signature scheme -> Bool
 
 
+data SignatureScheme = forall a. SignatureScheme_ a => SignatureScheme a
 -- A setup takes a scheme id and returns an element of the corresponding signature scheme
--- e.g. setup CamenischeLysyanskaya would return a pair (q, G, G', e) where q is the oder of G (G')
---  and e : G x G -> G' a bilnear map (note G and G' are not types)
---  SecurityParam is yet to be defined. e.g. in CL we need a bit length as a parameter
 --setup :: (SignatureScheme a) => [SecurityParam] -> SchemeId -> a
 
--- Example the signature scheme CamenischeLysyanskaya 
--- FiniteGroup is just and identifier G (g, p) where g is a generator and p is the order
--- What shoudl BinlinearMap be???
---data CL = (Int, FiniteGroup, FiniteGroup, BilinearMap)
---
-
---data Ed25519
 data CL
 
-    {-
-instance SignatureScheme Ed25519 where
-    data VerifyKey Ed25519    = Ed25519_PK ByteString
-    data SignKey Ed25519      = Ed25519_SK ByteString
-    data Signature Ed25519    = Ed25519_Sig ByteString
-    generatePrivateKey        = unsafePerfomIO $ do (Ed25519.SignKey x) <- Ed25519.newPrivKey 
-                                                    return (Ed25519_SK x)
-    publicKey (Ed25519_SK x) = unsafePerformIO $ do (Ed25519.VerifyKey y) <- pubKey (Ed25519.SignKey y) 
-                                                    return (Ed25519_PK y)
-    sign (Ed25519_SK x) (Ed25519_PK y) b = Ed25519_Sig b
-    verify (Ed25519_PK x) b s = True
--}
-instance SignatureScheme CL where
+instance SignatureScheme_ CL where
     data VerifyKey CL    = CL_PK ByteString
     data SignKey CL      = CL_SK ByteString
     data Signature CL    = CL_Sig ByteString
