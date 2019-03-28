8377efb1470fd2fa141912fa52d90681434395f7
Bassel Mannaa <bm@concordium.com>
2019-03-28 10:14:53 +0100

    dummy function

--- haskell-src/Concordium/ID/AccountHolder.hs
+++ haskell-src/Concordium/ID/AccountHolder.hs
@@ -1,9 +1,25 @@
 module Concordium.ID.AccountHolder where
 
-import  Concordium.ID.Types
+import Concordium.ID.Types
+import Data.ByteString.Char8
+import Data.ByteString.Random.MWC
+import System.IO.Unsafe
+
+
+
+
 
 -- This is a dummy module for testing
 --
 
 createAccount :: AccountHolderCertificate -> AccountCreationInformation  
 createAccount ahc = undefined
+
+verifyAccount :: AccountCreationInformation -> Bool 
+verifyAccount = true
+
+
+ar :: AnonimityRevoker
+ar = AR (AR_ID $ pack "superman") (AR_PK $ unsafePerformIO $ random (fromIntegral 32))
+
+
