92f9a08db65f2e6c9c629fa7daf50362ff21312e
Bassel Mannaa <bm@concordium.com>
2019-03-29 16:42:13 +0100

    sha224

--- rust-src/sha_2/src/sha_2.rs
+++ rust-src/sha_2/src/sha_2.rs
@@ -1,4 +1,3 @@
-use sha2::Sha256;
 use sha2::*;
 
 use std::slice;
@@ -39,3 +38,37 @@ pub extern fn sha256_result(hash: &mut[u8;32], ptr: *mut Sha256){
 }
 
 
+#[no_mangle]
+pub extern fn sha224_new() -> *mut Sha224 {
+    Box::into_raw(Box::new(Sha224::new()))
+}
+
+#[no_mangle]
+pub extern fn sha224_free(ptr: *mut Sha224) {
+    if ptr.is_null() { return }
+    unsafe { Box::from_raw(ptr); }
+}
+
+#[no_mangle]
+pub extern fn sha224_input(ptr: *mut Sha224, a: *const u8, len: usize) {
+    let hasher: &mut Sha224 = unsafe {
+        assert!(!ptr.is_null());
+        &mut *ptr
+    };
+
+    assert!(!a.is_null(), "Null pointer in sha224_input()");
+    let data: &[u8]= unsafe { slice::from_raw_parts(a, len)};
+    hasher.input(data);
+}
+
+#[no_mangle]
+pub extern fn sha224_result(hash: &mut[u8;28], ptr: *mut Sha224){
+    let hasher = unsafe{ Box::from_raw(ptr)};
+    //let hasher  = unsafe {
+    //    assert!(!ptr.is_null());
+    //    &mut *ptr
+    //};
+    //let s = hasher.result();
+    //hash.copy_from_slice(&[0u8;32]);
+    hash.copy_from_slice(hasher.result().as_slice());
+}
