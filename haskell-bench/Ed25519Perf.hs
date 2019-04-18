369a89888ec58107448ce9fdef913d3138f5ee3b
Aleš Bizjak <ab@concordium.com>
2019-04-18 17:35:56 +0200

    Add criterion performance measurement for sign and verify of Ed25519.

---
+++ haskell-bench/Ed25519Perf.hs
@@ -0,0 +1,51 @@
+module PerformanceTests.Ed25519Perf where
+
+import Criterion
+import Criterion.Main
+import Criterion.Types
+
+import Data.ByteString.Char8 as BS
+
+import Concordium.Crypto.SignatureScheme
+import Concordium.Crypto.Ed25519Signature as S
+
+-- setupEnv :: Int -> IO (SignKey, VerifyKey, ByteString)
+setupSignEnv :: Int -> IO (ByteString, ByteString, ByteString)
+setupSignEnv n = do
+  let doc = BS.replicate n '0'
+  kp@(KeyPair (SignKey sk) (VerifyKey pk)) <- newKeyPair
+  return (sk, pk, doc)
+
+setupVerifyEnv :: Int -> IO (ByteString, ByteString, ByteString)
+setupVerifyEnv n = do
+  let doc = BS.replicate n '8'
+  kp@(KeyPair _ (VerifyKey pk)) <- newKeyPair
+  let Signature s = S.sign kp doc
+  return (pk, s, doc)
+
+signN :: Int -> Benchmark
+signN n =
+    env (setupSignEnv n) $ \ ~(sk, pk, doc) ->
+          bench ("len = " ++ show n) $ nf (\x -> let Signature s = S.sign (KeyPair (SignKey sk) (VerifyKey pk)) x in s) doc
+
+
+verifyN :: Int -> Benchmark
+verifyN n =
+  env (setupVerifyEnv n) $ \ ~(pk, s, doc) ->
+          bench ("len = " ++ show n) $ nf (\d -> S.verify (VerifyKey pk) d (Signature s)) doc
+
+main :: IO ()
+main = defaultMainWith (defaultConfig { timeLimit = 15 }) [
+  bgroup "sign"
+  [signN 100
+  ,signN 1000
+  ,signN 10000
+  ,signN 100000
+  ],
+  bgroup "verify"
+  [verifyN 100
+  ,verifyN 1000
+  ,verifyN 10000
+  ,verifyN 100000
+  ]
+  ]
---
+++ haskell-tests/Perf.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import qualified PerformanceTests.Ed25519Perf as EdPerf
+
+main = EdPerf.main
