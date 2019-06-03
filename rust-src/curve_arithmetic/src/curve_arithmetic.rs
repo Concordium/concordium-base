f9d76bbde120060588b6c6343e3d4e090b1e31b8
Bassel Mannaa <bm@concordium.com>
2019-06-03 10:01:49 +0200

    minor

--- rust-src/curve_arithmetic/src/curve_arithmetic.rs
+++ rust-src/curve_arithmetic/src/curve_arithmetic.rs
@@ -5,6 +5,9 @@
 use pairing::Field;
 use std::fmt::{Debug, Display};
 
+pub enum FieldDecodingError {
+    NotFieldElement
+}
 pub enum CurveDecodingError {
     NotOnCurve,
 }
@@ -26,4 +29,5 @@ pub trait Curve:
     fn into_compressed(&self) -> Self::Compressed;
     fn from_compressed(c: &Self::Compressed) -> Result<Self, CurveDecodingError>;
     fn from_compressed_unchecked(c: &Self::Compressed) -> Result<Self, CurveDecodingError>;
+    fn scalar_to_bytes(s: Scalar)-> Box<[u8]>
 }
