1396f5bd72b34505b709e0fc5d9f16370c80e69c
Aleš Bizjak <ab@concordium.com>
2019-07-03 13:52:44 +0200

    Finished serializations for all the different proof formats.

--- rust-src/sigma_protocols/src/modified_com_eq.rs
+++ rust-src/sigma_protocols/src/modified_com_eq.rs
@@ -1,12 +1,53 @@
-use curve_arithmetic::{bls12_381_instance::*, curve_arithmetic::Curve};
-use pairing::{bls12_381::G1, Field};
+use curve_arithmetic::curve_arithmetic::Curve;
+use pairing::Field;
 use rand::*;
 use sha2::{Digest, Sha256};
 
+use failure::Error;
+use std::io::Cursor;
+
+use crate::common::*;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ModifiedComEqProof<T: Curve> {
     challenge:        T::Scalar,
     randomised_point: (Vec<T>, T),
     witness:          (T::Scalar, Vec<T::Scalar>, Vec<T::Scalar>),
+}
+
+impl<T: Curve> ModifiedComEqProof<T> {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let rp_len = self.randomised_point.0.len();
+        let witness1_len = self.witness.1.len();
+        let witness2_len = self.witness.2.len();
+        let bytes_len = T::SCALAR_LENGTH
+            + (rp_len + 1) * T::GROUP_ELEMENT_LENGTH
+            + (1 + witness1_len + witness2_len) * T::SCALAR_LENGTH;
+        let mut bytes = Vec::with_capacity(bytes_len);
+        write_curve_scalar::<T>(&self.challenge, &mut bytes);
+        write_curve_elements(&self.randomised_point.0, &mut bytes);
+        write_curve_element(&self.randomised_point.1, &mut bytes);
+        write_curve_scalar::<T>(&self.witness.0, &mut bytes);
+        write_curve_scalars::<T>(&self.witness.1, &mut bytes);
+        write_curve_scalars::<T>(&self.witness.2, &mut bytes);
+        bytes
+    }
+
+    pub fn from_bytes(bytes: &mut Cursor<&[u8]>) -> Result<Self, Error> {
+        let mut scalar_buffer = vec![0; T::SCALAR_LENGTH];
+        let mut group_buffer = vec![0; T::GROUP_ELEMENT_LENGTH];
+        let challenge = read_curve_scalar::<T>(bytes, &mut scalar_buffer)?;
+        let rp1 = read_curve_elements::<T>(bytes, &mut group_buffer)?;
+        let rp2 = read_curve::<T>(bytes, &mut group_buffer)?;
+        let w0 = read_curve_scalar::<T>(bytes, &mut scalar_buffer)?;
+        let w1 = read_curve_scalars::<T>(bytes, &mut scalar_buffer)?;
+        let w2 = read_curve_scalars::<T>(bytes, &mut scalar_buffer)?;
+        Ok(ModifiedComEqProof {
+            challenge,
+            randomised_point: (rp1, rp2),
+            witness: (w0, w1, w2),
+        })
+    }
 }
 
 pub fn prove_com_eq<T: Curve, R: Rng>(
@@ -140,50 +181,74 @@ pub fn verify_com_eq<T: Curve>(
     }
 }
 
-#[test]
-pub fn prove_verify_modified_com_eq() {
-    let mut csprng = thread_rng();
-    for i in 1..20 {
-        let mut axs = vec![<G1 as Curve>::Scalar::zero(); i];
-        let mut bxs = vec![<G1 as Curve>::Scalar::zero(); i];
-        let mut gxs = vec![<G1 as Curve>::zero_point(); i];
-        let g = G1::generate(&mut csprng);
-        let h = G1::generate(&mut csprng);
-        let q = G1::generate(&mut csprng);
-        let p = G1::generate(&mut csprng);
-        let mut cxs = vec![G1::zero_point(); i];
-        let mut y = G1::zero_point();
-        let sec = G1::generate_scalar(&mut csprng);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pairing::bls12_381::G1;
 
-        for j in 0..i {
-            axs[j] = G1::generate_scalar(&mut csprng);
-            bxs[j] = G1::generate_scalar(&mut csprng);
-            gxs[j] = G1::generate(&mut csprng);
-            y = y.plus_point(&gxs[j].mul_by_scalar(&axs[j]));
-            cxs[j] = cxs[j]
-                .plus_point(&g.mul_by_scalar(&axs[j]))
-                .plus_point(&h.mul_by_scalar(&bxs[j]));
+    #[test]
+    pub fn prove_verify_modified_com_eq() {
+        let mut csprng = thread_rng();
+        for i in 1..20 {
+            let mut axs = vec![<G1 as Curve>::Scalar::zero(); i];
+            let mut bxs = vec![<G1 as Curve>::Scalar::zero(); i];
+            let mut gxs = vec![<G1 as Curve>::zero_point(); i];
+            let g = G1::generate(&mut csprng);
+            let h = G1::generate(&mut csprng);
+            let q = G1::generate(&mut csprng);
+            let p = G1::generate(&mut csprng);
+            let mut cxs = vec![G1::zero_point(); i];
+            let mut y = G1::zero_point();
+            let sec = G1::generate_scalar(&mut csprng);
+
+            for j in 0..i {
+                axs[j] = G1::generate_scalar(&mut csprng);
+                bxs[j] = G1::generate_scalar(&mut csprng);
+                gxs[j] = G1::generate(&mut csprng);
+                y = y.plus_point(&gxs[j].mul_by_scalar(&axs[j]));
+                cxs[j] = cxs[j]
+                    .plus_point(&g.mul_by_scalar(&axs[j]))
+                    .plus_point(&h.mul_by_scalar(&bxs[j]));
+            }
+            y = y.plus_point(&q.mul_by_scalar(&sec));
+            y = y.plus_point(&p);
+            let coeff = (p, q, g, h, gxs);
+            let evaluation = (cxs, y);
+            let proof = prove_com_eq(&evaluation, &coeff, &(sec, bxs, axs), &mut csprng);
+            assert!(verify_com_eq(&evaluation, &coeff, &proof));
         }
-        y = y.plus_point(&q.mul_by_scalar(&sec));
-        y = y.plus_point(&p);
-        let coeff = (p, q, g, h, gxs);
-        let evaluation = (cxs, y);
-        let proof = prove_com_eq(&evaluation, &coeff, &(sec, bxs, axs), &mut csprng);
-        assert!(verify_com_eq(&evaluation, &coeff, &proof));
+    }
+
+    #[test]
+    pub fn test_modified_com_eq_proof_serialization() {
+        let mut csprng = thread_rng();
+        for _ in 1..100 {
+            let challenge = G1::generate_scalar(&mut csprng);
+            let lrp1 = csprng.gen_range(1, 30);
+            let mut rp1 = Vec::with_capacity(lrp1);
+            for _ in 0..lrp1 {
+                rp1.push(G1::generate(&mut csprng));
+            }
+            let rp2 = G1::generate(&mut csprng);
+            let lw1 = csprng.gen_range(1, 87);
+            let mut w1 = Vec::with_capacity(lw1);
+            for _ in 0..lw1 {
+                w1.push(G1::generate_scalar(&mut csprng));
+            }
+            let lw2 = csprng.gen_range(1, 100);
+            let mut w2 = Vec::with_capacity(lw1);
+            for _ in 0..lw2 {
+                w2.push(G1::generate_scalar(&mut csprng));
+            }
+            let cep = ModifiedComEqProof {
+                challenge,
+                randomised_point: (rp1, rp2),
+                witness: (G1::generate_scalar(&mut csprng), w1, w2),
+            };
+            let bytes = cep.to_bytes();
+            let cepp = ModifiedComEqProof::from_bytes(&mut Cursor::new(&bytes));
+            assert!(cepp.is_ok());
+            assert_eq!(cep, cepp.unwrap());
+        }
     }
 }
-
-//   let coeff = G1Affine::generate(&mut csprng);
-// let secret   = G1Affine::generate_scalar(&mut csprng);
-// let evaluation = coeff.mul_by_scalar(&secret);
-// let mut proof = prove_dlog(&evaluation, &coeff, &secret,  &mut csprng);
-// assert!(verify_dlog(&evaluation, &coeff, &proof));
-// let wrong_a = G1Affine::generate(&mut csprng);
-// let DlogProof(c,(a,z)) = proof;
-// assert!(!verify_dlog(&evaluation, &coeff, &DlogProof(c,(wrong_a, z))));
-// let wrong_c = G1Affine::generate_scalar(&mut csprng);
-// assert!(!verify_dlog(&evaluation, &coeff, &DlogProof(wrong_c,(a, z))));
-// let wrong_z = G1Affine::generate_scalar(&mut csprng);
-// assert!(!verify_dlog(&evaluation, &coeff, &DlogProof(c, (a, wrong_z))));
-//
-// }
