2877f6186e008449a3399d7f9a68df29c276d581
Bassel Mannaa <bm@concordium.com>
2019-07-11 16:44:14 +0200

    new id provider to reflect changes in the sig scheme

--- rust-src/id/src/identity_provider.rs
+++ rust-src/id/src/identity_provider.rs
@@ -4,7 +4,7 @@ use elgamal::cipher::Cipher;
 use pedersen_scheme::commitment::Commitment as PedersenCommitment;
 use ps_sig;
 use rand::*;
-use sigma_protocols::{com_enc_eq::*, com_eq_different_groups::*, dlog::*};
+use sigma_protocols::{com_enc_eq::*, com_eq_different_groups::*, dlog::*, com_eq::*};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Declined(pub Reason);
@@ -24,9 +24,16 @@ pub fn verify_credentials<
     context: Context<P, C>,
     ip_secret_key: &ps_sig::SecretKey<P>,
 ) -> Result<ps_sig::Signature<P>, Declined> {
+    let comm_sc_params = CommitmentParams((
+        context.commitment_key_sc.0[0],
+        context.commitment_key_sc.1,
+    ));
+
     let b_1 = verify_knowledge_of_id_cred_sec::<P::G_1>(
         &context.dlog_base,
+        &comm_sc_params,
         &pre_id_obj.id_cred_pub_ip,
+        &pre_id_obj.cmm_sc,
         &pre_id_obj.pok_sc,
     );
     if !b_1 {
@@ -57,6 +64,7 @@ pub fn verify_credentials<
     let message: ps_sig::UnknownMessage<P> = compute_message(
         &pre_id_obj.id_cred_pub_ip,
         &pre_id_obj.cmm_prf,
+        &pre_id_obj.cmm_sc,
         &pre_id_obj.alist,
         &context.ip_info.ip_verify_key,
     );
@@ -67,13 +75,14 @@ pub fn verify_credentials<
 fn compute_message<P: Pairing, AttributeType: Attribute<P::ScalarField>>(
     id_cred_pub: &P::G_1,
     cmm_prf: &PedersenCommitment<P::G_1>,
+    cmm_sc: &PedersenCommitment<P::G_1>,
     att_list: &AttributeList<P::ScalarField, AttributeType>,
     ps_public_key: &ps_sig::PublicKey<P>,
 ) -> ps_sig::UnknownMessage<P> {
     //TODO: handle the errors
     let variant = P::G_1::scalar_from_u64(att_list.variant as u64).unwrap();
     let expiry = P::G_1::scalar_from_u64(att_list.expiry.timestamp() as u64).unwrap();
-    let mut message = id_cred_pub.clone();
+    let mut message = cmm_sc.0;
     message = message.plus_point(&cmm_prf.0);
     let att_vec = &att_list.alist;
     let n = att_vec.len();
@@ -91,10 +100,14 @@ fn compute_message<P: Pairing, AttributeType: Attribute<P::ScalarField>>(
 
 fn verify_knowledge_of_id_cred_sec<C: Curve>(
     base: &C,
+    cmm_params: &CommitmentParams<C>,
     pk: &C,
-    proof: &DlogProof<C>,
+    commitment: &PedersenCommitment<C>,
+    proof: &ComEqProof<C>,
 ) -> bool {
-    verify_dlog(base, &pk, proof)
+    let PedersenCommitment(c) = commitment;
+    let CommitmentParams((h, g)) = cmm_params;
+    verify_com_eq(&(vec![*c],*pk),&(*h,*g, vec![*base]), &proof)
 }
 
 fn verify_vrf_key_data<C_1: Curve, C_2: Curve<Scalar = C_1::Scalar>>(
