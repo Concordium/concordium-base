012899ec3124a0d90afdae966cf3982af0d3a9a6
Nikolaj Friis Knudsen <nk@concordium.com>
2019-07-24 13:54:24 +0200

    Added rejected signature test, implemented aggregate signature

--- rust-src/aggregate_sig/src/main.rs
+++ rust-src/aggregate_sig/src/main.rs
@@ -47,12 +47,32 @@ fn scalar_from_message<P: Pairing>(m: &[u8]) -> P::ScalarField {
     }
 }
 
+// verifies a single signature. Should probably not be exposed, mainly used for testing.
+// should probably not take ownership of signature
 fn verify<P: Pairing>(message: &[u8], public_key: PublicKey<P>, signature: Signature<P>) -> bool {
     let scalar: P::ScalarField = scalar_from_message::<P>(message);
     let g1_hash = P::G_1::one_point().mul_by_scalar(&scalar);
     let left_hand_side = P::pair(signature.0, P::G_2::one_point());
     let right_hand_side = P::pair(g1_hash, public_key.0);
     left_hand_side == right_hand_side
+}
+
+// Signs the message using sk and aggregates the resulting signature to the supplied signature.
+fn aggregate_sig<P: Pairing>(
+    message: &[u8],
+    sk: SecretKey<P>,
+    signature: Signature<P>,
+) -> Signature<P> {
+    let my_signature = sign_message(&sk, message);
+    let aggregated_signature = signature.0.plus_point(&my_signature.0);
+    Signature(aggregated_signature)
+}
+
+fn verify_aggregate_sig<P: Pairing>(
+    messages: &[(&[u8], PublicKey<P>)],
+    signature: Signature<P>,
+) -> bool {
+    true // TODO
 }
 
 #[cfg(test)]
@@ -67,10 +87,19 @@ mod test {
         let sk = SecretKey::<Bls12>::generate(&mut csprng);
         let pk = PublicKey::from_secret(&sk);
 
+        // should verify correctly
         let m = [1u8, 64];
         let signature = sign_message(&sk, &m);
-        assert!(verify(&m, pk, signature))
+        assert!(verify(&m, pk, signature));
+
+        // should not verify!
+        let signature = sign_message(&sk, &m);
+        let sk2 = SecretKey::<Bls12>::generate(&mut csprng);
+        let pk2 = PublicKey::from_secret(&sk2);
+        assert!(!verify(&m, pk2, signature))
     }
+
+    // TODO: add more realistic test cases of sign/verify, including signatures that are rejected
 }
 
 fn main() {
