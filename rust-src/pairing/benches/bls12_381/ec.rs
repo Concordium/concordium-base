d6e2fe3eca4319fdb470140f63166ae99c334a04
Nikolaj Friis Knudsen <nk@concordium.com>
2019-08-24 00:41:34 +0200

    Refactor bench

--- rust-src/pairing/benches/bls12_381/ec.rs
+++ rust-src/pairing/benches/bls12_381/ec.rs
@@ -60,36 +60,28 @@ mod g1 {
         });
     }
 
-    #[bench]
-    fn bench_g1_hash_to_group_proj(b: &mut ::test::Bencher) {
-        const SAMPLES: usize = 1000;
+    macro_rules! bench_hashing {
+        ($benchname:ident, $curveimpl:path, $curvetype:path) => {
+            #[bench]
+            fn $benchname(b: &mut ::test::Bencher) {
+                const SAMPLES: usize = 1000;
 
-        let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+                let mut rng =
+                    XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
 
-        let v: Vec<[u8; 32]> = (0..SAMPLES).map(|_| rng.gen::<[u8; 32]>()).collect();
+                let v: Vec<[u8; 32]> = (0..SAMPLES).map(|_| rng.gen::<[u8; 32]>()).collect();
 
-        let mut count = 0;
-        b.iter(|| {
-            let _ = <G1 as CurveProjective>::hash_to_group_element(&v[count]);
-            count = (count + 1) % SAMPLES;
-        });
+                let mut count = 0;
+                b.iter(|| {
+                    let _ = <$curveimpl as $curvetype>::hash_to_group_element(&v[count]);
+                    count = (count + 1) % SAMPLES;
+                });
+            }
+        };
     }
 
-    #[bench]
-    fn bench_g1_hash_to_group_affine(b: &mut ::test::Bencher) {
-        const SAMPLES: usize = 1000;
-
-        let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
-
-        let v: Vec<[u8; 32]> = (0..SAMPLES).map(|_| rng.gen::<[u8; 32]>()).collect();
-
-        let mut count = 0;
-        b.iter(|| {
-            let _ = <G1Affine as CurveAffine>::hash_to_group_element(&v[count]);
-            count = (count + 1) % SAMPLES;
-        });
-    }
-
+    bench_hashing!(bench_g1_hash_to_group_projective, G1, CurveProjective);
+    bench_hashing!(bench_g1_hash_to_group_affine, G1Affine, CurveAffine);
 }
 
 mod g2 {
