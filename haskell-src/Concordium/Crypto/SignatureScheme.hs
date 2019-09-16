c689681c7ea20e94e596278875fe217b505de960
Aleš Bizjak <ab@concordium.com>
2019-09-16 18:28:26 +0200

    Disable the CL scheme since it is unimplemented.

--- haskell-src/Concordium/Crypto/SignatureScheme.hs
+++ haskell-src/Concordium/Crypto/SignatureScheme.hs
@@ -31,20 +31,18 @@ newtype Signature = Signature ShortByteString
     deriving FromJSON via Short65K
     deriving ToJSON via Short65K
 
-data SchemeId = Ed25519 | CL
+data SchemeId = Ed25519
     deriving (Eq, Show)
 
 instance ToJSON SchemeId where
   toJSON Ed25519 = object [pack "schemeId" .= "Ed25519"]
-  toJSON CL = object [pack "schemeId" .= "CL"]
 
 instance FromJSON SchemeId where
   parseJSON v = do
     name <- parseJSON v
     case name of
       "Ed25519" -> return Ed25519
-      "CL" -> return CL
-      _ -> fail "e"
+      err -> fail err
 
 data KeyPair = KeyPair {
       signKey :: !SignKey,
@@ -64,12 +62,10 @@ instance Enum SchemeId where
     toEnum n = case toScheme (fromIntegral n) of 
                  Just x -> x
                  Nothing -> errorWithoutStackTrace "SchemeId.toEnum: bad argument"
-    fromEnum CL = 0
     fromEnum Ed25519 = 1
 
 toScheme :: Word8 -> Maybe SchemeId
-toScheme n | n == 0 = Just CL
-           | n == 1 = Just Ed25519
+toScheme n | n == 1 = Just Ed25519
            | otherwise = Nothing
 
 data SignatureScheme = SigScheme {schemeId :: SchemeId,
