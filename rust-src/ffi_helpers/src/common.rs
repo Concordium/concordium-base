46bc74f3610ab4a780f37863247b82a864dd1e03
Javier Sagredo <js@concordium.com>
2020-01-14 09:56:29 +0100

    Fix the memory leak on byte slices

--- rust-src/ffi_helpers/src/common.rs
+++ rust-src/ffi_helpers/src/common.rs
@@ -1,11 +1,7 @@
-use libc::size_t;
-use std::slice;
-
 #[no_mangle]
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn free_array_len(ptr: *mut u8, len: size_t) {
-    let s = mut_slice_from_c_bytes!(ptr, len as usize);
+pub extern "C" fn free_array_len(ptr: *mut [u8]) {
     unsafe {
-        Box::from_raw(s.as_mut_ptr());
+        Box::from_raw(ptr);
     }
 }
