5d14bf3ce22fafc0f8bdc6583c5f6f04eec4bb83
Marianna Rapoport <mra@concordium.com>
2020-01-27 17:52:19 -0500

    Adding expiry field to TransactionHeader

--- haskell-tests/Types/TransactionGen.hs
+++ haskell-tests/Types/TransactionGen.hs
@@ -36,6 +36,7 @@ genTransactionHeader = do
   thPayloadSize <- PayloadSize . (`mod` 5000) <$> arbitrary
   thNonce <- Nonce <$> arbitrary
   thEnergyAmount <- Energy <$> arbitrary
+  thExpiry <- arbitrary
   return $ TransactionHeader{..}
 
 genBareTransaction :: Gen BareTransaction
