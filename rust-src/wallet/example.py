9da48115349c7927cf9e6ad46cf6013f462470fb
Aleš Bizjak <ab@concordium.com>
2020-02-09 13:10:35 +0100

    Minor cleanup.

--- rust-src/wallet/example.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import requests
-import json
-
-payload = {'id_request': open("id_request.json", 'r').read()}
-
-r = requests.get('http://localhost:8000/request_id', params=payload)
-
-print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
