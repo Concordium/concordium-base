4d86d2dfb96cb33cefc259786e5e31a51d139c47
Martin Karlsen Jensen <mkj@concordium.com>
2020-02-11 17:06:24 +0100

    Added Android gate

--- rust-src/mobile_wallet/src/android.rs
+++ rust-src/mobile_wallet/src/android.rs
@@ -1,27 +1,30 @@
 #![cfg(target_os = "android")]
 #![allow(non_snake_case)]
+#![allow(unused_assignments)]
 
 use jni::{
-    objects::JClass,
-    sys::{jbyte, jchar, jstring},
+    objects::{JClass, JString, JValue},
+    sys::{jint, jobject, jstring},
     JNIEnv,
 };
-use std::ffi::{CStr, CString};
-use wallet::{create_credential, create_id_request_and_private_data, free_response_string};
+use std::ffi::CString;
+use wallet::{create_credential_ext, create_id_request_and_private_data_ext};
 
 #[no_mangle]
-pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_id_request_and_private_data(
+pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1id_1request_1and_1private_1data(
     env: JNIEnv,
     _: JClass,
-    input: *const jbyte,
-    success: *mut jchar,
-) -> jstring {
-    let cstr = unsafe { CStr::from_ptr(input) };
+    input: JString,
+) -> jobject {
+    ndk_logger::init().unwrap();
+    let _str = env.get_string(input).expect("Could not create Java String");
+
+    let mut success = 127;
 
     let res = unsafe {
-        CString::from_raw(create_id_request_and_private_data(
-            cstr.as_ptr(),
-            success as *mut u8,
+        CString::from_raw(create_id_request_and_private_data_ext(
+            _str.as_ptr(),
+            &mut success,
         ))
     };
 
@@ -29,38 +32,68 @@ pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_i
         .new_string(res.to_str().unwrap())
         .expect("Could not create a Java string!");
 
-    output.into_inner()
+    let cls_name = "com/concordium/mobile_wallet_lib/ReturnValue";
+
+    let cls = env.find_class(cls_name).unwrap();
+
+    let ctr_sig = "(ILjava/lang/String;)V";
+
+    let method_id = env.get_method_id(cls, "<init>", ctr_sig).unwrap();
+
+    env.new_object_unchecked(cls, method_id, &[
+        JValue::Int(success as jint),
+        JValue::Object(*output),
+    ])
+    .unwrap()
+    .into_inner()
 }
 
 #[no_mangle]
-pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_credential(
+
+pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1credential(
     env: JNIEnv,
     _: JClass,
-    input: *const jbyte,
-    success: *mut jchar,
-) -> jstring {
-    let cstr = unsafe { CStr::from_ptr(input) };
+    input: JString,
+) -> jobject {
+    let _str = env.get_string(input).expect("Could not create Java String");
 
-    let res = unsafe { CString::from_raw(create_credential(cstr.as_ptr(), success as *mut u8)) };
+    let mut success: u8 = 127;
+
+    let res = unsafe {
+        let creds_ptr = create_credential_ext(_str.as_ptr(), &mut success);
+        CString::from_raw(creds_ptr)
+    };
 
     let output = env
         .new_string(res.to_str().unwrap())
         .expect("Could not create a Java string!");
 
-    output.into_inner()
+    let cls_name = "com/concordium/mobile_wallet_lib/ReturnValue";
+
+    let cls = env.find_class(cls_name).unwrap();
+
+    let ctr_sig = "(ILjava/lang/String;)V";
+
+    let method_id = env.get_method_id(cls, "<init>", ctr_sig).unwrap();
+
+    env.new_object_unchecked(cls, method_id, &[
+        JValue::Int(success as jint),
+        JValue::Object(*output),
+    ])
+    .unwrap()
+    .into_inner()
 }
 
 #[no_mangle]
-pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_free_response_string(
-    _: JNIEnv,
-    _: JClass,
-    ptr: *mut jbyte,
-) {
-    if ptr.is_null() {
-        return;
-    }
 
-    unsafe {
-        free_response_string(ptr);
-    }
+pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_link_1check(
+    env: JNIEnv,
+    _: JClass,
+    input: JString,
+) -> jstring {
+    let input = env.get_string(input).unwrap();
+
+    env.new_string(&format!("Hello, World {}!", input.to_str().unwrap()))
+        .unwrap()
+        .into_inner()
 }
