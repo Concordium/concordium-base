bc5e0966d86952b8d1e7b40a1946a07750ad382c
Martin Karlsen Jensen <mkj@concordium.com>
2020-02-13 09:35:31 +0100

    Fixed missing parameter to new wrapper call and use &str instead of String

--- mobile_wallet/src/android.rs
+++ mobile_wallet/src/android.rs
@@ -4,13 +4,16 @@
 
 use jni::{
     objects::{JClass, JString, JValue},
-    sys::{jint, jobject, jstring},
+    sys::{jint, jobject},
     JNIEnv,
 };
 use std::ffi::CString;
 use wallet::{create_credential_ext, create_id_request_and_private_data_ext};
 
 #[no_mangle]
+/// The JNI wrapper for the `create_id_request_and_private_data` method. 
+/// The `input` parameter must be a properly initalized `java.lang.String` that is non-null. 
+/// The input must be valid JSON according to specified format
 pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1id_1request_1and_1private_1data(
     env: JNIEnv,
     _: JClass,
@@ -20,28 +23,36 @@ pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1
         Ok(res_str) => res_str,
         Err(e) => {
             return wrap_return_tuple(
+                &env,
                 127,
-                format!("Could not read java.lang.String given as input due to {:?}", e),
+                &format!("Could not read java.lang.String given as input due to {:?}", e),
             )
         }
     };
+    
+    let mut success: u8 = 127;
+
     let cstr_res = unsafe {
         let unsafe_res_ptr =
             create_id_request_and_private_data_ext(input_str.as_ptr(), &mut success);
-        if (unsafe_res_ptr.is_null()) {
-            return wrap_return_tuple(127, "Pointer returned from crypto library was NULL");
+        if unsafe_res_ptr.is_null() {
+            return wrap_return_tuple(&env, 127, "Pointer returned from crypto library was NULL");
         }
         CString::from_raw(unsafe_res_ptr)
     };
+
     match cstr_res.to_str() {
-        Ok(str_ref) => wrap_return_tuple(success, str_ref),
+        Ok(str_ref) => wrap_return_tuple(&env, success, str_ref),
         Err(e) => {
-            wrap_return_tuple(127, &format!("Could not read CString from crypto library {:?}", e))
+            wrap_return_tuple(&env, 127, &format!("Could not read CString from crypto library {:?}", e))
         }
     }
 }
 
 #[no_mangle]
+/// The JNI wrapper for the `create_credential` method. 
+/// The `input` parameter must be a properly initalized `java.lang.String` that is non-null. 
+/// The input must be valid JSON according to specified format
 pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1credential(
     env: JNIEnv,
     _: JClass,
@@ -51,43 +62,53 @@ pub extern "system" fn Java_com_concordium_mobile_1wallet_1lib_WalletKt_create_1
         Ok(res_str) => res_str,
         Err(e) => {
             return wrap_return_tuple(
+                &env,
                 127,
-                format!("Could not read java.lang.String given as input due to {:?}", e),
+                &format!("Could not read java.lang.String given as input due to {:?}", e),
             )
         }
     };
+
     let mut success: u8 = 127;
     let cstr_res = unsafe {
         let unsafe_res_ptr = create_credential_ext(input_str.as_ptr(), &mut success);
-        if (unsafe_res_ptr.is_null()) {
-            return wrap_return_tuple(127, "Pointer returned from crypto library was NULL");
+        if unsafe_res_ptr.is_null() {
+            return wrap_return_tuple(&env, 127, "Pointer returned from crypto library was NULL");
         }
         CString::from_raw(unsafe_res_ptr)
     };
+
     match cstr_res.to_str() {
-        Ok(str_ref) => wrap_return_tuple(success, str_ref),
+        Ok(str_ref) => wrap_return_tuple(&env, success, str_ref),
         Err(e) => {
-            wrap_return_tuple(127, &format!("Could not read CString from crypto library {:?}", e))
+            wrap_return_tuple(&env, 127, &format!("Could not read CString from crypto library {:?}", e))
         }
     }
 }
 
-fn wrap_return_tuple(code: u8, message: &str) -> jobject {
+/// Method for wrapping the return value to Java
+/// We use a class in Java land for returning data from Rust
+/// If everything succeeds, then the `result` field will be 1 and the `output` field will contain the JSON response
+/// If something fails, then the `result` field will be different from 1, and the `output` field will contain the error message as a string 
+fn wrap_return_tuple(env: &JNIEnv, code: u8, message: &str) -> jobject {
     let class_name = "com/concordium/mobile_wallet_lib/ReturnValue";
-    let class = match env.find_class(cls_name) {
+    let class = match env.find_class(class_name) {
         Ok(clazz) => clazz,
-        Err(e) => {
+        Err(_e) => {
             return wrap_return_tuple(
+                &env,
                 127,
                 &format!("Can't find Java return tuple class {}", class_name),
             )
         }
     };
+
     let ctr_sig = "(ILjava/lang/String;)V";
     let jstr_value = match env.new_string(message) {
         Ok(new_str) => new_str,
         Err(e) => {
             return wrap_return_tuple(
+                &env,
                 127,
                 &format!(
                     "Can't wrap returned string from crypto library to a java.lang.String due to \
@@ -97,10 +118,12 @@ fn wrap_return_tuple(code: u8, message: &str) -> jobject {
             )
         }
     };
-    let method_id = match env.get_method_id(cls, "<init>", ctr_sig) {
+
+    let method_id = match env.get_method_id(class, "<init>", ctr_sig) {
         Ok(method_looked_up) => method_looked_up,
         Err(e) => {
             return wrap_return_tuple(
+                &env,
                 127,
                 &format!(
                     "Can't find constructor of (java.lang.Integer,java.lang.String) for class {} \
@@ -110,13 +133,15 @@ fn wrap_return_tuple(code: u8, message: &str) -> jobject {
             )
         }
     };
-    match env.new_object_unchecked(cls, method_id, &[
-        JValue::Int(success as jint),
-        JValue::Object(*output),
+
+    match env.new_object_unchecked(class, method_id, &[
+        JValue::Int(code as jint),
+        JValue::Object(*jstr_value),
     ]) {
         Ok(res) => res.into_inner(),
         Err(e) => {
             return wrap_return_tuple(
+                &env,
                 127,
                 &format!("Can't create new instance of {} due to {:?}", class_name, e),
             )
