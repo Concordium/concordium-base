197d45a985400e24f955e3338bd863033abcdbef
Javier Sagredo <js@concordium.com>
2020-04-03 15:59:37 +0200

    Updated with inlines and alterF

--- haskell-src/Concordium/Utils.hs
+++ haskell-src/Concordium/Utils.hs
@@ -6,7 +6,6 @@ module Concordium.Utils (
 import Data.HashMap.Strict as H
 import Data.Hashable
 import Data.Map.Strict as M
-import Data.Functor
 import Lens.Micro.Internal
 import Lens.Micro.Platform
 
@@ -18,21 +17,9 @@ class (Ixed m) => At' m where
   at' :: Index m -> Lens' m (Maybe (IxValue m))
 
 instance (Hashable k, Eq k) => At' (HashMap k v) where
-  at' = at'' H.lookup H.delete H.insert
+  at' = flip H.alterF
+  {-# INLINE at' #-}
 
 instance Ord k => At' (Map k v) where
-  at' = at'' M.lookup M.delete M.insert
-
-at'' :: Functor f =>
-       (k -> m -> Maybe v)       -- ^lookup function
-     -> (k -> m -> m)             -- ^delete function
-     -> (k -> v -> m -> m)         -- ^insert function
-     -> k                       -- ^index
-     -> (Maybe v -> f (Maybe v)) -- ^functor
-     -> m                       -- ^collection
-     -> f m
-at'' look del ins k f m = let !mv = look k m
-                              !v1 = f mv in
-    v1 <&> (\case
-               Nothing -> let !v = maybe m (const (del k m)) mv in v
-               Just v' -> let !v = ins k v' m in v)
+  at' = flip M.alterF
+  {-# INLINE at' #-}
