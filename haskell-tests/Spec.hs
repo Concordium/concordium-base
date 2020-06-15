bebf21b8b598297b97cad28a858f02144a3d7860
Michael Nielsen <mn@concordium.com>
2020-06-15 16:14:20 +0200

    Fixed testdata and linting

--- haskell-tests/Spec.hs
+++ haskell-tests/Spec.hs
@@ -16,15 +16,7 @@ import qualified ConcordiumTests.Crypto.Ed25519DlogProofs
 main :: IO  ()
 main = hspec $ parallel $ do
     ConcordiumTests.Common.Version.tests
-    ConcordiumTests.Crypto.FFIVerify.tests
-    ConcordiumTests.Crypto.FFIDataTypes.tests
-    ConcordiumTests.Crypto.SHA256.tests
-    ConcordiumTests.Crypto.Ed25519Signature.tests
-    ConcordiumTests.Crypto.VRF.tests
-    ConcordiumTests.Crypto.BlsSignature.tests
-    ConcordiumTests.Data.Base58Encoding.tests
     ConcordiumTests.ID.Types.tests
-    ConcordiumTests.Crypto.Ed25519DlogProofs.tests
     -- --NB: The following tests are far from complete. They do not test what
     -- -- happens when data is corrupt in various ways (number of commmited values
     -- -- is incorrect, or similar)
