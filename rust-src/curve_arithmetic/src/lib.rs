20c05ab3b537a094c92fd1443b471974a5231e91
Aleš Bizjak <ab@concordium.com>
2020-06-16 17:58:15 +0200

    Generic sigma proof interface with a generic And adapter.

--- rust-src/curve_arithmetic/src/lib.rs
+++ rust-src/curve_arithmetic/src/lib.rs
@@ -1,7 +1,7 @@
 pub(crate) mod bls12_381_g1hash;
 mod bls12_381_instance;
 mod curve_arithmetic;
-pub use curve_arithmetic::*;
+pub use crate::curve_arithmetic::*;
 
 pub mod secret_value;
 pub use secret_value::Value;
