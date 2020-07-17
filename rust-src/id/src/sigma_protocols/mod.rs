de0350f0d4f7321f8f00f3bdac0c31807c8dee73
Emil Madsen Bennedsen <eb@concordium.com>
2020-07-17 17:43:01 +0200

    Pushing comlin

--- rust-src/id/src/sigma_protocols/mod.rs
+++ rust-src/id/src/sigma_protocols/mod.rs
@@ -3,10 +3,10 @@ pub mod com_enc_eq;
 pub mod com_eq;
 pub mod com_eq_different_groups;
 pub mod com_eq_sig;
+pub mod com_lin;
 pub mod com_mult;
 pub mod common;
 pub mod dlog;
-pub mod com_lin;
 
 #[cfg(test)]
 pub mod sigma_test;
