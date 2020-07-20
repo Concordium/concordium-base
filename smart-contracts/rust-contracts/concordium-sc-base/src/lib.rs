dc2d5c364f502e056d7cb07981f05e238b8e57aa
Aleš Bizjak <ab@concordium.com>
2020-07-20 14:10:50 +0200

    More documentation of core rust library for smart contracts.

--- smart-contracts/rust-contracts/concordium-sc-base/src/lib.rs
+++ smart-contracts/rust-contracts/concordium-sc-base/src/lib.rs
@@ -1,3 +1,22 @@
+//! This library provides the core API that can be used to write smart contracts
+//! for the Concordium blockchain. It aims to provide safe wrappers around the
+//! core primitives exposed by the chain and accessible to smart contracts.
+//!
+//! By default the library will be linked with the
+//! [std](https://doc.rust-lang.org/std/) crate, the rust standard library,
+//! however to minimize code size this library supports toggling compilation
+//! with the `#![no_std]` attribute via the feature `std` which is enabled by
+//! default. Compilation without the `std` feature requires a nightly version of
+//! rust.
+//!
+//! To use this library without the `std` feature you have to disable it, which
+//! can be done, for example, as follows.
+//! ```
+//! [dependencies.concordium-sc-base]
+//! default-features = false
+//! ```
+//! In your project's `Cargo.toml` file.
+
 #![cfg_attr(not(feature = "std"), no_std, feature(alloc_error_handler))]
 
 #[cfg(not(feature = "std"))]
@@ -11,25 +30,23 @@ fn on_oom(_layout: alloc::alloc::Layout) -> ! { panic!() }
 #[panic_handler]
 fn abort_panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
 
-/// Provide some re-exports to make it easier to use the library.
-/// This should be expanded in the future.
+// Provide some re-exports to make it easier to use the library.
+// This should be expanded in the future.
 #[cfg(not(feature = "std"))]
 pub use core::result::*;
 
 #[cfg(not(feature = "std"))]
 pub use alloc::{vec, vec::Vec};
 
-extern crate wee_alloc;
-
 mod impls;
 mod prims;
-pub mod traits;
-pub mod types;
+mod traits;
+mod types;
 pub use prims::{events, internal};
+pub use traits::*;
 pub use types::*;
 
-pub use crate::traits::{Read, Seek, SeekFrom, Serialize, Write};
-
-// Use `wee_alloc` as the global allocator.
+extern crate wee_alloc;
+// Use `wee_alloc` as the global allocator to reduce code size.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
