610d7a2241aa7998a563c321a69f6881a1255fa4
Concordium developers <developers@concordium.com>
2020-07-30 15:45:34 +0200

    Changed amount parsing to combinators

--- haskell-tests/Types/TransactionSerializationSpec.hs
+++ haskell-tests/Types/TransactionSerializationSpec.hs
@@ -47,7 +47,7 @@ testBlockItem :: Property
 testBlockItem = forAll genBlockItem checkBlockItem
 
 checkAmountString :: Amount -> Property
-checkAmountString s = let ma = amountFromGTUString (amountToGTUString s)
+checkAmountString s = let ma = amountFromString (amountToString s)
                       in case ma of
                         Just a -> a === s
                         Nothing -> QC.property False
