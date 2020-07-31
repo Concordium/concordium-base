4590b9f4c12585cb1dc984115bf72b88ae118887
Concordium developers <developers@concordium.com>
2020-07-31 13:00:04 +0200

    Cleaned TX tests for amounts

--- haskell-tests/Types/TransactionSerializationSpec.hs
+++ haskell-tests/Types/TransactionSerializationSpec.hs
@@ -9,8 +9,6 @@ import Data.Serialize
 import qualified Data.ByteString as  BS
 
 import Types.TransactionGen
-import Types.CoreAllGen
-import Concordium.Types
 import Concordium.Types.Transactions
 
 groupIntoSize :: (Show a, Integral a) => a -> String
@@ -45,19 +43,8 @@ checkBlockItem bi =
 
 testBlockItem :: Property
 testBlockItem = forAll genBlockItem checkBlockItem
-
-checkAmountString :: Amount -> Property
-checkAmountString s = let ma = amountFromString (amountToString s)
-                      in case ma of
-                        Just a -> a === s
-                        Nothing -> QC.property False
-
-testAmountString :: Property
-testAmountString = forAll genAmount checkAmountString
-
 tests :: Spec
 tests = parallel $ do
-  specify "Amount string parsing" $ withMaxSuccess 10000 $ testAmountString
   specify "Transaction serialization with size = 100." $ withMaxSuccess 10000 $ testTransaction 100
   specify "Transaction serialization with size = 1000." $ withMaxSuccess 10000 $ testTransaction 1000
   specify "BlockItem serialization." $ withMaxSuccess 10000 $ testBlockItem
