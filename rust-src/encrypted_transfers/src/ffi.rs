82951a6691db060061bfe8b9099091c2f943a46f
Aleš Bizjak <ab@concordium.com>
2020-08-13 13:00:46 +0200

    Auxiliaries.

--- rust-src/encrypted_transfers/src/ffi.rs
+++ rust-src/encrypted_transfers/src/ffi.rs
@@ -6,7 +6,11 @@ use ffi_helpers::*;
 
 type Group = pairing::bls12_381::G1;
 
-pub extern "C" fn aggregate_encrypted_amounts(
+/// # Safety
+/// This function is safe if the pointers are all non-null, and produced
+/// by `Box::into_raw` for the input pointers.
+#[no_mangle]
+pub unsafe extern "C" fn aggregate_encrypted_amounts(
     first_high_ptr: *const Cipher<Group>,
     first_low_ptr: *const Cipher<Group>,
     second_high_ptr: *const Cipher<Group>,
@@ -14,14 +18,12 @@ pub extern "C" fn aggregate_encrypted_amounts(
     out_high_ptr: *mut *mut Cipher<Group>,
     out_low_ptr: *mut *mut Cipher<Group>,
 ) {
-    unsafe {
-        let first_high = from_ptr!(first_high_ptr);
-        let first_low = from_ptr!(first_low_ptr);
-        let second_high = from_ptr!(second_high_ptr);
-        let second_low = from_ptr!(second_low_ptr);
-        let out_high = first_high.combine(second_high);
-        let out_low = first_low.combine(second_low);
-        *out_high_ptr = Box::into_raw(Box::new(out_high));
-        *out_low_ptr = Box::into_raw(Box::new(out_low));
-    }
+    let first_high = from_ptr!(first_high_ptr);
+    let first_low = from_ptr!(first_low_ptr);
+    let second_high = from_ptr!(second_high_ptr);
+    let second_low = from_ptr!(second_low_ptr);
+    let out_high = first_high.combine(second_high);
+    let out_low = first_low.combine(second_low);
+    *out_high_ptr = Box::into_raw(Box::new(out_high));
+    *out_low_ptr = Box::into_raw(Box::new(out_low));
 }
