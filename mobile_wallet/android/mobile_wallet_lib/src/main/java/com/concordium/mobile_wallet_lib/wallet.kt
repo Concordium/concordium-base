afc9e1f1f314dc37ed1fee031b94b91d125143f7
Javier Sagredo <js@concordium.com>
2020-08-24 10:47:18 +0200

    Update also kotlin wrapper

--- mobile_wallet/android/mobile_wallet_lib/src/main/java/com/concordium/mobile_wallet_lib/wallet.kt
+++ mobile_wallet/android/mobile_wallet_lib/src/main/java/com/concordium/mobile_wallet_lib/wallet.kt
@@ -3,6 +3,12 @@ package com.concordium.mobile_wallet_lib
 external fun create_id_request_and_private_data(input: String) : ReturnValue
 external fun create_credential(input: String) : ReturnValue
 external fun create_transfer(input: String) : ReturnValue
+external fun create_encrypted_transfer(input: String) : ReturnValue
+external fun create_pub_to_sec_transfer(input: String) : ReturnValue
+external fun create_sec_to_pub_transfer(input: String) : ReturnValue
+external fun combine_encrypted_amounts(input1: String, input2: String) : ReturnValue
+external fun id_object_response(input: String) : ReturnValue
+external fun decrypt_encrypted_amount(input: String) : ReturnValue
 external fun check_account_address(input: String) : Boolean
 external fun link_check(input: String) : String
 
