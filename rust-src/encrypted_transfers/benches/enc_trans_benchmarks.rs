dfa41bc4791888d3064d232bde9ae3b3a8841b55
Emil Madsen Bennedsen <eb@concordium.com>
2020-09-03 10:31:22 +0200

    Updated benchmarks to also cover sec to pub transfers

--- rust-src/encrypted_transfers/benches/enc_trans_benchmarks.rs
+++ rust-src/encrypted_transfers/benches/enc_trans_benchmarks.rs
@@ -112,11 +112,94 @@ pub fn enc_trans_bench(c: &mut Criterion) {
     });
 }
 
+#[allow(non_snake_case)]
+pub fn sec_to_pub_bench(c: &mut Criterion) {
+    type SomeCurve = G1;
+    let mut csprng = thread_rng();
+    let sk: SecretKey<G1> = SecretKey::generate_all(&mut csprng);
+    let pk = PublicKey::from(&sk);
+    let s = csprng.gen(); // amount on account.
+
+    let a = csprng.gen_range(0, s); // amount to send
+
+    let m = 2; // 2 chunks
+    let n = 32;
+    let nm = n * m;
+
+    let context = GlobalContext::<SomeCurve>::generate_size(nm);
+    let generator = context.encryption_in_exponent_generator(); // h
+    let s_value = Value::from_u64(s);
+    let S = pk.encrypt_exponent_given_generator(&mut csprng, &s_value, generator);
+
+    let challenge_prefix = generate_challenge_prefix(&mut csprng);
+    let ro = RandomOracle::domain(&challenge_prefix);
+
+    let mut transcript = Transcript::new(&[]);
+    let index = csprng.gen();
+
+    let context_clone = context.clone();
+    let sk_clone = sk.clone();
+    c.bench_function("Create sec to pub transaction with proofs", move |b| {
+        b.iter(|| {
+            gen_sec_to_pub_trans(
+                &context_clone,
+                ro.split(),
+                &mut transcript,
+                &pk,
+                &sk_clone,
+                index,
+                &S,
+                Amount::from(s),
+                Amount::from(a),
+                &mut csprng,
+            )
+            .expect("Could not produce proof.");
+        })
+    });
+
+    let challenge_prefix = generate_challenge_prefix(&mut csprng);
+    let ro = RandomOracle::domain(&challenge_prefix);
+    let ro_copy = ro.split();
+
+    let mut transcript = Transcript::new(&[]);
+    let index = csprng.gen();
+    let transaction = gen_sec_to_pub_trans(
+        &context,
+        ro_copy,
+        &mut transcript,
+        &pk,
+        &sk,
+        index,
+        &S,
+        Amount::from(s),
+        Amount::from(a),
+        &mut csprng,
+    )
+    .expect("Could not produce proof.");
+    c.bench_function("Verify sec to pub transaction and proofs", move |b| {
+        b.iter(|| {
+            let ro = RandomOracle::domain(&challenge_prefix);
+            let mut transcript = Transcript::new(&[]);
+            assert_eq!(
+                verify_sec_to_pub_trans(
+                    &context,
+                    ro,
+                    &mut transcript,
+                    &transaction,
+                    &pk,
+                    &S,
+                ),
+                Ok(())
+            )
+        })
+    });
+}
+
 criterion_group! {
     name = elgamal_benches;
     config = Criterion::default().measurement_time(Duration::from_millis(1000)).sample_size(10);
     targets =
-        enc_trans_bench
+        enc_trans_bench, sec_to_pub_bench
 }
 
 criterion_main!(elgamal_benches);
