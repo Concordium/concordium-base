dbbc16a4d25a2321d4e91d3a882ad5b02d737bc9
Aleš Bizjak <ab@concordium.com>
2020-09-04 11:54:35 +0200

    Move Amount definition to Concordium.Common in crypto.

--- haskell-src/Concordium/Types.hs
+++ haskell-src/Concordium/Types.hs
@@ -9,13 +9,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# OPTIONS_GHC -Wall #-}
-module Concordium.Types (module Concordium.Types, AccountAddress(..), SchemeId, AccountVerificationKey) where
+module Concordium.Types (module Concordium.Types,
+                         AccountAddress(..),
+                         SchemeId, AccountVerificationKey,
+                         module Concordium.Common.Amount) where
 
 import GHC.Generics
 import Data.Data (Typeable, Data)
 
-import qualified Text.ParserCombinators.ReadP as RP
-
+import Concordium.Common.Amount
 import qualified Concordium.Crypto.BlockSignature as Sig
 import qualified Concordium.Crypto.SHA256 as Hash
 import qualified Concordium.Crypto.VRF as VRF
@@ -32,12 +34,10 @@ import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Short as BSS
 import Data.Bits
 import Data.Ratio
-import Data.Char (digitToInt,isDigit)
 
 import Data.Aeson as AE
 import Data.Aeson.TH
 
-import Data.Text (pack)
 import Data.Time
 import Data.Time.Clock.POSIX
 
@@ -240,74 +240,6 @@ isTimestampBefore ts ym =
         expiryMonth = if month == 12 then 1 else (month + 1) -- (month % 12) + 1
         expiryDay = fromGregorian expiryYear expiryMonth 1 -- unchecked, always valid
 
-
--- |Type representing the amount unit which is defined as the smallest
--- meaningful amount of GTU. This unit is 10^-6 GTU and denoted microGTU.
-type AmountUnit = Word64
-newtype Amount = Amount { _amount :: AmountUnit }
-    deriving (Show, Read, Eq, Ord, Enum, Bounded, Num, Integral, Real, Hashable) via AmountUnit
-
-instance S.Serialize Amount where
-  {-# INLINE get #-}
-  get = Amount <$> G.getWord64be
-  {-# INLINE put #-}
-  put (Amount v) = P.putWord64be v
-
-instance FromJSON Amount where
-  parseJSON = fmap Amount . withEmbeddedJSON "Amount" parseJSON
-
-instance ToJSON Amount where
-  toJSON = AE.String . pack . show . _amount
-
--- |Converts an amount to GTU decimal representation.
-amountToString :: Amount -> String
-amountToString (Amount amount) =
-  let
-    high = show $ amount `div` 1000000
-    low = show $ amount `mod` 1000000
-    pad = replicate (6 - length low) '0'
-  in
-    high ++ "." ++ pad ++ low
-
--- |Parse an amount from GTU decimal representation.
-amountFromString :: String -> Maybe Amount
-amountFromString s =
-    if length s == 0 || length parsed /= 1
-    then Nothing
-    else Just $ Amount (fst (head parsed))
-  where parsed = RP.readP_to_S amountParser s
-
--- |Parse a Word64 as a decimal number with scale 10^6
--- i.e. between 0 and 18446744073709.551615
-amountParser :: RP.ReadP Word64
-amountParser = decimalAmount RP.<++ noDecimalAmount
-  where
-    fitInWord64 v = v <= (toInteger (maxBound :: Word64))
-    noDecimalAmount = do
-      (_, num) <- readNumber True
-      let value = num * 1000000
-      if fitInWord64 value then return $ fromIntegral value
-      else RP.pfail
-    decimalAmount = do
-      (sLen, num) <- readNumber False
-      (mLen, mantissa) <- readNumber True
-      if sLen > 0 && mLen > 0 && mLen <= 6 then do
-        let value = num * 1000000 + (mantissa * 10 ^ (6-mLen))
-        if fitInWord64 value then return $ fromIntegral value
-        else RP.pfail
-      else RP.pfail
-
--- |Reads a number by reading digits, returning (#digits,number)
-readNumber :: Bool -> RP.ReadP (Int, Integer)
-readNumber eof = do
-    digits <- RP.manyTill readDigit terminal
-    return $ (length digits, (foldl (\acc v -> (acc*10+v)) 0 digits))
-  where terminal = if eof then RP.eof
-                   else (RP.char '.' >> return ())
-        readDigit = do
-          c <- RP.get
-          if isDigit c then return $ toInteger (digitToInt c)
-          else RP.pfail
 
 -- |Type representing a difference between amounts.
 newtype AmountDelta = AmountDelta { amountDelta :: Integer }
