cf74e5e10f4d296bef046843167bb11d66ad67a5
Thomas Dinsdale-Young <ty@concordium.com>
2020-09-10 17:48:55 +0200

    Documentation.

--- haskell-src/Concordium/Types/Updates.hs
+++ haskell-src/Concordium/Types/Updates.hs
@@ -1,5 +1,39 @@
 {-# LANGUAGE BangPatterns, DerivingStrategies, OverloadedStrings, ScopedTypeVariables, TemplateHaskell, StandaloneDeriving, DeriveTraversable, RankNTypes #-}
--- |Types for chain update instructions.
+-- |Types for chain update instructions, together with basic validation functions.
+-- For specification, see: https://concordium.gitlab.io/whitepapers/update-mechanism/main.pdf
+--
+-- The specification defines the following update types:
+--
+--   - authorization updates
+--   - parameter updates
+--   - protocol updates
+--   - emergency updates
+--
+-- Authorization updates alter the set of keys used to authorize chain updates.
+-- (Practically, they are a type of parameter update.)
+--
+-- Parameter updates update a chain parameter.
+-- Currently provided parameters are:
+--
+--   - election difficulty
+--   - Energy to Euro exchange rate
+--   - GTU to Euro exchange rate
+--
+-- Each parameter has an independent update queue.
+-- Sequence numbers for each different parameter are this independent.
+-- (Note, where two parameters are tightly coupled, such that one should
+-- not be changed independently of the other, then they should be combined
+-- as a single parameter.)
+--
+-- Protocol updates specify a new protocol version.
+-- The implementation should stop the current chain when a protocol update takes effect.
+-- If it supports the new protocol version, it should begin a new chain according to that protocol,
+-- and based on the state when the update took effect.
+-- (Currently, this is not implemented.)
+--
+-- Emergency updates are inherently outside the scope of the chain implementation itself.
+-- The chain only records the keys authorized for emergency updates, but does
+-- not support any kind of emergency update messages.
 module Concordium.Types.Updates where
 
 import qualified Data.Aeson as AE
@@ -27,37 +61,14 @@ import Concordium.Utils.Serialization
 import Concordium.Types
 import Concordium.Types.HashableTo
 
--- |Types of updates to the chain.
-data UpdateType
-    = UpdateAuthorization
-    -- ^Update the access structures that authorize updates
-    | UpdateProtocol
-    -- ^Update the chain protocol
-    | UpdateElectionDifficulty
-    -- ^Update the election difficulty
-    | UpdateEuroPerEnergy
-    -- ^Update the euro per energy exchange rate
-    | UpdateMicroGTUPerEuro
-    -- ^Update the microGTU per euro exchange rate
-    deriving (Eq, Ord, Show, Ix, Bounded, Enum)
-
-instance Serialize UpdateType where
-    put UpdateAuthorization = putWord8 0
-    put UpdateProtocol = putWord8 1
-    put UpdateElectionDifficulty = putWord8 2
-    put UpdateEuroPerEnergy = putWord8 3
-    put UpdateMicroGTUPerEuro = putWord8 4
-    get = getWord8 >>= \case
-        0 -> return UpdateAuthorization
-        1 -> return UpdateProtocol
-        2 -> return UpdateElectionDifficulty
-        3 -> return UpdateEuroPerEnergy
-        4 -> return UpdateMicroGTUPerEuro
-        n -> fail $ "invalid update type: " ++ show n
+--------------------
+-- * Authorizations
+--------------------
 
 -- |Key type for update authorization.
 type UpdatePublicKey = VerifyKey
 
+-- |Index of a key in an 'Authorizations'.
 type UpdateKeyIndex = Word16
 
 -- |Access structure for update authorization.
@@ -85,13 +96,9 @@ instance Serialize AccessStructure where
 checkKeySet :: AccessStructure -> Set.Set UpdateKeyIndex -> Bool
 checkKeySet AccessStructure{..} ks = Set.size (ks `Set.intersection` accessPublicKeys) >= fromIntegral accessThreshold
 
--- |Sequence number for updates of a given type.
-type UpdateSequenceNumber = Nonce
-
-minUpdateSequenceNumber :: UpdateSequenceNumber
-minUpdateSequenceNumber = minNonce
-
--- |Payload of an update to authorization.
+-- |The set of keys authorized for chain updates, together with
+-- access structures determining which keys are authorized for
+-- which update types. This is the payload of an update to authorization.
 data Authorizations = Authorizations {
         asKeys :: !(Vec.Vector UpdatePublicKey),
         -- |New emergency keys
@@ -180,6 +187,38 @@ instance AE.ToJSON Authorizations where
                     "threshold" AE..= accessThreshold
                 ]
 
+----------------------
+-- * Protocol updates
+----------------------
+
+-- |Types of updates to the chain.
+data UpdateType
+    = UpdateAuthorization
+    -- ^Update the access structures that authorize updates
+    | UpdateProtocol
+    -- ^Update the chain protocol
+    | UpdateElectionDifficulty
+    -- ^Update the election difficulty
+    | UpdateEuroPerEnergy
+    -- ^Update the euro per energy exchange rate
+    | UpdateMicroGTUPerEuro
+    -- ^Update the microGTU per euro exchange rate
+    deriving (Eq, Ord, Show, Ix, Bounded, Enum)
+
+instance Serialize UpdateType where
+    put UpdateAuthorization = putWord8 0
+    put UpdateProtocol = putWord8 1
+    put UpdateElectionDifficulty = putWord8 2
+    put UpdateEuroPerEnergy = putWord8 3
+    put UpdateMicroGTUPerEuro = putWord8 4
+    get = getWord8 >>= \case
+        0 -> return UpdateAuthorization
+        1 -> return UpdateProtocol
+        2 -> return UpdateElectionDifficulty
+        3 -> return UpdateEuroPerEnergy
+        4 -> return UpdateMicroGTUPerEuro
+        n -> fail $ "invalid update type: " ++ show n
+
 -- |Payload of a protocol update.
 data ProtocolUpdate = ProtocolUpdate {
         -- |A brief message about the update
@@ -237,6 +276,21 @@ instance AE.FromJSON ProtocolUpdate where
             unless (BS.null garbage) $ fail "Unable to parse \"specificationAuxiliaryData\" as Base-16"
             return ProtocolUpdate{..}
 
+-------------------------
+-- * Update Instructions
+-------------------------
+
+-- |Sequence number for updates of a given type.
+type UpdateSequenceNumber = Nonce
+
+-- |Lowest 'UpdateSequenceNumber'.
+minUpdateSequenceNumber :: UpdateSequenceNumber
+minUpdateSequenceNumber = minNonce
+
+-- |The header for an update instruction, consisting of the
+-- sequence number, effective time, expiry time (timeout),
+-- and payload size.  This structure is the same for all
+-- update payload types.
 data UpdateHeader = UpdateHeader {
         updateSeqNumber :: UpdateSequenceNumber,
         updateEffectiveTime :: TransactionTime,
@@ -258,12 +312,18 @@ instance Serialize UpdateHeader where
         updatePayloadSize <- get
         return UpdateHeader{..}
 
+-- |The payload of an update instruction.
 data UpdatePayload
     = AuthorizationUpdatePayload !Authorizations
+    -- ^Update the authorization structure
     | ProtocolUpdatePayload !ProtocolUpdate
+    -- ^Update the protocol
     | ElectionDifficultyUpdatePayload !ElectionDifficulty
+    -- ^Update the election difficulty parameter
     | EuroPerEnergyUpdatePayload !ExchangeRate
+    -- ^Update the euro-per-energy parameter
     | MicroGTUPerEuroUpdatePayload !ExchangeRate
+    -- ^Update the microGTU-per-euro parameter
     deriving (Eq, Show)
 
 instance Serialize UpdatePayload where
@@ -285,6 +345,7 @@ $(deriveJSON defaultOptions{
     }
     ''UpdatePayload)
 
+-- |Determine the 'UpdateType' associate with an 'UpdatePayload'.
 updateType :: UpdatePayload -> UpdateType
 updateType AuthorizationUpdatePayload{} = UpdateAuthorization
 updateType ProtocolUpdatePayload{} = UpdateProtocol
@@ -301,6 +362,8 @@ checkUpdateAuthorizationKeys Authorizations{..} (ElectionDifficultyUpdatePayload
 checkUpdateAuthorizationKeys Authorizations{..} (EuroPerEnergyUpdatePayload _) ks = checkKeySet asParamEuroPerEnergy ks
 checkUpdateAuthorizationKeys Authorizations{..} (MicroGTUPerEuroUpdatePayload _) ks = checkKeySet asParamMicroGTUPerEuro ks
 
+-- |Signatures on an update instruction.
+-- The serialization of 'UpdateInstructionSignatures' is uniquely determined.
 newtype UpdateInstructionSignatures = UpdateInstructionSignatures {updateInstructionSignatures :: Map.Map UpdateKeyIndex Signature}
     deriving newtype (Eq, Show)
 
@@ -311,6 +374,21 @@ instance Serialize UpdateInstructionSignatures where
         sz <- getWord16be
         when (sz == 0) $ fail "signatures must not be empty"
         UpdateInstructionSignatures <$> getSafeSizedMapOf sz get get
+
+-- |Hash of an update instruction, as used for signing.
+newtype UpdateInstructionSignHashV0 = UpdateInstructionSignHashV0 {v0UpdateInstructionSignHash :: SHA256.Hash}
+  deriving newtype (Eq, Ord, Show, Serialize, AE.ToJSON, AE.FromJSON, AE.FromJSONKey, AE.ToJSONKey, Read, Hashable)
+
+-- |Alias for 'UpdateInstructionSignHashV0'.
+type UpdateInstructionSignHash = UpdateInstructionSignHashV0
+
+-- |Construct an 'UpdateInstructionSignHash' from the serialized header and payload of
+-- an update instruction.
+makeUpdateInstructionSignHash ::
+    ByteString
+    -- ^Serialized update instruction header and payload
+    -> UpdateInstructionSignHash
+makeUpdateInstructionSignHash body = UpdateInstructionSignHashV0 (SHA256.hash body)
 
 -- |An update instruction.
 -- The header must have the correct length of the payload, and the
@@ -337,6 +415,10 @@ instance Serialize UpdateInstruction where
         put uiPayload
         put uiSignatures
 
+--------------------------------------
+-- * Constructing Update Instructions
+--------------------------------------
+
 -- |An update instruction without signatures and payload length.
 -- This is used for constructing an update instruction.
 data RawUpdateInstruction = RawUpdateInstruction {
@@ -346,6 +428,7 @@ data RawUpdateInstruction = RawUpdateInstruction {
         ruiPayload :: UpdatePayload
     } deriving (Eq, Show)
 
+-- |Serialize a 'RawUpdateInstruction'; used for signing.
 putRawUpdateInstruction :: Putter RawUpdateInstruction
 putRawUpdateInstruction RawUpdateInstruction{..} = do
         put ruiSeqNumber
@@ -355,18 +438,8 @@ putRawUpdateInstruction RawUpdateInstruction{..} = do
     where
         putPayloadSize l = put (fromIntegral l :: PayloadSize)
 
-newtype UpdateInstructionSignHashV0 = UpdateInstructionSignHashV0 {v0UpdateInstructionSignHash :: SHA256.Hash}
-  deriving newtype (Eq, Ord, Show, Serialize, AE.ToJSON, AE.FromJSON, AE.FromJSONKey, AE.ToJSONKey, Read, Hashable)
-
-type UpdateInstructionSignHash = UpdateInstructionSignHashV0
-
-makeUpdateInstructionSignHash ::
-    ByteString
-    -- ^Serialized update instruction header and payload
-    -> UpdateInstructionSignHash
-makeUpdateInstructionSignHash body = UpdateInstructionSignHashV0 (SHA256.hash body)
-
-
+-- |Produce a signature for an update instruction with the given 'UpdateInstructionSignHash'
+-- using the supplied keys.
 signUpdateInstruction :: UpdateInstructionSignHash -> Map.Map UpdateKeyIndex KeyPair -> UpdateInstructionSignatures
 signUpdateInstruction sh = UpdateInstructionSignatures . fmap (\kp -> sign kp (encode sh))
 
@@ -385,6 +458,10 @@ makeUpdateInstruction rui@RawUpdateInstruction{..} keys = UpdateInstruction {
     where
         uiSignHash = makeUpdateInstructionSignHash (runPut $ putRawUpdateInstruction rui)
         uiSignatures = signUpdateInstruction uiSignHash keys
+
+----------------
+-- * Validation
+----------------
 
 -- |Check if the signatures on an 'UpdateInstruction' are valid with respect
 -- to the given 'Authorizations'. This does not check if the keys are the
