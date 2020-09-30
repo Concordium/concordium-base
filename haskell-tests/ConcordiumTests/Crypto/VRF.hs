c452abb5b4bd76863ca9f9e60c8b6fd0e7109b9b
Aleš Bizjak <ab@concordium.com>
2020-09-30 09:13:57 +0200

    A bit of cleanup.

--- haskell-tests/ConcordiumTests/Crypto/VRF.hs
+++ haskell-tests/ConcordiumTests/Crypto/VRF.hs
@@ -6,7 +6,6 @@ import qualified Concordium.Crypto.VRF as VRF
 
 import Data.Serialize
 import qualified Data.ByteString as BS
-import Test.QuickCheck.Monadic
 import Test.QuickCheck
 import Test.Hspec
 import qualified Data.Aeson as AE
@@ -27,18 +26,18 @@ testSerializeKeyPair :: Property
 testSerializeKeyPair = property $ \(kp :: VRF.KeyPair) -> Right kp === runGet get (runPut $ put kp)
 
 testSerializeProof :: Property
-testSerializeProof = property $ \kp doc -> monadicIO $ do
-        let pf = VRF.prove kp (BS.pack doc)
-        return $ Right pf === runGet get (runPut $ put pf)
+testSerializeProof = property $ \kp doc ->
+  let pf = VRF.prove kp (BS.pack doc)
+  in Right pf === runGet get (runPut $ put pf)
 
 testGenVerifyKey :: Property
 testGenVerifyKey = property $ \kp -> VRF.verifyKey (VRF.publicKey kp)
 
 testProveVerify :: Property
-testProveVerify = property $ \kp doc0 -> monadicIO $ do
-                    let doc = BS.pack doc0
-                    let pf = VRF.prove kp doc
-                    return $ VRF.verify (VRF.publicKey kp) doc pf
+testProveVerify = property $ \kp doc0 ->
+  let doc = BS.pack doc0
+      pf = VRF.prove kp doc
+  in VRF.verify (VRF.publicKey kp) doc pf
 
 testPublicKeyOrd :: Property
 testPublicKeyOrd = property $ \(kp1, kp2) ->
@@ -51,36 +50,36 @@ testPublicKeyOrd = property $ \(kp1, kp2) ->
   in k1 .&&. k2 .&&. k3
 
 testProofOrd :: Property
-testProofOrd = property $ \kp doc0 -> monadicIO $ do
+testProofOrd = property $ \kp doc0 doc1 ->
   let doc = BS.pack doc0
-  let pf1 = VRF.prove kp doc
-  let pf2 = VRF.prove kp doc
-  let k1 = case compare pf1 pf2 of
+      doc' = BS.pack doc1
+      pf1 = VRF.prove kp doc
+      pf2 = VRF.prove kp doc'
+      k1 = case compare pf1 pf2 of
              LT -> property True
              GT -> property True
              EQ -> pf1 === pf2
-  let k2 = compare pf1 pf1 === EQ
-  let k3 = compare pf2 pf2 === EQ
-  return (k1 .&&. k2 .&&. k3)
+      k2 = compare pf1 pf1 === EQ
+      k3 = compare pf2 pf2 === EQ
+  in (k1 .&&. k2 .&&. k3)
 
 testProveDeterministic :: Property
-testProveDeterministic = property $ \kp doc0 -> monadicIO $ do
-        let doc = BS.pack doc0
-        let pf1 = VRF.prove kp doc
-        let pf2 = VRF.prove kp doc
-        return $ pf1 === pf2
+testProveDeterministic = property $ \kp doc0 ->
+  let doc = BS.pack doc0
+      pf1 = VRF.prove kp doc
+      pf2 = VRF.prove kp doc
+  in pf1 === pf2
 
 -- Generate a bunch of proofs and convert them to hashes. They should be
 -- different. This is really regression testing the FFI bug where a generated
 -- proof was not valid.
 stressTest :: Property
-stressTest = property $ \kp doc0 doc1 -> monadicIO $
-        let doc = BS.pack doc0
-            doc' = BS.pack doc1 in do
-              let pf1 = VRF.prove kp doc
-              let pf2 = VRF.prove kp doc'
-              return $ (VRF.proofToHash pf1 == VRF.proofToHash pf2) == (doc == doc')
-
+stressTest = property $ \kp doc0 doc1 ->
+  let doc = BS.pack doc0
+      doc' = BS.pack doc1
+      pf1 = VRF.prove kp doc
+      pf2 = VRF.prove kp doc'
+  in (VRF.proofToHash pf1 == VRF.proofToHash pf2) == (doc == doc')
 
 tests :: Spec
 tests = describe "Concordium.Crypto.VRF" $ do
