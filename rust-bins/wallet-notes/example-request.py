f6158ca10cc302b45c7634ba9fd208b593cd6533
Aleš Bizjak <ab@concordium.com>
2020-11-03 10:56:31 +0100

    Cleanup pending on finalization and error.

--- rust-bins/wallet-notes/example-request.py
+++ rust-bins/wallet-notes/example-request.py
@@ -8,16 +8,14 @@ inputPio = json.loads(open(sys.argv[1], 'r').read())
 
 input = json.dumps({'idObjectRequest': inputPio})
 
-print(input)
-
 payload = {'state': input,
            'redirect_uri': 'http://localhost:1234'
           }
 
 r = requests.get('http://localhost:8100/api/identity', params=payload)
 
-print(r)
-
+print(r.headers)
+print(r.status_code)
 print(r.text)
 
 print(json.dumps(json.loads(r.text), indent=4, sort_keys=True))
