a913223bce4ec16b3e0079ae39fb2e94c57588ce
Aleš Bizjak <ab@concordium.com>
2020-11-18 11:13:23 +0100

    Payload serialization checks more properties now, so tests need updating.

--- haskell-tests/Types/PayloadSerializationSpec.hs
+++ haskell-tests/Types/PayloadSerializationSpec.hs
@@ -109,17 +109,22 @@ genPayload = oneof [genDeployModule,
           n <- choose (0,1000)
           BS.pack <$> vector n
 
-        genText = do
-          n <- choose (0,1000)
-          Text.pack <$> vector n
+        -- These generators name contracts as numbers to make sure the names are valid.
+        genInitName :: Gen InitName
+        genInitName =
+          InitName . Text.pack . ("init_" ++) . show <$> (arbitrary :: Gen Word)
 
-        genInitName = InitName <$> genText
-        genReceiveName = ReceiveName <$> genText
+        genReceiveName :: Gen ReceiveName
+        genReceiveName = do
+          contract <- show <$> (arbitrary :: Gen Word)
+          receive <- show <$> (arbitrary :: Gen Word)
+          return . ReceiveName . Text.pack $ receive ++ "." ++ contract
+
         genParameter = do
           n <- choose (0,1000)
           Parameter . BSS.pack <$> vector n
 
-        genDeployModule = DeployModule <$> (WasmModule <$> arbitrary <*> genByteString)
+        genDeployModule = DeployModule <$> (WasmModule 0 <$> genByteString)
 
         genInit = do
           icAmount <- Amount <$> arbitrary
