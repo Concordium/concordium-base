e1b925869d3af92fd1742031f52409a5658b7d5a
Javier Sagredo <js@concordium.com>
2020-11-19 15:03:40 +0100

    Rework Setup.hs and stack.yaml to match the static build process

--- Setup.hs
+++ Setup.hs
@@ -4,106 +4,62 @@ import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
 import Distribution.System
+import Distribution.Verbosity
+import Control.Monad
 import System.Directory
 import System.Environment
 import Data.Maybe
--- copyExtLib :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
--- copyExtLib args flags pkg_descr lbi = do
---     let libPref = dynlibdir (configInstallDirs . configFlags $ lbi)
---     let verbosity = fromFlag $ copyVerbosity flags
-    
---     -- rawSystemExit verbosity "cp" ["rust-src/target/release/libecvrf.so",
--- --                                  "rust-src/target/release/libeddsa_ed25519.so",
--- --                                  "rust-src/target/release/libsha_2.so",
--- --                                  libPref]
+
+concordiumLibs :: [String]
+concordiumLibs = [ "ecvrf"
+                 , "sha_2"
+                 , "eddsa_ed25519"
+                 , "ffi_helpers"
+                 , "id"
+                 , "aggregate_sig"
+                 , "encrypted_transfers"
+                 ]
+
+type WithEnvAndVerbosity = [(String, String)] -> Verbosity -> IO ()
+
+-- |In linux, we will produce two kind of builds:
+-- - Static with musl: the rust libraries will only build static artifacts. Intended to be used inside alpine to produce a static binary.
+-- - With glibc: Normal compilation. Rust will produce static and dynamic artifacts.
+linuxBuild :: Bool -> WithEnvAndVerbosity
+linuxBuild True env verbosity = do
+  noticeNoWrap verbosity "Static linking."
+  rawSystemExitWithEnv verbosity "cargo" ["build", "--release", "--manifest-path", "rust-src/Cargo.toml", "--target", "x86_64-unknown-linux-musl"]
+    (("CARGO_NET_GIT_FETCH_WITH_CLI", "true") : ("RUSTFLAGS", "-C target-feature=-crt-static") : env)
+  -- as I have not been able to modify the extra-lib-dirs based on the flag assignment, for now we will copy the musl libs onto the normal target libraries
+  let copyLib lib = rawSystemExit verbosity "cp" ["rust-src/target/x86_64-unknown-linux-musl/release/lib" ++ lib ++ ".a", "rust-src/target/release"]
+  mapM_ copyLib concordiumLibs
+linuxBuild False env verbosity = do
+  noticeNoWrap verbosity "Dynamic linking."
+  rawSystemExitWithEnv verbosity "cargo" ["build", "--release", "--manifest-path", "rust-src/Cargo.toml"] (("CARGO_NET_GIT_FETCH_WITH_CLI", "true") : env)
+
+-- |I honestly have no idea about what we should do on windows.
+windowsBuild :: WithEnvAndVerbosity
+windowsBuild env verbosity = do
+  rawSystemExitWithEnv verbosity "cargo" ["build", "--release", "--manifest-path", "rust-src/Cargo.toml"] (("CARGO_NET_GIT_FETCH_WITH_CLI", "true")  : env)
+
+-- |On Mac, we will delete the dynamic artifacts if we want to create a static binary.
+osxBuild :: Bool -> WithEnvAndVerbosity
+osxBuild static env verbosity = do
+  rawSystemExitWithEnv verbosity "cargo" ["build", "--release", "--manifest-path", "rust-src/Cargo.toml"] (("CARGO_NET_GIT_FETCH_WITH_CLI", "true")  : env)
+  when static $ do
+      let deleteDynLib lib = rawSystemExit verbosity "rm" ["-f", "rust-src/target/release/lib" ++ lib ++ ".dylib"]
+      mapM_ deleteDynLib concordiumLibs
+
 makeRust :: Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-makeRust args flags _ lbi = do
+makeRust _ flags _ lbi = do
     let verbosity = fromFlag $ configVerbosity flags
-        forcedStaticLinkingFlag = lookupFlagAssignment (mkFlagName "forced-static-linking") (flagAssignment lbi)
-        hasMusl = lookupFlagAssignment (mkFlagName "link-with-musl") (flagAssignment lbi)
-    noticeNoWrap verbosity $ "Forced static linking: " ++ maybe "Not set" show forcedStaticLinkingFlag
-    noticeNoWrap verbosity $ "Linking aginst Musl: " ++ maybe "Not set" show hasMusl
+        staticLinking = Just True == lookupFlagAssignment (mkFlagName "static") (flagAssignment lbi)
+        build = case buildOS of
+          Windows -> windowsBuild
+          OSX -> osxBuild staticLinking
+          Linux -> linuxBuild staticLinking
     env <- getEnvironment
-    rawSystemExitWithEnv verbosity "cargo"
-        ["build", "--release", "--manifest-path", "rust-src/Cargo.toml"]
-        (("CARGO_NET_GIT_FETCH_WITH_CLI", "true") : env)
-    -- NB: This list must be updated when new libraries are added to dependencies.
-    let libs = [
-                "ecvrf",
-                "sha_2",
-                "eddsa_ed25519",
-                "ffi_helpers",
-                "id",
-                "aggregate_sig",
-                "encrypted_transfers"
-            ]
-    rawSystemExit verbosity "mkdir" ["-p", "./lib"]
-    -- On Windows, copy the static libraries and DLLs. (The DLLs should not be used by the
-    -- linker, but seem to be needed sometimes by TemplateHaskell at compile time.)
-    -- On other platforms, symlink the shared libraries.
-    case (buildOS, forcedStaticLinkingFlag, hasMusl) of
-        ( Windows, _, _ ) -> do
-            let copyLib lib = do
-                rawSystemExit verbosity "cp" ["rust-src/target/release/lib" ++ lib ++ ".a", "./lib/"]
-                rawSystemExit verbosity "cp" ["rust-src/target/release/" ++ lib ++ ".dll", "./lib/"]
-                notice verbosity $ "Copied " ++ lib ++ "."
-            notice verbosity "Copying libraries to ./lib"
-            mapM_ copyLib libs
-        ( OSX, Just False, _ ) -> do
-            let copyLib lib = do
-                  let source = "../rust-src/target/release/lib" ++ lib ++ ".dylib"
-                  let others = "./lib/lib" ++ lib ++ ".a"
-                  let target = "./lib/lib" ++ lib ++ ".dylib"
-                  rawSystemExit verbosity "rm" ["-f", others]
-                  rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-                  noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-                  noticeNoWrap verbosity $ "Removed: " ++ others
-            notice verbosity "Linking libraries to ./lib"
-            mapM_ copyLib libs
-        ( OSX, Just True, _ ) -> do
-            let copyLib lib = do
-                  let source = "../rust-src/target/release/lib" ++ lib ++ ".a"
-                  let others = "./lib/lib" ++ lib ++ ".dylib"
-                  let target = "./lib/lib" ++ lib ++ ".a"
-                  rawSystemExit verbosity "rm" ["-f", others]
-                  rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-                  noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-                  noticeNoWrap verbosity $ "Removed: " ++ others
-            notice verbosity "Linking libraries to ./lib"
-            mapM_ copyLib libs
-        ( Linux, _, Just True ) -> do
-            let copyLib lib = do
-                  let source = "../rust-src/target/x86_64-unknown-linux-musl/release/lib" ++ lib ++ ".a"
-                  let others = "./lib/lib" ++ lib ++ ".so"
-                  let target = "./lib/lib" ++ lib ++ ".a"
-                  rawSystemExit verbosity "rm" ["-f", others]
-                  rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-                  noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-                  noticeNoWrap verbosity $ "Removed: " ++ others
-            notice verbosity "Linking libraries to ./lib"
-            mapM_ copyLib libs
-        ( _, Just False, _ ) -> do
-            let copyLib lib = do
-                  let source = "../rust-src/target/release/lib" ++ lib ++ ".so"
-                  let others = "./lib/lib" ++ lib ++ ".a"
-                  let target = "./lib/lib" ++ lib ++ ".so"
-                  rawSystemExit verbosity "rm" ["-f", others]
-                  rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-                  noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-                  noticeNoWrap verbosity $ "Removed: " ++ others
-            notice verbosity "Linking libraries to ./lib"
-            mapM_ copyLib libs
-        ( _, Just True, _ ) -> do
-            let copyLib lib = do
-                  let source = "../rust-src/target/release/lib" ++ lib ++ ".a"
-                  let others = "./lib/lib" ++ lib ++ ".so"
-                  let target = "./lib/lib" ++ lib ++ ".a"
-                  rawSystemExit verbosity "rm" ["-f", others]
-                  rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-                  noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-            notice verbosity "Linking libraries to ./lib"
-            mapM_ copyLib libs
-    return ()
+    build env verbosity
 
 main = defaultMainWithHooks simpleUserHooks
   {
