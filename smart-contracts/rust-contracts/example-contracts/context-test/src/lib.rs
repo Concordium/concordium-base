0c5f0a4b4f6f3d16205addd263c7caf3084af6db
Aleš Bizjak <ab@concordium.com>
2020-12-08 08:54:55 +0100

    More testing of policies via an example smart contract.

--- smart-contracts/rust-contracts/example-contracts/context-test/src/lib.rs
+++ smart-contracts/rust-contracts/example-contracts/context-test/src/lib.rs
@@ -25,3 +25,36 @@ fn contract_init(
     }
     Ok(0)
 }
+
+#[init(contract = "context_test_2")]
+// can only be initialized
+fn contract_init_2(
+    ctx: &impl HasInitContext,
+    _amount: Amount,
+    _logger: &mut impl HasLogger,
+) -> InitResult<u8> {
+    if ctx.policies().len() != 1 {
+        return Ok(1);
+    }
+    for mut policy in ctx.policies() {
+        if policy.identity_provider() != 17 {
+            return Ok(2);
+        }
+        if policy.created_at() != policy.valid_to() {
+            return Ok(3);
+        }
+        let mut buf = [0u8; 31];
+        if let Some(p) = policy.next_item(&mut buf) {
+            if p.1 != 31 {
+                return Ok(p.1);
+            } else if p.0 != attributes::COUNTRY_OF_RESIDENCE {
+                return Ok(5);
+            } else if &buf[..] != (1..=31).collect::<Vec<_>>().as_slice() {
+                return Ok(6);
+            }
+        } else {
+            return Ok(7);
+        }
+    }
+    Ok(0)
+}
