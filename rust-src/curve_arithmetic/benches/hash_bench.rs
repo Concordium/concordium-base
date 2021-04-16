de8d703dfdd7daed985812232e5fa97577d88ad3
Emil B <eb@concordium.com>
2021-04-16 15:26:33 +0200

    Fixed sswu function and removed some inversions.

--- rust-src/curve_arithmetic/benches/hash_bench.rs
+++ rust-src/curve_arithmetic/benches/hash_bench.rs
@@ -40,5 +40,5 @@ pub fn bench_hash_to_curve(c: &mut Criterion) {
 // }
 
 // criterion_group!(hash_to_fq, bench_hash_to_fq);
-criterion_group!(hash_to_curve, bench_hash_to_curve);
-criterion_main!(hash_to_curve);
+criterion_group!(hash_to_curve_, bench_hash_to_curve);
+criterion_main!(hash_to_curve_);
