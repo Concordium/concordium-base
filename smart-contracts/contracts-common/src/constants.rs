e487bf710885b6813e8def211d9ac56694c46355
Kasper Dissing Bargsteen <kb@concordium.com>
2021-06-11 11:11:51 +0200

    Explain name validation and move MAX_FUNC_NAME_SIZE from wasm-transform

--- smart-contracts/contracts-common/src/constants.rs
+++ smart-contracts/contracts-common/src/constants.rs
@@ -1,5 +1,6 @@
-/// Must stay in sync with wasm-transform/src/constants.rs
 /// Maximum size of function names.
-pub(crate) const MAX_FUNC_NAME_SIZE: usize = 100;
+/// A contract name is defined by its init name, so this also limits the size
+/// of contract names.
+pub const MAX_FUNC_NAME_SIZE: usize = 100;
 
 pub(crate) static MAX_PREALLOCATED_CAPACITY: usize = 4096;
