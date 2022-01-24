b1d7f058fabba028f123557990bff2466b078826
Aleš Bizjak <ab@concordium.com>
2022-01-24 09:20:23 +0100

    Documentation improvements.

--- smart-contracts/wasm-chain-integration/src/constants.rs
+++ smart-contracts/wasm-chain-integration/src/constants.rs
@@ -1,4 +1,4 @@
-/// Maximum size of contract state in bytes.
+/// Maximum size of a V0 contract state in bytes.
 pub const MAX_CONTRACT_STATE: u32 = 16384; // 16kB
 
 /// Maximum number of nested function calls.
