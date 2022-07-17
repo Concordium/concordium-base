e0a02044192036789d77050e96156986da388275
tschudid <14889202+tschudid@users.noreply.github.com>
2022-07-17 10:13:00 +0200

    add basic or test

--- rust-src/id/src/sigma_protocols/sigma_test.rs
+++ rust-src/id/src/sigma_protocols/sigma_test.rs
@@ -14,3 +14,33 @@ pub fn test_and() {
         assert!(verify(&mut RandomOracle::domain("test"), &prover, &proof))
     })
 }
+
+#[test]
+pub fn test_or() {
+    let mut csprng = rand::thread_rng();
+    //Knowledge of both
+    OrAdapter::<
+        AndAdapter<dlog::Dlog<G1>, com_eq_sig::ComEqSig<Bls12, G1>>,
+        com_enc_eq::ComEncEq<G2>,
+    >::with_valid_data(38, &mut csprng, |prover, secret, csprng| {
+        let proof = prove(&mut RandomOracle::domain("test"), &prover, secret, csprng)
+            .expect("Proving should succeed.");
+        assert!(verify(&mut RandomOracle::domain("test"), &prover, &proof))
+    });
+    //Knowledge of of one
+    OrAdapter::<
+        AndAdapter<dlog::Dlog<G1>, com_eq_sig::ComEqSig<Bls12, G1>>,
+        com_enc_eq::ComEncEq<G2>,
+    >::with_valid_data(38, &mut csprng, |prover, secret, csprng| {
+        if let OrSecret::Both(s1,s2) = secret{
+            let sec1 = OrSecret::P1(s1);
+            let proof = prove(&mut RandomOracle::domain("test"), &prover, sec1, csprng)
+            .expect("Proving should succeed.");
+            assert!(verify(&mut RandomOracle::domain("test"), &prover, &proof));
+            let sec2 = OrSecret::P2(s2);
+            let proof = prove(&mut RandomOracle::domain("test"), &prover, sec2, csprng)
+            .expect("Proving should succeed.");
+            assert!(verify(&mut RandomOracle::domain("test"), &prover, &proof))
+        } else {panic!("with_valid_data should provide both secrets")}
+    })
+}
