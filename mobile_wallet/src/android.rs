2d880cb22b40a7aab6797ecf206959ea248d15b2
Emil B <eb@concordium.com>
2022-08-01 15:26:02 +0200

    Format

--- mobile_wallet/src/android.rs
+++ mobile_wallet/src/android.rs
@@ -3,8 +3,9 @@
 
 use crate::{
     check_account_address, combine_encrypted_amounts, create_configure_baker_transaction,
-    create_configure_delegation_transaction, create_credential, create_credential_v1, create_encrypted_transfer,
-    create_id_request_and_private_data, create_id_request_and_private_data_v1, create_pub_to_sec_transfer, create_sec_to_pub_transfer,
+    create_configure_delegation_transaction, create_credential, create_credential_v1,
+    create_encrypted_transfer, create_id_request_and_private_data,
+    create_id_request_and_private_data_v1, create_pub_to_sec_transfer, create_sec_to_pub_transfer,
     create_transfer, decrypt_encrypted_amount, generate_accounts, generate_baker_keys,
     get_account_keys_and_randomness, get_identity_keys_and_randomness,
 };
