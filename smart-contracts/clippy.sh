b2b252254ba803ea259861829f40f723b4818a9d
Emil Holm Gjørup <eg@concordium.com>
2022-08-11 13:15:42 +0200

    Remove old smart-contract project files and merge CI

--- smart-contracts/clippy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name Cargo.toml -exec cargo clippy --color=always --tests --manifest-path {} -- -Dclippy::all \;
--- smart-contracts/fmt.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name Cargo.toml -exec cargo +nightly-2021-06-09 fmt --manifest-path {} \;
