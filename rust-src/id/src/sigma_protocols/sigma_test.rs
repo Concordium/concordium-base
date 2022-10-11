0ad7b15e926967721aae936f4432cc77699108d2
Danil Annenkov <danil.v.annenkov@gmail.com>
2022-10-11 18:40:13 +0200

    Add simple interpreter

--- rust-src/id/src/sigma_protocols/sigma_test.rs
+++ rust-src/id/src/sigma_protocols/sigma_test.rs
@@ -1,6 +1,10 @@
 use crate::sigma_protocols::{com_enc_eq, com_eq_sig, common::*, dlog};
+use curve_arithmetic::Curve;
 use pairing::bls12_381::{Bls12, G1, G2};
+use pedersen_scheme::{Commitment, CommitmentKey};
 use random_oracle::RandomOracle;
+
+use super::com_eq;
 
 #[test]
 pub fn test_and() {
@@ -14,3 +18,50 @@ pub fn test_and() {
         assert!(verify(&mut RandomOracle::domain("test"), &prover, &proof))
     })
 }
+
+// Just an example, "real" sigma-protocol atomic statements will be determined once we know
+// what kind of (in)equalities we want to prove
+#[derive(Clone, Copy)]
+enum SigmaAtom<C: Curve> {
+    Eq(C, C),
+    EqCommitment(Commitment<C>, C, CommitmentKey<C>, C),
+}
+enum ZkLang<C: Curve> {
+    And(Vec<SigmaAtom<C>>),
+}
+
+fn interpret_sigma_atom<C: Curve>(
+    s: SigmaAtom<C>,
+) -> EitherAdapter<dlog::Dlog<C>, com_eq::ComEq<C, C>> {
+    match s {
+        SigmaAtom::Eq(c1, c2) => EitherAdapter {
+            // discrete log for this one
+            protocol: Either::Left(dlog::Dlog {
+                public: c1,
+                coeff: c2,
+            }),
+        },
+        SigmaAtom::EqCommitment(c1, y, c2, g) => EitherAdapter {
+            protocol: Either::Right(com_eq::ComEq {
+                // discrete log and commitments
+                commitment: c1,
+                y,
+                cmm_key: c2,
+                g,
+            }),
+        },
+    }
+}
+
+fn interpret_sigma_zk_lang<C : Curve> (
+    s : ZkLang<C>
+ ) -> ReplicateAdapter<EitherAdapter<dlog::Dlog<C>, com_eq::ComEq<C,C>>> {
+    match s {
+        ZkLang::And(statements) => {
+            let protocols = 
+                statements.iter().map(|s| interpret_sigma_atom(*s)).collect();
+            ReplicateAdapter { protocols }
+        }
+    }
+    
+ }
