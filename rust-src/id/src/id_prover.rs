83a6347314bd387852b714af5263e51cb2250176
Aleš Bizjak <ab@concordium.com>
2022-10-25 11:41:06 +0200

    Sketch some parts of the id library.

--- rust-src/id/src/id_prover.rs
+++ rust-src/id/src/id_prover.rs
@@ -1,6 +1,7 @@
-use crate::types::*;
+use crate::{types::*, constants::AttributeKind};
 use bulletproofs::{
     range_proof::{prove_given_scalars, RangeProof},
+    set_membership_proof::SetMembershipProof,
     utils::Generators,
 };
 use curve_arithmetic::Curve;
@@ -9,6 +10,189 @@ use ff::Field;
 use pedersen_scheme::{CommitmentKey as PedersenKey, Randomness as PedersenRandomness};
 use random_oracle::RandomOracle;
 use sha2::{Digest, Sha256};
+
+// #[derive(SerdeSerialize, SerdeDeserialize)]
+// #[serde(bound(
+//     serialize = "C: Curve, AttributeType: Attribute<C::Scalar> +
+// SerdeSerialize",     deserialize = "C: Curve, AttributeType:
+// Attribute<C::Scalar> + SerdeDeserialize<'de>" ))]
+// #[serde(tag = "type")]
+pub enum AtomicProof<C: Curve, AttributeType: Attribute<C::Scalar>> {
+    // #[serde(rename_all = "camelCase")]
+    RevealAttribute {
+        attribute_tag: AttributeTag,
+        attribute:     AttributeType,
+        proof:         super::sigma_protocols::com_eq::Witness<C>,
+    },
+    // #[serde(rename_all = "camelCase")]
+    AttributeInRange {
+        attribute_tag: AttributeTag,
+        lower:         AttributeType,
+        upper:         AttributeType,
+        proof:         RangeProof<C>,
+    },
+    AttributeInSet {
+        attribute_tag: AttributeTag,
+        set:           std::collections::BTreeSet<AttributeType>,
+        proof:         SetMembershipProof<C>,
+    }
+}
+
+// needs to be serializable to JSON
+pub enum AtomicStatement<AttributeType> {
+    // #[serde(rename_all = "camelCase")]
+    RevealAttribute {
+        attribute_tag: AttributeTag,
+    },
+    // #[serde(rename_all = "camelCase")]
+    AttributeInRange {
+        attribute_tag: AttributeTag,
+        lower:         AttributeType,
+        upper:         AttributeType,
+    },
+    AttributeInSet {
+        attribute_tag: AttributeTag,
+        set:           std::collections::BTreeSet<AttributeType>,
+    },
+}
+
+/// TODO
+/// The secret should contain the statement.
+pub enum AtomicSecret<AttributeType> {
+    // #[serde(rename_all = "camelCase")]
+    RevealAttribute {
+        attribute_tag: AttributeTag,
+        attribute:     AttributeType,
+        secret: ...,
+    },
+    // #[serde(rename_all = "camelCase")]
+    AttributeInRange {
+        attribute_tag: AttributeTag,
+        lower:         AttributeType,
+        upper:         AttributeType,
+    },
+    AttributeInSet {
+        attribute_tag: AttributeTag,
+        set:           std::collections::BTreeSet<AttributeType>,
+    },
+}
+
+pub struct StatementWithContext<AttributeType> {
+    pub account:    AccountAddress,
+    pub credential: CredentialRegistrationID,
+    pub statement:  Statement<AttributeType>,
+}
+
+pub struct Secret<AttributeType> {
+    pub account:    AccountAddress,
+    pub credential: CredentialRegistrationID,
+    pub secrets: Vec<AtomicSecret<AttributeType>>
+}
+
+impl <AttributeType>Secret<AttributeType> {
+    pub fn prove<C: Curve>(&self) -> Result<Proof<C>, ()> {
+        todo!()
+    }
+}
+
+#[derive(Default)]
+pub struct Statement<AttributeType> {
+    pub statements: Vec<AtomicStatement<AttributeType>>,
+}
+
+impl <AttributeType>Statement<AttributeType> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn reveal_attribute(&mut self, attribute_tag: AttributeTag) -> &mut Self {
+        self.statements.push(AtomicStatement::RevealAttribute { attribute_tag })
+    }
+
+    pub fn in_range(&mut self, tag: AttributeTag, lower: AttributeType, upper: AttributeType) -> &mut Self {
+        todo!()
+    }
+
+    pub fn member_of(&mut self, tag: AttributeTag, set: &[AttributeKind]) -> &mut Self {
+        todo!()
+    }
+
+    pub fn older_than(&mut self, age: u64) -> &mut Self {
+        let current_time = todo!();
+    }
+
+    pub fn younger_than(&mut self, age: u64) -> &mut Self {
+        let current_time = todo!();
+    }
+
+    pub fn age_in_range(&mut self, lower: u64, upper: u64) -> &mut Self {
+        todo!()
+    }
+
+    // document expiry no earlier than ...
+
+    pub fn residence_in(&mut self, set: &[AttributeKind]) -> &mut Self {
+        todo!()
+    }
+
+    pub fn document_issuer(&mut self, set: &[AttributeKind]) -> &mut Self {
+        todo!()
+    }
+}
+
+// example
+
+fn example() {
+    let mut statement = Statement::new();
+    statement.older_than(18)
+        .residence_in(&["DK"]);
+    let to_prove = statement.add_secrets(todo!()); // wallet supplies the secrets.
+    let proof = to_prove.prove(challenge);
+    // send proof
+}
+
+// DAPP
+// - Constructs statements.
+
+// Browser wallet
+// - Parses statements, supplies secrets -> constructs `Secret`
+// - produces proof.
+
+// Backend
+// Verifies proofs, gets additional data (commitments) from the node.
+
+// Needs to have JSON + binary serialization
+pub struct Proof<C: Curve> {
+    pub account:    AccountAddress,
+    pub credential: CredentialRegistrationID,
+    pub proofs:     Vec<AtomicProof<C>>,
+}
+
+impl<C: Curve> Proof<C> {
+    pub fn verify<AttributeType: Attribute<C::Scalar>>(&self, challenge: &[u8]) -> bool {
+        let ro = todo!();
+        for proof in &self.proofs {
+            match proof {
+                AtomicProof::RevealAttribute {
+                    attribute_tag,
+                    attribute,
+                    proof,
+                } => todo!(),
+                AtomicProof::AttributeInRange {
+                    attribute_tag,
+                    lower,
+                    upper,
+                    proof,
+                } => todo!(),
+                AtomicProof::AttributeInSet {
+                    attribute_tag,
+                    set,
+                    proof,
+                } => todo!(),
+            }
+        }
+    }
+}
 
 /// Function for proving ownership of an account. The parameters are
 /// - data - the CredentialData containing the private keys of the prover
