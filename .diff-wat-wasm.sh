ed5e62c63457ea9bdb7e43e0a93614436a20a72b
Kasper Dissing Bargsteen <kb@concordium.com>
2023-03-01 16:08:39 +0100

    Add ignore file for wat/wasm checker and install diffutils
    - Also fix a few things suggested by shellcheck

--- .diff-wat-wasm.sh
+++ .diff-wat-wasm.sh
@@ -6,22 +6,39 @@
 
 # This script should be run from the directory that it resides in. The idea is
 # to report all the files that failed, hence no early exit from the script.
+#
+# The script will ignore wat files listed in `/smart-contracts/testdata/contracts/.diff-wat-wasm-ignore`.
 
-pushd smart-contracts/testdata/contracts
+
+pushd smart-contracts/testdata/contracts || exit
+
+# Create an array with all the files to ignore.
+declare FILES_TO_IGNORE
+while read -r line
+do
+   FILES_TO_IGNORE+=("$line")
+done < "./.diff-wat-wasm-ignore"
 
 RET=0
 
-for wat in $(find -name '*.wat'); do
+for wat in $(find . -name '*.wat'); do
+   # Skip the file if it is in the ignore list.
+   if [[ "${FILES_TO_IGNORE[*]}" =~ "$wat" ]]; then
+     echo "Ignoring file: '$wat'"
+     continue
+   fi
+   echo "Validating file: '$wat'"
+   # Otherwise, continue with the check.
    OUT=$(mktemp)
-   wat2wasm $wat -o $OUT;
-   if ! $(diff $OUT "${wat%.wat}.wasm")
+   wat2wasm "$wat" -o "$OUT";
+   if ! diff "$OUT" "${wat%.wat}.wasm"
    then
      RET=1
      echo "The $wat contract's .wasm output does not match the expected one. Regenerate the .wasm file."
    fi
-   rm $OUT
+   rm "$OUT"
 done
 
-popd
+popd || exit
 
 exit $RET
