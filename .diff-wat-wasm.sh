72c90c2506f91554f05964069c83bc01495d8ae1
Kasper Dissing Bargsteen <kb@concordium.com>
2023-03-03 13:38:06 +0100

    Do not validate modules that are intentionally invalid
    - Also fixes a few minor issues pointed out by shellcheck

--- .diff-wat-wasm.sh
+++ .diff-wat-wasm.sh
@@ -7,21 +7,45 @@
 # This script should be run from the directory that it resides in. The idea is
 # to report all the files that failed, hence no early exit from the script.
 
-pushd testdata/contracts
+
+pushd smart-contracts/testdata/contracts || exit
+
+# Files which should only be converted, not validated/checked.
+NO_CHECK_FILES=(
+    # Our interpreter accepts this file, but wat2wasm does not due to a change in the wasm spec.
+    # See https://github.com/Concordium/concordium-base/issues/331 for more info.
+    './global-offset-test.wat',
+
+    # This module is invalid because it tries to use a mutable global value as an offset in the data and elem section.
+    './mut-global-offset-test.wat',
+
+    # This module is invalid because it tries to initialize a global value with the reference of another global value.
+    './init-global-with-ref-test.wat'
+    )
 
 RET=0
 
-for wat in $(find -name '*.wat'); do
+for wat in $(find . -name '*.wat'); do
    OUT=$(mktemp)
-   wat2wasm $wat -o $OUT;
-   if ! $(diff $OUT "${wat%.wat}.wasm")
+
+   # Convert to wasm.
+   if [[ "${NO_CHECK_FILES[*]}" =~ "$wat" ]]; then
+     echo "Comparing: '$wat' (with --no-check)"
+     wat2wasm "$wat" -o "$OUT" --no-check;
+     continue
+   else
+     echo "Comparing: '$wat'"
+     wat2wasm "$wat" -o "$OUT";
+   fi
+
+   if ! diff "$OUT" "${wat%.wat}.wasm"
    then
      RET=1
      echo "The $wat contract's .wasm output does not match the expected one. Regenerate the .wasm file."
    fi
-   rm $OUT
+   rm "$OUT"
 done
 
-popd
+popd || exit
 
 exit $RET
