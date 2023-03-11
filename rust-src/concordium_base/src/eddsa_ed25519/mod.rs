f615e9bee5d4f0843b6fe57ed4830aa18462cc8a
Aleš Bizjak <ab@concordium.com>
2023-03-11 13:19:31 +0100

    Address clippy issues.
    In particular the "module inception" issue where we had some modules in the
    pattern
    
    mod foo;
    pub use foo::*;
    
    under the foo.

--- rust-src/concordium_base/src/eddsa_ed25519/mod.rs
+++ rust-src/concordium_base/src/eddsa_ed25519/mod.rs
@@ -1,6 +1,5 @@
 //! A few helpers around the dalek ed25519 signature scheme.
 mod dlog_ed25519;
-// this module only has FFI exports, so we don't need to re-export anything.
-mod eddsa_ed25519;
+mod ffi;
 
 pub use dlog_ed25519::*;
