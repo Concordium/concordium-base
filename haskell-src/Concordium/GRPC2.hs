01b8dca9693d90bd6b347ac1202887170035c52e
Emil Lai <mail@emillai.com>
2023-08-11 09:51:07 +0200

    Add a 'CommissionRates' field to 'CurrentPaydayBakerPoolStatus'.

--- haskell-src/Concordium/GRPC2.hs
+++ haskell-src/Concordium/GRPC2.hs
@@ -1669,6 +1669,7 @@ instance ToProto QueryTypes.CurrentPaydayBakerPoolStatus where
         ProtoFields.lotteryPower .= bpsLotteryPower
         ProtoFields.bakerEquityCapital .= toProto bpsBakerEquityCapital
         ProtoFields.delegatedCapital .= toProto bpsDelegatedCapital
+        ProtoFields.commissionRates .= toProto bpsCommissionRates
 
 instance ToProto QueryTypes.RewardStatus where
     type Output QueryTypes.RewardStatus = Proto.TokenomicsInfo
