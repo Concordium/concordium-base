a37d5e4d522a179404489012003736aed17190b2
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-23 11:57:16 +0100

    better naming

--- rust-src/concordium_base/src/ffi_helpers/common.rs
+++ rust-src/concordium_base/src/ffi_helpers/common.rs
@@ -8,7 +8,7 @@ extern "C" fn free_array_len(ptr: *mut u8, len: u64) {
 }
 
 #[no_mangle]
-/// Free a vector from its raw pointer, length and capacity.
+/// Free a vector using its raw pointer, length and capacity.
 extern "C" fn free_array_len_cap(ptr: *mut u8, len: u64, cap: u64) {
     unsafe {
         Vec::from_raw_parts(ptr, len as usize, cap as usize);
