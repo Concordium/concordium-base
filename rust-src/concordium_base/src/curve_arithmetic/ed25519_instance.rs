287a2b89be8873eee070a863d65020dde0152891
Hamidreza <54936533+hamiidreza@users.noreply.github.com>
2023-12-19 17:23:09 +0100

    Adding tests for ed25519

--- rust-src/concordium_base/src/curve_arithmetic/ed25519_instance.rs
+++ rust-src/concordium_base/src/curve_arithmetic/ed25519_instance.rs
@@ -98,8 +98,11 @@ impl PrimeField for RistrettoScalar {
             .try_into()
             .map_err(|_| super::CurveDecodingError::NotInField(format!("{:?}", r)))?;
         let mut s_bytes = [0u8; 32];
+        let mut offset = 0;
         for x in tmp {
-            LittleEndian::write_u64(&mut s_bytes, x);
+            let max = offset + 8;
+            LittleEndian::write_u64(&mut s_bytes[offset..max], x);
+            offset = max;
         }
         let res = Scalar::from_canonical_bytes(s_bytes).ok_or(
             super::CurveDecodingError::NotInField(format!("{:?}", s_bytes)),
@@ -226,5 +229,125 @@ impl MultiExp for RistrettoMultiExpNoPrecompute {
         exps: &[X],
     ) -> Self::CurvePoint {
         Self::CurvePoint::vartime_multiscalar_mul(exps.iter().map(|p| p.borrow().0), &self.points)
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::{RistrettoScalar, *};
+    use crate::common::*;
+    use curve25519_dalek::ristretto::RistrettoPoint;
+    use std::io::Cursor;
+
+    // Test serialization for scalars
+    #[test]
+    fn test_scalar_serialization() {
+        let mut csprng = rand::thread_rng();
+        for _ in 0..1000 {
+            let mut out = Vec::<u8>::new();
+            let scalar = RistrettoScalar::random(&mut csprng);
+            scalar.serial(&mut out);
+            let scalar_res = RistrettoScalar::deserial(&mut Cursor::new(out));
+            assert!(scalar_res.is_ok());
+            assert_eq!(scalar, scalar_res.unwrap());
+        }
+    }
+
+    // Test serialization for curve points
+    #[test]
+    fn test_point_serialization() {
+        let mut csprng = rand::thread_rng();
+        for _ in 0..1000 {
+            let mut out = Vec::<u8>::new();
+            let point = RistrettoPoint::generate(&mut csprng);
+            point.serial(&mut out);
+            let point_res = RistrettoPoint::deserial(&mut Cursor::new(out));
+            assert!(point_res.is_ok());
+            assert!(point_res.is_ok());
+            assert_eq!(point, point_res.unwrap());
+        }
+    }
+
+    // Turn scalar elements into representations and back again, and compare.
+    #[test]
+    fn test_into_from_rep() {
+        let mut csprng = rand::thread_rng();
+        for _ in 0..1000 {
+            let scalar = RistrettoScalar::random(&mut csprng);
+            let scalar_vec64 = scalar.into_repr();
+            let scalar_res = RistrettoScalar::from_repr(&scalar_vec64);
+            assert!(scalar_res.is_ok());
+            assert_eq!(scalar, scalar_res.unwrap());
+        }
+    }
+
+    // Turn curve points into representations and back again, and compare.
+    #[test]
+    fn test_point_byte_conversion_unchecked() {
+        let mut csprng = rand::thread_rng();
+        for _ in 0..1000 {
+            let point = RistrettoPoint::generate(&mut csprng);
+            let bytes = to_bytes(&point);
+            let point_res = RistrettoPoint::bytes_to_curve_unchecked(&mut Cursor::new(&bytes));
+            assert!(point_res.is_ok());
+            assert_eq!(point, point_res.unwrap());
+        }
+    }
+
+    /// Random element from the scalar field GF(2\^{252} +
+    /// 27742317777372353535851937790883648493)
+    /// a = 2238329342913194256032495932344128051776374960164957527413114840482143558222
+    static A_BYTES: [u8; 32] = [
+        0x4e, 0x5a, 0xb4, 0x34, 0x5d, 0x47, 0x08, 0x84, 0x59, 0x13, 0xb4, 0x64, 0x1b, 0xc2, 0x7d,
+        0x52, 0x52, 0xa5, 0x85, 0x10, 0x1b, 0xcc, 0x42, 0x44, 0xd4, 0x49, 0xf4, 0xa8, 0x79, 0xd9,
+        0xf2, 0x04,
+    ];
+
+    /// 1/a = 6859937278830797291664592131120606308688036382723378951768035303146619657244
+    static AINV_BYTES: [u8; 32] = [
+        0x1c, 0xdc, 0x17, 0xfc, 0xe0, 0xe9, 0xa5, 0xbb, 0xd9, 0x24, 0x7e, 0x56, 0xbb, 0x01, 0x63,
+        0x47, 0xbb, 0xba, 0x31, 0xed, 0xd5, 0xa9, 0xbb, 0x96, 0xd5, 0x0b, 0xcd, 0x7a, 0x3f, 0x96,
+        0x2a, 0x0f,
+    ];
+
+    /// a^2 = 7223459340038346301359662082310065231337362643762546120300009460524582755663
+    static ASQ_BYTES: [u8; 32] = [
+        0x4f, 0xdd, 0x54, 0x3d, 0xc3, 0x58, 0x8c, 0x8, 0x74, 0xd3, 0xde, 0xf1, 0x15, 0xeb, 0x46,
+        0x1, 0x9e, 0x90, 0xcc, 0x16, 0x4a, 0xc2, 0x3c, 0x3, 0xe4, 0x52, 0x13, 0x22, 0x46, 0x55,
+        0xf8, 0xf,
+    ];
+
+    #[test]
+    fn test_scalar_mult() {
+        let a: RistrettoScalar = Scalar::from_bytes_mod_order(A_BYTES).into();
+        let mut aa = a.clone();
+        aa.mul_assign(&a);
+        let asq: RistrettoScalar = Scalar::from_bytes_mod_order(ASQ_BYTES).into();
+        assert_eq!(asq, aa);
+    }
+
+    #[test]
+    fn test_scalar_square() {
+        let a: RistrettoScalar = Scalar::from_bytes_mod_order(A_BYTES).into();
+        let mut aa = a.clone();
+        aa.square();
+        let asq: RistrettoScalar = Scalar::from_bytes_mod_order(ASQ_BYTES).into();
+        assert_eq!(asq, aa);
+    }
+
+    #[test]
+    fn test_scalar_inverse() {
+        // Zero element has no inverse in a field
+        let zero: RistrettoScalar = Scalar::zero().into();
+        let zero_inv = zero.inverse();
+        assert_eq!(zero_inv, None);
+        // Every non-zero element 'a' should have an inverse computed as 'a.inverse()'
+        let a: RistrettoScalar = Scalar::from_bytes_mod_order(A_BYTES).into();
+        let ainv = a.inverse().unwrap();
+        let should_be_inverse: RistrettoScalar = Scalar::from_bytes_mod_order(AINV_BYTES).into();
+        let mut one = a.clone();
+        one.mul_assign(&ainv);
+        assert_eq!(ainv, should_be_inverse);
+        assert_eq!(RistrettoScalar::one(), one);
     }
 }
