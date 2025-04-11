8fdba324a71d7a7aeceb1622441d65d119e1ad2a
Thomas Dinsdale-Young <ty@concordium.com>
2025-04-11 16:48:56 +0200

    CBOR encoding of `TokenInitializationParameters`.

--- haskell-src/Concordium/Types/ProtocolLevelTokens/CBOR.hs
+++ haskell-src/Concordium/Types/ProtocolLevelTokens/CBOR.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -6,7 +5,12 @@
 module Concordium.Types.ProtocolLevelTokens.CBOR where
 
 import Codec.CBOR.Decoding
+import Codec.CBOR.Encoding
+import qualified Codec.CBOR.Write as CBOR
 import Control.Monad
+import qualified Data.ByteString.Lazy as LBS
+import Data.Function
+import qualified Data.Map.Lazy as Map
 import Data.Maybe
 import Data.Scientific
 import Data.Text
@@ -14,6 +18,147 @@ import Data.Word
 import Lens.Micro.Platform
 
 import Concordium.Types.Queries.Tokens
+
+-- * Decoder helpers
+
+-- | A 'MapValueDecoder' consumes a value corresponding to a known key and sets it in the builder.
+--  It should fail if entry in the builder corresponding to the key is already set.
+type MapValueDecoder s builder = builder -> Decoder s builder
+
+-- | Build a 'MapValueDecoder' for a particular key. This enforces that a field cannot be set more
+--  than once.
+mapValueDecoder ::
+    -- | The key being decoded (used for error reporting).
+    Text ->
+    -- | The decoder to use for the value.
+    Decoder s v ->
+    -- | A lens to access the field on the builder.
+    Lens' builder (Maybe v) ->
+    MapValueDecoder s builder
+mapValueDecoder key decodeVal l = \builder -> do
+    val <- decodeVal
+    case builder ^. l of
+        Nothing -> return $ builder & l ?~ val
+        Just _ -> fail $ "Key already set: " ++ show key
+
+-- | Decode a CBOR-encoded map to type @r@. This exclusively supports UTF-8 string keys.
+--  The decoding uses a @builder@ type that is used to progressively decode the map.
+--  The first argument is a function that determines if each key is allowed, and if so,
+--  how the corresponding value should be decoded and added to the builder.
+decodeMap ::
+    -- | Function that determines the 'MapValueDecoder' for a map key.
+    (Text -> Maybe (MapValueDecoder s builder)) ->
+    -- | Function to run the builder and produce a value.
+    (builder -> Either String r) ->
+    -- | The empty builder.
+    builder ->
+    Decoder s r
+decodeMap valDecoder runBuilder emptyBuilder = do
+    builder <-
+        decodeMapLenOrIndef >>= \case
+            Nothing -> decodeIndef emptyBuilder
+            Just len -> decodeDef len emptyBuilder
+    case runBuilder builder of
+        Left e -> fail e
+        Right r -> return r
+  where
+    decodeKV builder = do
+        key <- decodeString
+        case valDecoder key of
+            Nothing -> fail $ "Unexpected key " ++ show key
+            Just decoder -> decoder builder
+    decodeIndef builder = do
+        decodeBreakOr >>= \case
+            True -> return builder
+            False -> do
+                builder' <- decodeKV builder
+                decodeIndef builder'
+    decodeDef n builder
+        | n > 0 = decodeDef (n - 1) =<< decodeKV builder
+        | otherwise = return builder
+
+-- | Decode a CBOR decimal fraction into a 'Scientific'. The result is not normalized.
+decodeDecimalFraction :: Decoder s Scientific
+decodeDecimalFraction = do
+    tag <- decodeTag
+    unless (tag == 4) $ fail $ "Expected decimal fraction (tag 4), but found tag " ++ show tag
+    mLen <- decodeListLenOrIndef
+    forM_ mLen $ \len ->
+        unless (len == 2) $
+            fail $
+                "Expected an array of length 2, but length was " ++ show len
+    exp10 <- decodeInt
+    mantissa <- decodeInteger
+    when (isNothing mLen) $ decodeBreakOr >>= \b -> unless b (fail "Expected end of array")
+    return $ scientific mantissa exp10
+
+-- * Encoding helpers
+
+-- | An 'Encoding' that represents a key in a CBOR map. The 'Ord' instance corresponds to
+--  lexicographic ording of the CBOR encodings, so that these can be used to order keys for
+--  deterministic CBOR encoding.
+data MapKeyEncoding = MapKeyEncoding
+    { meEncoding :: Encoding,
+      meBytes :: LBS.ByteString
+    }
+
+instance Eq MapKeyEncoding where
+    (==) = (==) `on` meBytes
+
+instance Ord MapKeyEncoding where
+    compare = compare `on` meBytes
+
+-- | Convert an 'Encoding' to a 'MapKeyEncoding'
+makeMapKeyEncoding :: Encoding -> MapKeyEncoding
+makeMapKeyEncoding meEncoding = MapKeyEncoding{meBytes = CBOR.toLazyByteString meEncoding, ..}
+
+-- | Encode a map deterministically. Specifically, the map length is definite and the keys are
+--  ordered in bytewise lexicographic order. It is assumed that all encodings in the map are
+--  deterministic.
+encodeMapDeterministic :: Map.Map MapKeyEncoding Encoding -> Encoding
+encodeMapDeterministic m =
+    encodeMapLen (fromIntegral $ Map.size m)
+        <> mconcat [meEncoding k <> v | (k, v) <- Map.toAscList m]
+
+-- * Builder helpers
+
+-- | Convert a 'Maybe' to an 'Either'.
+orFail :: Maybe a -> b -> Either b a
+orFail Nothing = Left
+orFail (Just v) = const (Right v)
+
+-- | Unwrap a 'Maybe' using a supplied default value.
+orDefault :: Maybe a -> a -> a
+orDefault Nothing = id
+orDefault (Just a) = const a
+
+-- * Token amounts
+
+-- | Decode a CBOR-encoded 'TokenAmount'. This limits the number of decimals to the
+--  range @[0..255]@.
+decodeTokenAmount :: Decoder s TokenAmount
+decodeTokenAmount = do
+    sci <- decodeDecimalFraction
+    unless (coefficient sci >= 0) $ fail "Unexpected negative token amount"
+    unless (coefficient sci <= fromIntegral (maxBound :: Word64)) $
+        fail "Token amount exceeds expressible bound"
+    unless (base10Exponent sci <= 0) $ fail "Token amount cannot have a positive exponent"
+    unless (base10Exponent sci >= -255) $ fail "Token amount exponent is too small"
+    return
+        TokenAmount
+            { digits = fromIntegral (coefficient sci),
+              nrDecimals = fromIntegral (negate (base10Exponent sci))
+            }
+
+-- | Encode a 'TokenAmount' as CBOR.
+encodeTokenAmount :: TokenAmount -> Encoding
+encodeTokenAmount TokenAmount{..} =
+    encodeTag 4
+        <> encodeListLen 2
+        <> encodeInteger (-fromIntegral nrDecimals)
+        <> encodeWord64 digits
+
+-- * Initialization parameters
 
 -- | The parsed token-initialization-parameters. These parameters are passed to the token module
 --  to initialize the token.
@@ -35,6 +180,7 @@ data TokenInitializationParameters = TokenInitializationParameters
     }
     deriving (Eq, Show)
 
+-- | Builder for 'TokenInitializationParameters'
 data TokenInitializationParametersBuilder = TokenInitializationParametersBuilder
     { _tipbName :: Maybe Text,
       _tipbMetadata :: Maybe Text,
@@ -52,18 +198,14 @@ emptyTokenInitializationParametersBuilder :: TokenInitializationParametersBuilde
 emptyTokenInitializationParametersBuilder =
     TokenInitializationParametersBuilder Nothing Nothing Nothing Nothing Nothing Nothing Nothing
 
-orFail :: Maybe a -> b -> Either b a
-orFail Nothing = Left
-orFail (Just v) = const (Right v)
-
-orDefault :: Maybe a -> a -> a
-orDefault Nothing = id
-orDefault (Just a) = const a
-
-buildTokenInitializationParameters :: TokenInitializationParametersBuilder -> Either String TokenInitializationParameters
+-- | Construct a 'TokenInitializationParameters' from a 'TokenInitializationParametersBuilder'.
+--  This results in @Left err@ (where @err@ describes the failure reason) when a required parameter
+--  is missing. Missing optional parameters are populated with the appropriate default values.
+buildTokenInitializationParameters ::
+    TokenInitializationParametersBuilder -> Either String TokenInitializationParameters
 buildTokenInitializationParameters TokenInitializationParametersBuilder{..} = do
-    tipName <- _tipbName `orFail` "Missing 'name'"
-    tipMetadata <- _tipbMetadata `orFail` "Missing 'metadata'"
+    tipName <- _tipbName `orFail` "Missing \"name\""
+    tipMetadata <- _tipbMetadata `orFail` "Missing \"metadata\""
     let tipAllowList = _tipbAllowList `orDefault` False
     let tipDenyList = _tipbDenyList `orDefault` False
     let tipInitialSupply = _tipbInitialSupply
@@ -71,74 +213,10 @@ buildTokenInitializationParameters TokenInitializationParametersBuilder{..} = do
     let tipBurnable = _tipbBurnable `orDefault` False
     return TokenInitializationParameters{..}
 
-data MapValueDecoder s a = forall b.
-      MapValueDecoder
-    { mvdDecoder :: Decoder s b,
-      mvdTrySet :: b -> a -> Maybe a
-    }
-
--- | Given a 'Lens'' for a field in a builder, set the field to the given value if it is not
---  already present. If a value is already present, then this returns @Nothing@.
-trySet :: Lens' a (Maybe b) -> b -> a -> Maybe a
-trySet l = \v -> l (maybe (Just (Just v)) (const Nothing))
-
-decodeMap :: (Text -> Maybe (MapValueDecoder s builder)) -> (builder -> Either String r) -> builder -> Decoder s r
-decodeMap valDecoder runBuilder emptyBuilder = do
-    builder <-
-        decodeMapLenOrIndef >>= \case
-            Nothing -> decodeIndef emptyBuilder
-            Just len -> decodeDef len emptyBuilder
-    case runBuilder builder of
-        Left e -> fail e
-        Right r -> return r
-  where
-    decodeKV builder = do
-        key <- decodeString
-        case valDecoder key of
-            Nothing -> fail $ "Unexpected key " ++ show key
-            Just MapValueDecoder{..} -> do
-                val <- mvdDecoder
-                case mvdTrySet val builder of
-                    Nothing -> fail $ "Key already set: " ++ show key
-                    Just builder' -> return builder'
-    decodeIndef builder = do
-        decodeBreakOr >>= \case
-            True -> return builder
-            False -> do
-                builder' <- decodeKV builder
-                decodeIndef builder'
-    decodeDef n builder
-        | n > 0 = decodeDef (n - 1) =<< decodeKV builder
-        | otherwise = return builder
-
-decodeDecimalFraction :: Decoder s Scientific
-decodeDecimalFraction = do
-    tag <- decodeTag
-    unless (tag == 4) $ fail $ "Expected decimal fraction (tag 4), but found tag " ++ show tag
-    mLen <- decodeListLenOrIndef
-    forM_ mLen $ \len ->
-        unless (len == 2) $
-            fail $
-                "Expected an array of length 2, but length was " ++ show len
-    exp10 <- decodeInt
-    mantissa <- decodeInteger
-    when (isNothing mLen) $ decodeBreakOr >>= \b -> unless b (fail "Expected end of array")
-    return $ scientific mantissa exp10
-
-decodeTokenAmount :: Decoder s TokenAmount
-decodeTokenAmount = do
-    sci <- decodeDecimalFraction
-    unless (coefficient sci >= 0) $ fail "Unexpected negative token amount"
-    unless (coefficient sci <= fromIntegral (maxBound :: Word64)) $
-        fail "Token amount exceeds expressible bound"
-    unless (base10Exponent sci <= 0) $ fail "Token amount cannot have a positive exponent"
-    unless (base10Exponent sci >= -255) $ fail "Token amount exponent is too small"
-    return
-        TokenAmount
-            { digits = fromIntegral (coefficient sci),
-              nrDecimals = fromIntegral (negate (base10Exponent sci))
-            }
-
+-- | Decode a CBOR-encoded 'TokenInitializationParameters'.
+--  This decoder enforces CBOR-validity (in particular, no duplicate map keys), disallows
+--  extraneous keys, and applies defaults specified by the CDDL schema.  It does not require
+--  deterministic encoding.
 decodeTokenInitializationParameters :: Decoder s TokenInitializationParameters
 decodeTokenInitializationParameters =
     decodeMap
@@ -146,11 +224,44 @@ decodeTokenInitializationParameters =
         buildTokenInitializationParameters
         emptyTokenInitializationParametersBuilder
   where
-    valDecoder "name" = Just $ MapValueDecoder decodeString (trySet tipbName)
-    valDecoder "metadata" = Just $ MapValueDecoder decodeString (trySet tipbMetadata)
-    valDecoder "allowList" = Just $ MapValueDecoder decodeBool (trySet tipbAllowList)
-    valDecoder "denyList" = Just $ MapValueDecoder decodeBool (trySet tipbAllowList)
-    valDecoder "initialSupply" = Just $ MapValueDecoder decodeTokenAmount (trySet tipbInitialSupply)
-    valDecoder "mintable" = Just $ MapValueDecoder decodeBool (trySet tipbMintable)
-    valDecoder "burnable" = Just $ MapValueDecoder decodeBool (trySet tipbBurnable)
+    valDecoder k@"name" = Just $ mapValueDecoder k decodeString tipbName
+    valDecoder k@"metadata" = Just $ mapValueDecoder k decodeString tipbMetadata
+    valDecoder k@"allowList" = Just $ mapValueDecoder k decodeBool tipbAllowList
+    valDecoder k@"denyList" = Just $ mapValueDecoder k decodeBool tipbDenyList
+    valDecoder k@"initialSupply" = Just $ mapValueDecoder k decodeTokenAmount tipbInitialSupply
+    valDecoder k@"mintable" = Just $ mapValueDecoder k decodeBool tipbMintable
+    valDecoder k@"burnable" = Just $ mapValueDecoder k decodeBool tipbBurnable
     valDecoder _ = Nothing
+
+-- | Encode a 'TokenInitializationParameters' as CBOR.
+encodeTokenInitializationParametersNoDefaults :: TokenInitializationParameters -> Encoding
+encodeTokenInitializationParametersNoDefaults TokenInitializationParameters{..} =
+    encodeMapDeterministic $
+        Map.empty
+            & k "name" ?~ encodeString tipName
+            & k "metadata" ?~ encodeString tipMetadata
+            & k "allowList" ?~ encodeBool tipAllowList
+            & k "denyList" ?~ encodeBool tipDenyList
+            & k "initialSupply" .~ (encodeTokenAmount <$> tipInitialSupply)
+            & k "mintable" ?~ encodeBool tipMintable
+            & k "burnable" ?~ encodeBool tipBurnable
+  where
+    k = at . makeMapKeyEncoding . encodeString
+
+-- | Encode a 'TokenInitializationParameters' as CBOR. Keys that hold their default values will
+--  be omitted from the encoding.
+encodeTokenInitializationParametersWithDefaults :: TokenInitializationParameters -> Encoding
+encodeTokenInitializationParametersWithDefaults TokenInitializationParameters{..} =
+    encodeMapDeterministic $
+        Map.empty
+            & k "name" ?~ encodeString tipName
+            & k "metadata" ?~ encodeString tipMetadata
+            & setIfTrue "allowList" tipAllowList
+            & setIfTrue "denyList" tipDenyList
+            & k "initialSupply" .~ (encodeTokenAmount <$> tipInitialSupply)
+            & setIfTrue "mintable" tipMintable
+            & setIfTrue "burnable" tipBurnable
+  where
+    k = at . makeMapKeyEncoding . encodeString
+    setIfTrue _ False = id
+    setIfTrue key True = k key ?~ encodeBool True
--- haskell-tests/Types/CBOR.hs
+++ haskell-tests/Types/CBOR.hs
@@ -3,14 +3,17 @@
 module Types.CBOR where
 
 import Codec.CBOR.Read
+import Codec.CBOR.Write
+import qualified Data.ByteString.Base16.Lazy as B16L
+import qualified Data.ByteString.Lazy.Char8 as LBS
 
--- import qualified Data.ByteString as BS
 import Test.HUnit
 import Test.Hspec
 
 import Concordium.Types.ProtocolLevelTokens.CBOR
 import Concordium.Types.Queries.Tokens
 
+-- | A test value for 'TokenInitializationParameters'.
 tip1 :: TokenInitializationParameters
 tip1 =
     TokenInitializationParameters
@@ -23,9 +26,10 @@ tip1 =
           tipBurnable = False
         }
 
+-- | Basic tests for CBOR encoding/decoding of 'TokenInitializationParameters'.
 testInitializationParameters :: Spec
 testInitializationParameters = describe "token-initialization-parameters decoding" $ do
-    it "example 1 " $
+    it "example 1" $
         assertEqual
             "Decoded CBOR"
             (Right ("", tip1))
@@ -34,6 +38,44 @@ testInitializationParameters = describe "token-initialization-parameters decodin
                 "\xA4\x64name\x69\
                 \ABC token\x68metadata\x76https://abc.token/meta\x69\
                 \allowList\xF4\x6DinitialSupply\xC4\x82\x24\x19\x27\x10"
+            )
+    it "Missing \"name\"" $
+        assertEqual
+            "Decoded CBOR"
+            (Left (DeserialiseFailure 64 "Missing \"name\""))
+            ( deserialiseFromBytes
+                decodeTokenInitializationParameters
+                "\xA3\x68metadata\x76https://abc.token/meta\x69\
+                \allowList\xF4\x6DinitialSupply\xC4\x82\x24\x19\x27\x10"
+            )
+    it "Duplicate \"name\" key" $
+        assertEqual
+            "Decode result"
+            (Left (DeserialiseFailure 90 "Key already set: \"name\""))
+            ( deserialiseFromBytes
+                decodeTokenInitializationParameters
+                "\xA5\x64name\x69\
+                \ABC token\x68metadata\x76https://abc.token/meta\x69\
+                \allowList\xF4\x6DinitialSupply\xC4\x82\x24\x19\x27\x10\
+                \\x64name\x65token"
+            )
+    it "Encode and decode (no defaults)" $ do
+        -- LBS.putStrLn $ B16L.encode $ toLazyByteString $ encodeTokenInitializationParametersNoDefaults tip1
+        assertEqual
+            "Decode result"
+            (Right ("", tip1))
+            ( deserialiseFromBytes
+                decodeTokenInitializationParameters
+                (toLazyByteString $ encodeTokenInitializationParametersNoDefaults tip1)
+            )
+    it "Encode and decode (with defaults)" $ do
+        LBS.putStrLn $ B16L.encode $ toLazyByteString $ encodeTokenInitializationParametersWithDefaults tip1
+        assertEqual
+            "Decode result"
+            (Right ("", tip1))
+            ( deserialiseFromBytes
+                decodeTokenInitializationParameters
+                (toLazyByteString $ encodeTokenInitializationParametersWithDefaults tip1)
             )
 
 tests :: Spec
