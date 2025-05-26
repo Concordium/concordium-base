d1d0d971f78b528cae193b9929c937c2cd4db450
Allan Rasmussen <allanbrondum@gmail.com>
2025-05-26 09:38:50 +0200

    cbor

--- rust-src/concordium_base/src/protocol_level_tokens/token_reject_reason.rs
+++ rust-src/concordium_base/src/protocol_level_tokens/token_reject_reason.rs
@@ -1,4 +1,3 @@
-use concordium_base_derive::CborSerialize;
 use crate::internal::cbor::{
     CborDecoder, CborDeserialize, CborEncoder, CborError, CborResult, CborSerialize, MapKey,
     MapKeyRef,
@@ -6,6 +5,7 @@ use crate::internal::cbor::{
 use crate::protocol_level_tokens::{
     token_holder::TokenHolder, RawCbor, TokenAmount, TokenId, TokenModuleCborTypeDiscriminator,
 };
+use concordium_base_derive::{CborDeserialize, CborSerialize};
 
 /// Details provided by the token module in the event of rejecting a
 /// transaction.
@@ -43,73 +43,22 @@ pub enum TokenModuleRejectReasonType {
 }
 
 /// A token holder address was not valid.
-#[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize, CborSerialize)]
+#[derive(
+    Debug,
+    Clone,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    CborSerialize,
+    CborDeserialize,
+)]
 #[serde(rename_all = "camelCase")]
 pub struct AddressNotFoundRejectReason {
     /// The index in the list of operations of the failing operation.
     pub index: usize,
     /// The address that could not be resolved.
     pub address: TokenHolder,
-}
-
-// impl CborSerialize for AddressNotFoundRejectReason {
-//     fn serialize<C: CborEncoder>(&self, encoder: &mut C) -> CborResult<()> {
-//         encoder.encode_map(
-//             if self.index.is_null() { 0 } else { 1 } + if self.address.is_null() { 0 } else { 1 },
-//         )?;
-// 
-//         if !self.index.is_null() {
-//             MapKeyRef::Text("index").serialize(encoder)?;
-//             self.index.serialize(encoder)?;
-//         }
-// 
-//         if !self.address.is_null() {
-//             MapKeyRef::Text("address").serialize(encoder)?;
-//             self.address.serialize(encoder)?;
-//         }
-//         Ok(())
-//     }
-// }
-
-impl CborDeserialize for AddressNotFoundRejectReason {
-    fn deserialize<C: CborDecoder>(decoder: &mut C) -> CborResult<Self>
-    where
-        Self: Sized,
-    {
-        let mut index = None;
-        let mut address = None;
-        let map_size = decoder.decode_map()?;
-        for _ in 0..map_size {
-            let map_key = MapKey::deserialize(decoder)?;
-
-            match map_key.as_ref() {
-                MapKeyRef::Text("index") => {
-                    index = Some(CborDeserialize::deserialize(decoder)?);
-                }
-                MapKeyRef::Text("address") => {
-                    address = Some(CborDeserialize::deserialize(decoder)?);
-                }
-                key => return Err(CborError::unknown_map_key(key)),
-            }
-        }
-        let index = match index {
-            None => match CborDeserialize::null() {
-                None => return Err(CborError::map_value_missing(MapKeyRef::Text("index"))),
-                Some(null) => null,
-            },
-            Some(coin_info) => coin_info,
-        };
-
-        let address = match address {
-            None => match CborDeserialize::null() {
-                None => return Err(CborError::map_value_missing(MapKeyRef::Text("address"))),
-                Some(null) => null,
-            },
-            Some(address) => address,
-        };
-
-        Ok(Self { address, index })
-    }
 }
 
 /// The balance of tokens on the sender account is insufficient
--- rust-src/concordium_base_derive/src/cbor.rs
+++ rust-src/concordium_base_derive/src/cbor.rs
@@ -10,7 +10,83 @@ fn get_cbor_module() -> syn::Result<TokenStream> {
 }
 
 pub fn impl_cbor_deserialize(ast: &syn::DeriveInput) -> syn::Result<TokenStream> {
-    todo!()
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clauses) = ast.generics.split_for_impl();
+
+    let cbor_module = get_cbor_module()?;
+
+    let deserialize_body = match &ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields_named),
+            ..
+        }) => {
+            struct Field {
+                ident: Ident,
+            }
+
+            let fields = fields_named
+                .named
+                .iter()
+                .map(|field| {
+                    let ident = field
+                        .ident
+                        .clone()
+                        .ok_or(syn::Error::new_spanned(field, "unnamed field"))?;
+
+                    Ok(Field { ident })
+                })
+                .collect::<syn::Result<Vec<_>>>()?;
+
+            let field_idents: Vec<_> = fields.iter().map(|field| &field.ident).collect();
+            let field_idents_literals: Vec<_> = fields
+                .iter()
+                .map(|field| LitStr::new(&field.ident.to_string(), field.ident.span()))
+                .collect();
+
+            quote! {
+                #(let mut #field_idents = None;)*
+                let map_size = #cbor_module::CborDecoder::decode_map(decoder)?;
+                for _ in 0..map_size {
+                    let map_key: #cbor_module::MapKey = #cbor_module::CborDeserialize::deserialize(decoder)?;
+
+                    match map_key.as_ref() {
+                        #(
+                            #cbor_module::MapKeyRef::Text(#field_idents_literals) => {
+                                #field_idents = Some(#cbor_module::CborDeserialize::deserialize(decoder)?);
+                            }
+                        )*
+                        key => return Err(#cbor_module::CborError::unknown_map_key(key)),
+                    }
+                }
+
+                #(
+                    let #field_idents = match #field_idents {
+                        None => match #cbor_module::CborDeserialize::null() {
+                            None => return Err(#cbor_module::CborError::map_value_missing(#cbor_module::MapKeyRef::Text(#field_idents_literals))),
+                            Some(null_value) => null_value,
+                        },
+                        Some(#field_idents) => #field_idents,
+                    };
+                )*
+
+                Ok(Self { #(#field_idents),* })
+            }
+        }
+        _ => {
+            return Err(syn::Error::new(
+                Span::call_site(),
+                "CborDeserialize can only be applied to structs with named fields",
+            ))
+        }
+    };
+
+    Ok(quote! {
+        impl #impl_generics #cbor_module::CborDeserialize for #name #ty_generics #where_clauses {
+            fn deserialize<C: #cbor_module::CborDecoder>(decoder: &mut C) -> #cbor_module::CborResult<Self> {
+                #deserialize_body
+            }
+        }
+    })
 }
 
 pub fn impl_cbor_serialize(ast: &syn::DeriveInput) -> syn::Result<TokenStream> {
@@ -47,11 +123,6 @@ pub fn impl_cbor_serialize(ast: &syn::DeriveInput) -> syn::Result<TokenStream> {
                 .map(|field| LitStr::new(&field.ident.to_string(), field.ident.span()))
                 .collect();
 
-            // let map_size = fields.map(
-            //     |field| quote!(
-            //         ,
-            // );
-
             quote! {
                 #cbor_module::CborEncoder::encode_map(encoder,
                     #(if #cbor_module::CborSerialize::is_null(&self.#field_idents) {0} else {1})+*
@@ -77,7 +148,7 @@ pub fn impl_cbor_serialize(ast: &syn::DeriveInput) -> syn::Result<TokenStream> {
 
     Ok(quote! {
         impl #impl_generics #cbor_module::CborSerialize for #name #ty_generics #where_clauses {
-            fn serialize<C: #cbor_module::CborEncoder>(&self, encoder: &mut C) -> CborResult<()> {
+            fn serialize<C: #cbor_module::CborEncoder>(&self, encoder: &mut C) -> #cbor_module::CborResult<()> {
                 #serialize_body
             }
         }
