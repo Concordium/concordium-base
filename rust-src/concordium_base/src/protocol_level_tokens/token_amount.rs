09b93277fdac6da2863430346bf7eaecdd180c71
Allan Rasmussen <allanbrondum@gmail.com>
2025-06-12 11:11:55 +0200

    token amount decimals

--- rust-src/concordium_base/src/protocol_level_tokens/token_amount.rs
+++ rust-src/concordium_base/src/protocol_level_tokens/token_amount.rs
@@ -2,14 +2,21 @@ use crate::common::cbor::{
     CborDecoder, CborDeserialize, CborEncoder, CborSerializationResult, CborSerialize,
     DecimalFraction,
 };
-use anyhow::Context;
+use anyhow::{bail, Context};
 
-/// Protocol level token (PLT) amount representation.
-#[derive(Debug, Clone, Copy, serde::Deserialize, serde::Serialize)]
+/// Protocol level token (PLT) amount representation. The numerical amount represented is
+/// `value * 10^(-decimals)`.
+/// The number of decimals in the token amount should always match the number of decimals
+/// for the token it represents an amount for. As such, `TokenAmount` can be considered a fixed point decimal.
+///
+/// Notice that the `decimal` part could hence be left out of the representation without loss of information,
+/// but it is there to make `TokenAmount` self-contained with regard to the numerical value represented.
+/// This enables additional validation, both programmatic and at user level.
+#[derive(Debug, Clone, Copy, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(try_from = "TokenAmountJson", into = "TokenAmountJson")]
 pub struct TokenAmount {
-    /// The amount of tokens without decimal places.
-    value:    u64,
+    /// The amount of tokens as an unscaled integer value.
+    value: u64,
     /// The number of decimals in the token amount.
     decimals: u8,
 }
@@ -30,7 +37,8 @@ impl CborSerialize for TokenAmount {
 impl CborDeserialize for TokenAmount {
     fn deserialize<C: CborDecoder>(decoder: &mut C) -> CborSerializationResult<Self>
     where
-        Self: Sized, {
+        Self: Sized,
+    {
         let decimal_fraction = DecimalFraction::deserialize(decoder)?;
 
         let decimals = decimal_fraction
@@ -49,23 +57,78 @@ impl TokenAmount {
     /// Construct a [`TokenAmount`] from a value without decimal places and the
     /// number of decimals, meaning the token amount is computed as `value *
     /// 10^(-decimals)`.
-    pub fn from_raw(value: u64, decimals: u8) -> Self { Self { value, decimals } }
-
-    /// Construct a [`TokenAmount`] representing an integer amount and zero
-    /// decimals.
-    pub fn from_integer(value: u64) -> Self { Self { value, decimals: 0 } }
+    pub fn from_raw(value: u64, decimals: u8) -> Self {
+        Self { value, decimals }
+    }
 
     /// The number of decimals in the token amount.
     ///
-    /// Together with the `raw_value` the token amount can be represented as
-    /// `raw_value * 10^(-decimals)`
-    pub fn decimals(&self) -> u8 { self.decimals }
+    /// The numerical amount represented by the `TokenAmount`
+    /// is `value * 10^(-decimals)`
+    pub fn decimals(&self) -> u8 {
+        self.decimals
+    }
 
-    /// The amount of tokens without decimal places.
+    /// The amount of tokens as an unscaled integer value.
     ///
-    /// Together with the `decimals` the token amount can be represented as
-    /// `raw_value * 10^(-decimals)`
-    pub fn raw_value(&self) -> u64 { self.value }
+    /// The numerical amount represented by the `TokenAmount`
+    /// is `value * 10^(-decimals)`
+    pub fn value(&self) -> u64 {
+        self.value
+    }
+
+    /// Converts [`rust_decimal::Decimal`] to a token amount of the same numerical value but represented
+    /// with the given number of `decimals`. This may require rescaling but no rounding will be performed.
+    /// If rounding is required to represent the numeric value with the given number of `decimals`, an
+    /// error is returned.
+    ///
+    /// The given number of `decimals` should
+    /// be equal to tbe number of decimals for the token it represents an amount for.
+    pub fn try_from_rust_decimal(
+        decimal: rust_decimal::Decimal,
+        decimals: u8,
+    ) -> anyhow::Result<Self> {
+        let decimals = decimals as u32;
+        let original_scale = decimal.scale();
+        let mut decimal_scaled = decimal;
+        decimal_scaled.rescale(decimals);
+        if decimal_scaled.scale() != decimals {
+            bail!("rust_decimal::Decimal cannot be rescaled to match token amount");
+        }
+        let this = Self::from_raw(
+            decimal_scaled.mantissa().try_into().context(
+                "rust_decimal::Decimal mantissa cannot be converted to token amount value",
+            )?,
+            decimal_scaled.scale().try_into().context(
+                "rust_decimal::Decimal scale cannot be converted to token amount decimals",
+            )?,
+        );
+        decimal_scaled.rescale(decimal.scale());
+        if decimal_scaled.mantissa() != decimal.mantissa() {
+            bail!("rust_decimal::Decimal rescaling results in rounding");
+        }
+        Ok(this)
+    }
+
+    /// Converts the token amount to a [`rust_decimal::Decimal`] of the same scale.
+    pub fn try_to_rust_decimal(&self) -> anyhow::Result<rust_decimal::Decimal> {
+        rust_decimal::Decimal::try_from_i128_with_scale(self.value as i128, self.decimals as u32)
+            .context("token amount cannot be represented as rust_decimal::Decimal")
+    }
+
+    /// Interprets the given string as a decimal number (decimal separator must be "." if specified) and
+    /// parses it into a token amount of the same numerical value
+    /// represented with the given number of `decimals`.     
+    /// If rounding is required to represent the numeric value with the given number of `decimals`, an
+    /// error is returned.
+    ///
+    /// The given number of `decimals` should
+    /// be equal to tbe number of decimals for the token it represents an amount for.
+    pub fn try_from_str(decimal_str: &str, decimals: u8) -> anyhow::Result<Self> {
+        let decimal = rust_decimal::Decimal::from_str_exact(decimal_str)
+            .context("cannot convert string to rust_decimal::Decimal")?;
+        Self::try_from_rust_decimal(decimal, decimals)
+    }
 }
 
 impl std::fmt::Display for TokenAmount {
@@ -84,123 +147,25 @@ impl std::fmt::Display for TokenAmount {
     }
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum TokenAmountParseError {
-    #[error("The number of digits after the decimal point can be at most 255, found {0}")]
-    InvalidNumberOfDecimals(usize),
-    #[error("Unable to parse digits before the decimal point: {0}")]
-    FailedParsingWholeNumber(std::num::ParseIntError),
-    #[error("Unable to parse digits after the decimal point: {0}")]
-    FailedParsingFractionalPart(std::num::ParseIntError),
-    #[error("Parse digits are invalid and caused an overflow")]
-    Overflow,
-}
-
-impl std::str::FromStr for TokenAmount {
-    type Err = TokenAmountParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if let Some((integer, fraction)) = s.split_once('.') {
-            let decimals: u8 = fraction
-                .len()
-                .try_into()
-                .map_err(|_| TokenAmountParseError::InvalidNumberOfDecimals(fraction.len()))?;
-            let integer: u64 = integer
-                .parse()
-                .map_err(TokenAmountParseError::FailedParsingWholeNumber)?;
-            let fraction: u64 = fraction
-                .parse()
-                .map_err(TokenAmountParseError::FailedParsingFractionalPart)?;
-            let value = 10u64
-                .checked_pow(decimals.into())
-                .ok_or(TokenAmountParseError::Overflow)?
-                .checked_mul(integer)
-                .ok_or(TokenAmountParseError::Overflow)?
-                .checked_add(fraction)
-                .ok_or(TokenAmountParseError::Overflow)?;
-            Ok(Self { value, decimals })
-        } else {
-            Ok(Self {
-                value:    s
-                    .parse()
-                    .map_err(TokenAmountParseError::FailedParsingWholeNumber)?,
-                decimals: 0,
-            })
-        }
-    }
-}
-
 impl PartialOrd for TokenAmount {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> { Some(self.cmp(other)) }
-}
-
-impl Ord for TokenAmount {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let ord_decimals = self.decimals.cmp(&other.decimals);
-        // If the decimals are equal we can compare the values directly.
-        if ord_decimals.is_eq() {
-            return self.value.cmp(&other.value);
-        }
-        // If either of the values are 0 we can ignore the decimals and compare the
-        // values directly.
-        if self.value == 0 || other.value == 0 {
-            return self.value.cmp(&other.value);
-        };
-        // To avoid overflows we check the ordering in floored log_10 first
-        let self_log = self.value.ilog10(); // Safe since the value cannot be 0.
-        let other_log = other.value.ilog10(); // Safe since the value cannot be 0.
-        let left = self_log + u32::from(other.decimals);
-        let right = other_log + u32::from(self.decimals);
-        let log_ord = left.cmp(&right);
-        // When floored log_10 values are not equal we can return this ordering.
-        if !log_ord.is_eq() {
-            return log_ord;
-        }
-        // When floored log_10 values are equal, there must be the same number of digits
-        // in the representation, so we scale value with fewest decimals up to the same
-        // number of decimals and compare.
-        // If this overflows we know the scaled value must be greater.
-        match ord_decimals {
-            std::cmp::Ordering::Equal => panic!("Impossible case: this should be handled above"),
-            std::cmp::Ordering::Less => {
-                let decimal_diff = other.decimals - self.decimals;
-                if let Some(scaled_self) = self.value.checked_mul(10u64.pow(decimal_diff.into())) {
-                    scaled_self.cmp(&other.value)
-                } else {
-                    std::cmp::Ordering::Greater
-                }
-            }
-            std::cmp::Ordering::Greater => {
-                let decimal_diff = self.decimals - other.decimals;
-                if let Some(scaled_other) = other.value.checked_mul(10u64.pow(decimal_diff.into()))
-                {
-                    scaled_other.cmp(&self.value)
-                } else {
-                    std::cmp::Ordering::Less
-                }
-            }
-        }
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        (self.decimals == other.decimals).then(|| self.value.cmp(&other.value))
     }
 }
-
-impl PartialEq for TokenAmount {
-    fn eq(&self, other: &Self) -> bool { matches!(self.cmp(other), std::cmp::Ordering::Equal) }
-}
-impl Eq for TokenAmount {}
 
 /// JSON representation of the token amount.
 /// The purpose of this type is to derive the JSON representation matching other
 /// SDKs.
 #[derive(serde::Serialize, serde::Deserialize)]
 struct TokenAmountJson {
-    value:    String,
+    value: String,
     decimals: u8,
 }
 
 impl From<TokenAmount> for TokenAmountJson {
     fn from(amount: TokenAmount) -> Self {
         Self {
-            value:    amount.value.to_string(),
+            value: amount.value.to_string(),
             decimals: amount.decimals,
         }
     }
@@ -211,7 +176,7 @@ impl TryFrom<TokenAmountJson> for TokenAmount {
 
     fn try_from(json: TokenAmountJson) -> Result<Self, Self::Error> {
         Ok(Self {
-            value:    json.value.parse()?,
+            value: json.value.parse()?,
             decimals: json.decimals,
         })
     }
@@ -221,168 +186,102 @@ impl TryFrom<TokenAmountJson> for TokenAmount {
 mod test {
     use super::*;
     use crate::common::cbor;
+    use std::cmp::Ordering;
 
     #[test]
-    fn display_token_amount() {
+    fn test_display() {
         let amount = TokenAmount {
-            value:    123456,
+            value: 123450,
             decimals: 3,
         };
-        assert_eq!(amount.to_string().as_str(), "123.456");
+        assert_eq!(amount.to_string().as_str(), "123.450");
 
         let amount = TokenAmount {
-            value:    10,
+            value: 10,
             decimals: 5,
         };
         assert_eq!(amount.to_string().as_str(), "0.00010");
     }
 
     #[test]
-    fn parse_token_amount() {
-        let amount = TokenAmount {
-            value:    123456,
-            decimals: 3,
-        };
-        let parsed: TokenAmount = "123.456"
-            .parse()
-            .expect("Parsing token amount should succeed");
-        assert_eq!(amount.value, parsed.value);
-        assert_eq!(amount.decimals, parsed.decimals);
+    fn test_try_from_str() {
+        let str = "123.450";
+        
+        let token_amount = TokenAmount::try_from_str(str, 3).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(123450, 3));
+        let token_amount = TokenAmount::try_from_str(str, 2).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(12345, 2));
+        let token_amount = TokenAmount::try_from_str(str, 5).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(12345000, 5));
+
+        let err = TokenAmount::try_from_str(str, 1)
+            .unwrap_err()
+            .to_string();
+        assert!(
+            err.contains("rescaling results in rounding"),
+            "error: {}",
+            err
+        );
+        
     }
 
     #[test]
-    fn ord_token_amount() {
+    fn test_partial_cmp() {
         // Check using same decimals.
         {
-            let first = TokenAmount {
-                value:    123,
-                decimals: 3,
-            };
-
-            let last = TokenAmount {
-                value:    456,
-                decimals: 3,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert!(first < last);
-            assert!(last > first);
+            let amount1 = TokenAmount::from_raw(123, 3);
+            let amount2 = TokenAmount::from_raw(456, 3);
+            assert_eq!(amount1.partial_cmp(&amount2), Some(Ordering::Less));
         }
 
         {
             // Check using different decimals.
-            // 0.123
-            let first = TokenAmount {
-                value:    123,
-                decimals: 3,
-            };
-            // 1.23
-            let last = TokenAmount {
-                value:    123,
-                decimals: 2,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert!(first < last);
-            assert!(last > first);
-        }
-
-        {
-            // Check with matching max decimal
-            // 0.123
-            let first = TokenAmount {
-                value:    1,
-                decimals: u8::MAX,
-            };
-            // 1.23
-            let last = TokenAmount {
-                value:    u64::MAX,
-                decimals: u8::MAX,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert!(first < last);
-            assert!(last > first);
-        }
-
-        {
-            // Check with large difference in decimals
-            // 0.123
-            let first = TokenAmount {
-                value:    u64::MAX,
-                decimals: u8::MAX,
-            };
-            // 1.23
-            let last = TokenAmount {
-                value:    u64::MAX,
-                decimals: u8::MIN,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert!(first < last);
-            assert!(last > first);
-        }
-
-        {
-            // Check with large difference in decimals
-            // 0.123
-            let first = TokenAmount {
-                value:    10_000_000_000_000_000_000,
-                decimals: 19,
-            };
-            // 1.23
-            let last = TokenAmount {
-                value:    2,
-                decimals: 0,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert!(first < last);
-            assert!(last > first);
+            let amount1 = TokenAmount::from_raw(123, 3);
+            let amount2 = TokenAmount::from_raw(456, 2);
+            assert_eq!(amount1.partial_cmp(&amount2), None);
         }
     }
 
     #[test]
-    fn eq_token_amount() {
-        // Same number of decimals
-        {
-            // 123.456
-            let a = TokenAmount {
-                value:    123456,
-                decimals: 3,
-            };
-            // 123.456
-            let b = TokenAmount {
-                value:    123456,
-                decimals: 3,
-            };
-            assert_eq!(a, b);
-        }
-        // Different number of decimals
-        {
-            // 5.0
-            let a = TokenAmount {
-                value:    50,
-                decimals: 1,
-            };
-            // 5
-            let b = TokenAmount {
-                value:    5,
-                decimals: 0,
-            };
-            // Note that both directions are needed to cover both branches in the
-            // implementation.
-            assert_eq!(a, b);
-            assert_eq!(b, a);
-        }
+    fn test_try_from_rust_decimal() {
+        let decimal = rust_decimal::Decimal::new(12600, 4);
+
+        let token_amount = TokenAmount::try_from_rust_decimal(decimal, 4).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(12600, 4));
+
+        let token_amount = TokenAmount::try_from_rust_decimal(decimal, 2).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(126, 2));
+
+        let token_amount = TokenAmount::try_from_rust_decimal(decimal, 6).unwrap();
+        assert_eq!(token_amount, TokenAmount::from_raw(1260000, 6));
+
+        let err = TokenAmount::try_from_rust_decimal(decimal, 30)
+            .unwrap_err()
+            .to_string();
+        assert!(err.contains("cannot be rescaled"), "error: {}", err);
+
+        let err = TokenAmount::try_from_rust_decimal(decimal, 1)
+            .unwrap_err()
+            .to_string();
+        assert!(
+            err.contains("rescaling results in rounding"),
+            "error: {}",
+            err
+        );
+    }
+
+    #[test]
+    fn test_try_to_rust_decimal() {
+        let token_amount = TokenAmount::from_raw(12640, 4);
+
+        let decimal = token_amount.try_to_rust_decimal().unwrap();
+        assert_eq!(decimal.mantissa(), 12640);
+        assert_eq!(decimal.scale(), 4);
     }
 
     #[test]
     fn test_token_amount_cbor() {
-        let token_amount = TokenAmount {
-            value:    12300,
-            decimals: 3,
-        };
+        let token_amount = TokenAmount::from_raw(12300, 3);
 
         let cbor = cbor::cbor_encode(&token_amount).unwrap();
         assert_eq!(hex::encode(&cbor), "c4822219300c");
