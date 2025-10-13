97793696c2f964ebe35522a1785267fc2d596273
Allan Rasmussen <allanbrondum@gmail.com>
2025-10-13 14:48:26 +0200

    random oracle log

--- rust-src/concordium_base/src/id/identity_attributes_credentials.rs
+++ rust-src/concordium_base/src/id/identity_attributes_credentials.rs
@@ -678,9 +678,9 @@ mod test {
         IpInfo, Policy,
     };
     use crate::id::{identity_provider, test};
+    use crate::random_oracle::RandomOracle;
     use assert_matches::assert_matches;
     use std::collections::BTreeMap;
-    use crate::random_oracle::RandomOracle;
 
     struct IdentityObjectFixture {
         id_object: IdentityObjectV1<IpPairing, ArCurve, AttributeKind>,
@@ -769,6 +769,8 @@ mod test {
             &mut transcript,
         )
         .expect("verify");
+
+        println!("transcript log:\n {:#?}", transcript.log())
     }
 
     /// Test that the verifier accepts a valid proof. Test variant with revealed attribute values
@@ -882,7 +884,7 @@ mod test {
             &id_object_fixture.ip_info,
             &id_object_fixture.ars_infos,
             &id_attr_info,
-            &mut  transcript,
+            &mut transcript,
         );
         assert_matches!(res, Err(AttributeCommitmentVerificationError::Signature));
     }
