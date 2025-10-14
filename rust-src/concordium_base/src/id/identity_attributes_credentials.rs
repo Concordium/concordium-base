780fbf91bf1ffbb93fb410051316802958a70be1
Allan Rasmussen <allanbrondum@gmail.com>
2025-10-14 11:14:13 +0200

    fix

--- rust-src/concordium_base/src/id/identity_attributes_credentials.rs
+++ rust-src/concordium_base/src/id/identity_attributes_credentials.rs
@@ -1,5 +1,5 @@
-//! Functionality to prove and verify attribute commitments based on identity credentials. These are to a large
-//! extent equivalent to attribute commitments deployed on chain, but there is no on-chain account credentials involved.
+//! Functionality to prove and verify identity attribute credentials based on identity credentials. These are to a large
+//! extent equivalent to account credentials deployed on chain, but there is no on-chain account credentials involved.
 
 use super::{account_holder, secret_sharing::*, types::*, utils};
 use crate::pedersen_commitment::{CommitmentKey, Randomness};
@@ -678,9 +678,9 @@ mod test {
         IpInfo, Policy,
     };
     use crate::id::{identity_provider, test};
+    use crate::random_oracle::RandomOracle;
     use assert_matches::assert_matches;
     use std::collections::BTreeMap;
-    use crate::random_oracle::RandomOracle;
 
     struct IdentityObjectFixture {
         id_object: IdentityObjectV1<IpPairing, ArCurve, AttributeKind>,
@@ -882,7 +882,7 @@ mod test {
             &id_object_fixture.ip_info,
             &id_object_fixture.ars_infos,
             &id_attr_info,
-            &mut  transcript,
+            &mut transcript,
         );
         assert_matches!(res, Err(AttributeCommitmentVerificationError::Signature));
     }
