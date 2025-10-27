5f0b7f1bee6e2f3c4ed40aae50c75acf7b7413b4
Allan Rasmussen <allanbrondum@gmail.com>
2025-10-27 15:11:26 +0100

    make attribute tags public

--- rust-src/concordium_base/src/id/identity_attributes_credentials.rs
+++ rust-src/concordium_base/src/id/identity_attributes_credentials.rs
@@ -302,8 +302,8 @@ fn signature_knowledge_prover<
     // Attribute tag set we choose not to be public. In that way we only disclose the tags
     // for the attributes we commit to or reveal.
     let tags_val = utils::encode_tags(alist.alist.keys())?;
-    msgs.push(PsSigMsg::Known);
-    secrets.push(PsSigWitnessMsg::Known(Value::new(tags_val)));
+    msgs.push(PsSigMsg::Public(Value::new(tags_val)));
+    secrets.push(PsSigWitnessMsg::Public);
 
     // Max accounts is not public, we prove knowledge of it
     let max_accounts_val = C::scalar_from_u64(alist.max_accounts.into());
@@ -577,7 +577,8 @@ fn signature_knowledge_verifier<
     }
 
     // Attribute tag set is just verified known
-    msgs.push(PsSigMsg::Known);
+    let tags_val = utils::encode_tags(id_attr_info.values.attributes.keys()).ok()?;
+    msgs.push(PsSigMsg::Public(Value::new(tags_val)));
 
     // Max accounts is not public, we verify knowledge of it
     msgs.push(PsSigMsg::Known);
@@ -1208,7 +1209,7 @@ mod test {
         let id_object_fixture = identity_object_fixture();
 
         // Comment out to regenerate proof. We can do that as long as we have not released yet
-        //
+
         // let mut attributes_handling = BTreeMap::new();
         // attributes_handling.insert(TAG_0, IdentityAttributeHandling::Commit);
         // attributes_handling.insert(TAG_1, IdentityAttributeHandling::Reveal);
@@ -1226,7 +1227,7 @@ mod test {
         // let proof_bytes_hex = hex::encode(common::to_bytes(&id_attr_info));
         // assert_eq!(proof_bytes_hex, "");
 
-        let proof_bytes_hex = "0000000004000500000001b1e598e73ec0a06a4e68a643083d7146f9155414b428efd0dc39758adc6e7a2dba718d456a03b3fc2de66ed09170c778951b05efd8044b6f2a74397d57ec0c2b5fc5758fb4dfd3f811b4ab149a375e226c74373bcf235c72e19e8f689a2df8e900000002b7d95f164d44215f908e1a2f74887d336200a9444f760d5700004341a1a5e43690b24445ff090deb68b40340c34bff26b27fb696a056d0ac25b0605c3b234616bee6456191d1b9382119e63bb610ebfdab536c16aefb432b4cfe88956162dcbf00000003b8e6654ab88938e076b172816b61bd0da83ccb47d460128e74d8170d6a37b14c4945fcad34c9550231179c968f90fce5b11b713b44da97b5ffb02c14b4d797eabcbd8970f1dad96a0bc0758a5a685e66f2f9ea89dee66816cf3d5f1d0b4e218b00000004b8bbc524fe3b4112167c8ea7f92c5212a94914ab7be792a342648d5b562f210a4c4de257ac48486441c117df7dc8de8ab8f4a745961bd0ad3212087cb15d6b6f3dbe19c56be728bdd508ef5aeeb610d5ea10a64f078f6b21a871ebe86da6737c00000005929d59d7894d007f71c9e382689cb2086c8b0caf188cdd31fcd8138ffddd76a0b614a22162a51d4e2b795f919be3af7f8d8049a086121762dcccc4883651339954ba985b532efe4f0c40680d210cc0f93065790a4334b3d49ef67bf1c172929e000300008e7a1c2f48419f19dc97751937a2d1c3163c4546aa45ce37863f37a54c1d43d2f5a9646b031a1ead25980cc6cdf94a2b0201057465737431050207e60507e405a3eb4da90047ef8ecda05e0c3f6b57659fb2f3550fab1285628f7192d8ae135f22defc274a0e9dadf98b226507ae8a1d991eed12b245373698522784cd8ff32718fbb9f4b4794b57ace9d9d2dbac445366d7e424fd15eba1b5ddb5fa47c2fcb800000000000000048b1440e9efd587181ce32b8864b9c6287adb0fa24662118f0089d99a57b611b9596d33a5fd04fd0024840ce1002b7fb586ac9f85236780ea53f280f3820d0be385b6d4400dc028e940569d1f31cc0486a024ec8f302790e5ffe7b40a9fcb9bb6917e34ac78b6482a3089324cdea492f6a8287e3a1993f248a1db590af49fc3f7ec9c8c97429c809286672f5e88ba33648237b5833480623e5e88d7de54cb53e6c1b40a4642aaf002fdb1e1b8e615037143ac72c2dca990c1aef279f2ee0e6e27a759129fb5b196774d66e9f5f219fe183160091d6eccb7cc63040a621e771b37000000050000000109ea4726659a439f89b3356bd4001ebad9e2901868a996b5295bfe75f995990f4aca3be89c4e25ee69cda09084c2b3eca3c54fdd8b370ba17fc5c0210a3fbb622268ccfe2ee1091f53725febc54986efe2bedbb7b49c390417652187797312880000000273aea54c713d331c75949c1858aaa26a8be88ed9f0024fb7f03aed7630dcdc7b1641b162ab90f2096c29dc8bd1cac00e833c759407ec866f5162ea31d296a09858bc342c5c75c523c8d31c0570025bb0485811b8b22ad72f6a8f5f67c6cf21780000000332b1a857a9c89bb4dc2fa09f1caa1d8e4e46550fa16e97e56c83955b32d4375a110e749d8a704e3152514c310ea20e62d8a9183efee6c3766888b49dbc1970652088cca07655ec588df869e4d609db645cb2b266f816fbfe3327aa05e29977ae00000004409dbba015e514777740d9dc39b79b46f0fa569c974931c317ad661b852d7adb33a7192ff305a48b8ebecb8b35c4eca59239fbc07b7ea09c4fe6c0d3159ad97c40b41a6fec9262088c5b50a4874a267160683e6fae4111252f4346528c7ff3d2000000054b408170b31dfa73767e9ea56eab38f04bd10be7c69d60df6b6b9e28e6edb7ea536fce8c1c7b76a9e3b99a842de20cc8f8bcf84f6dddca2a650bb4ccc3cab3bf0c67ab96dcabe3d6b103857e492d2704c5fa1fbc01181b3268d75de80af7e8440bbac55533e357d3c0c94009b1aecd6f19b33414c307478a66960d098564cb8e00000009004809112af37ee003339e4a7095f86aad64bb5d202ad610f7ac3b79c36e29522b62db69a6236c5d6f5f8a252a567d1911abbe7322077ddefa73dc8af086044f86023f5964a47841261fec78b59f361443903606fcf8c150e6a587f46c2003dac7b701010202329315e825b6b0f079289ff6cc82d40e0198f601060d78faabeecc4d70ce4e027262d15a64182e33c45d3137c65cf3904b2d66b75b94e5cdf5ea54786a193387006357bbbd9d1d48a41c6e6f1dc764218bbbe8e68214b57d184063b5eb1eabdb312a249a04064d8963b8ecf84baf6e3d7b1cc384b6903f1cd2e7f4652fd0eea5110102281969f31040c1107fa839908acb5f6186d5a9ab0f746b084d90cabc9c2bee24";
+        let proof_bytes_hex = include_str!("identity_attributes_credentials_stable_proof.hex");
         let proof_bytes = hex::decode(&proof_bytes_hex).unwrap();
         let id_attr_info: IdentityAttributesCredentialsInfo<Bls12, G1, AttributeKind> =
             common::from_bytes(&mut proof_bytes.as_slice()).expect("deserialize");
