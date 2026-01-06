9cfdb9f7c3c5b9b9276578afc22b99a0685fe5bb
Allan Rasmussen <allanbrondum@gmail.com>
2026-01-06 17:28:00 +0100

    tokenid display

--- rust-src/concordium_base/src/protocol_level_tokens/token_id.rs
+++ rust-src/concordium_base/src/protocol_level_tokens/token_id.rs
@@ -32,6 +32,12 @@ impl AsRef<str> for TokenId {
     }
 }
 
+impl std::fmt::Display for TokenId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.value)
+    }
+}
+
 impl std::str::FromStr for TokenId {
     type Err = TokenIdFromStringError;
 
