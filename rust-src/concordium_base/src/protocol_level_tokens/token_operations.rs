fabdc5dcddb13051dd4ab3ceab96c1a5e799f25a
Allan Rasmussen <allanbrondum@gmail.com>
2026-01-09 12:06:45 +0100

    fix

--- rust-src/concordium_base/src/protocol_level_tokens/token_operations.rs
+++ rust-src/concordium_base/src/protocol_level_tokens/token_operations.rs
@@ -292,6 +292,15 @@ pub enum CborMemo {
     Cbor(Memo),
 }
 
+impl From<CborMemo> for Memo {
+    fn from(value: CborMemo) -> Self {
+        match value {
+            CborMemo::Raw(memo) => memo,
+            CborMemo::Cbor(memo) => memo,
+        }
+    }
+}
+
 #[cfg(test)]
 pub mod test {
     use super::*;
