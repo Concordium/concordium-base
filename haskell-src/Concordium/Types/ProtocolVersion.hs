91f543ae74f78ba36d8346df6c9477462aa635ea
Allan Rasmussen <allanbrondum@gmail.com>
2026-03-04 12:25:28 +0100

    pvread

--- haskell-src/Concordium/Types/ProtocolVersion.hs
+++ haskell-src/Concordium/Types/ProtocolVersion.hs
@@ -571,6 +571,7 @@ instance ToJSON ChainParametersVersion where
     toJSON = toJSON . chainParameterVersionToWord64
 
 deriving instance Show ProtocolVersion
+deriving instance Read ProtocolVersion
 
 deriving instance Show ChainParametersVersion
 
