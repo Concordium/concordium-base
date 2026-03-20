037042507ea65c0850328baa5e16170487153dc0
Allan Rasmussen <allanbrondum@gmail.com>
2026-03-20 03:59:41 +0100

    Remove musl build variant

--- Setup.hs
+++ Setup.hs
@@ -16,30 +16,9 @@ concordiumLibs =
 
 type WithEnvAndVerbosity = [(String, String)] -> Verbosity -> IO ()
 
--- | In linux, we will produce two kind of builds:
---  - Static with musl: the rust libraries will only build static artifacts. Intended to be used inside alpine to produce a static binary.
---  - With glibc: Normal compilation. Rust will produce static and dynamic artifacts.
---
---  The first argument chooses whether to build statically with musl or not.
 linuxBuild :: Bool -> WithEnvAndVerbosity
-linuxBuild True env verbosity = do
-    fail "static build True: This code path should not be reached unless we compile to musl target"
-    -- noticeNoWrap verbosity "Static linking."
-    -- -- the target-feature=-crt-static is needed so that C symbols are not included in the generated rust libraries. For more information check https://rust-lang.github.io/rfcs/1721-crt-static.html
-    -- let makeLib (libName, libFeatures) = do
-    --         -- the target-feature=-crt-static is needed so that C symbols are not included in the generated rust libraries. For more information check https://rust-lang.github.io/rfcs/1721-crt-static.html
-    --         rawSystemExitWithEnv
-    --             verbosity
-    --             "cargo"
-    --             (["rustc", "--release", "--manifest-path", "rust-src/" ++ libName ++ "/Cargo.toml", "--target", "x86_64-unknown-linux-musl", "--crate-type", "staticlib"] ++ libFeatures)
-    --             (("RUSTFLAGS", "-C target-feature=-crt-static") : env)
-    --         let source = "../rust-src/target/x86_64-unknown-linux-musl/release/lib" ++ libName ++ ".a"
-    --             target = "./lib/lib" ++ libName ++ ".a"
-    --         rawSystemExit verbosity "ln" ["-s", "-f", source, target]
-    --         noticeNoWrap verbosity $ "Linked: " ++ target ++ " -> " ++ source
-    -- mapM_ makeLib concordiumLibs
-linuxBuild False env verbosity = do
-    noticeNoWrap verbosity "Dynamic linking."
+linuxBuild env verbosity = do
+linuxBuild env verbosity = do
     let makeLib (libName, libFeatures) = do
             rawSystemExitWithEnv verbosity "cargo" (["rustc", "--release", "--manifest-path", "rust-src/" ++ libName ++ "/Cargo.toml", "--crate-type", "cdylib", "--crate-type", "staticlib"] ++ libFeatures) env
             notice verbosity "Linking library to ./lib"
@@ -98,7 +77,7 @@ makeRust _ flags _ lbi = do
         build = case buildOS of
             Windows -> windowsBuild
             OSX -> osxBuild staticLinking
-            Linux -> linuxBuild staticLinking
+            Linux -> linuxBuild
     env <- getEnvironment
     rawSystemExit verbosity "mkdir" ["-p", "./lib"]
     build env verbosity
