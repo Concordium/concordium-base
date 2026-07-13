d50e85d2637dafbdd90be0d9febdca44b0cc9d5f
Allan Rasmussen <allanbrondum@gmail.com>
2026-07-13 14:17:16 +0200

    Simplify imports rested to std

--- smart-contracts/contracts-common/concordium-contracts-common/src/lib.rs
+++ smart-contracts/contracts-common/concordium-contracts-common/src/lib.rs
@@ -80,13 +80,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(not(feature = "std"))]
 extern crate alloc;
 
-#[cfg(not(feature = "std"))]
 use alloc::{string::String, vec::Vec};
-#[cfg(feature = "std")]
-use std::{string::String, vec::Vec};
 
 #[macro_use]
 mod traits;
